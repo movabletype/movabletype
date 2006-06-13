c65bf82003c9b3854af46d84635b3f9179db87c1
Jay Allen <info@endevver.com>
2006-06-13 09:14:10 +0000

    r1067@mt-master (orig r30928):  fyoshimatsu | 2006-06-12 21:51:14 -0700  Some efficiency added on checking whether the page is list_author  BugID: 29889

--- mt-static/mt.js
+++ mt-static/mt.js
@@ -93,8 +93,10 @@ function doRemoveItems (f, singular, plural, nameRestrict, args) {
     }
     var toRemove = "";
     for (var i in f.childNodes) {
-        if (f.childNodes[i].name == '_type')
+        if (f.childNodes[i].name == '_type') {
             toRemove = f.childNodes[i].value;
+            break;
+        }
     }
     if(toRemove == 'author') {
         singularMessage = 'Deleting an author is an irrevocable action which creates orphans of the author\'s entries.  If you wish to retire an author or remove their access to the system, removing all of their permissions is the recommended course of action.  Are you sure you want to delete this author?';
