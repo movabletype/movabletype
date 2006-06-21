6b53bf3709543b421c1a23237216941a59626471
Jay Allen <info@endevver.com>
2006-06-21 16:42:50 +0000

    r1570@mt-master (orig r31892):  fyoshimatsu | 2006-06-21 09:12:12 -0700  Localization take 2.

--- mt-static/mt.js
+++ mt-static/mt.js
@@ -99,11 +99,11 @@ function doRemoveItems (f, singular, plural, nameRestrict, args) {
         }
     }
     if(toRemove == 'author') {
-        singularMessage = 'Deleting an author is an irrevocable action which creates orphans of the author\'s entries.  If you wish to retire an author or remove their access to the system, removing all of their permissions is the recommended course of action.  Are you sure you want to delete this author?';
-        pluralMessage = 'Deleting an author is an irrevocable action which creates orphans of the author\'s entries.  If you wish to retire an author or remove their access to the system, removing all of their permissions is the recommended course of action.  Are you sure you want to delete the [_1] selected authors?';
+        singularMessage = trans('Deleting an author is an irrevocable action which creates orphans of the author\'s entries.  If you wish to retire an author or remove their access to the system, removing all of their permissions is the recommended course of action.  Are you sure you want to delete this author?');
+        pluralMessage = trans('Deleting an author is an irrevocable action which creates orphans of the author\'s entries.  If you wish to retire an author or remove their access to the system, removing all of their permissions is the recommended course of action.  Are you sure you want to delete the [_1] selected authors?');
     } else {
-        singularMessage = 'Are you sure you want to delete this [_1]?';
-        pluralMessage = 'Are you sure you want to delete the [_1] selected [_2]?';
+        singularMessage = trans('Are you sure you want to delete this [_1]?');
+        pluralMessage = trans('Are you sure you want to delete the [_1] selected [_2]?');
     } 
 
     if (confirm(count == 1 ? trans(singularMessage, singular) : trans(pluralMessage, count, plural))) {
