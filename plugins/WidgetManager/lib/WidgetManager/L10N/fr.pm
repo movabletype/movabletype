262f27130f81be82165e3765246969fbc96ebb85
Jay Allen <info@endevver.com>
2006-06-22 20:40:23 +0000

    r1676@mt-master (orig r32089):  mschenk | 2006-06-22 13:39:06 -0700  Adding translation files for fr and nl to WidgetManager

---
+++ plugins/WidgetManager/lib/WidgetManager/L10N/fr.pm
@@ -0,0 +1,24 @@
+# Plugin Gestionnaires de Widget pour Movable Type
+# Auteur: Byrne Reese, Six Apart (http://www.sixapart.com)
+# Propos� sous License Artistique
+#
+package WidgetManager::L10N::fr;
+
+use strict;
+use base 'WidgetManager::L10N::en_us';
+use vars qw( %Lexicon );
+
+## The following is the translation table.
+
+%Lexicon = (
+    'Technorati Search' => 'Recherche Technorati',
+    'Calendar' => 'Calendrier',
+    'Category list (nested)' => 'Liste de Cat�gories',
+    'Creative Commons' => 'Creative Commons',
+    'Monthly archive list' => 'Liste des archives mensuelles',
+    'Recent posts' => 'Notes r�centes',
+    'Search form' => 'Formulaire de recherche',
+    'Subscribe to feed' => 'S\'abonner au flux',
+);
+1;
+
---
+++ plugins/WidgetManager/lib/WidgetManager/L10N/nl.pm
@@ -0,0 +1,24 @@
+# WidgetManager plugin for Movable Type
+# Author: Byrne Reese, Six Apart (http://www.sixapart.com)
+# Released under the Artistic License
+#
+package WidgetManager::L10N::nl;
+
+use strict;
+use base 'WidgetManager::L10N::en_us';
+use vars qw( %Lexicon );
+
+## The following is the translation table.
+
+%Lexicon = (
+    'Technorati Search' => 'Technorati zoekformulier',
+    'Calendar' => 'Kalender',
+    'Category list (nested)' => 'Categorielijst (genest)',
+    'Creative Commons' => 'Creative Commons',
+    'Monthly archive list' => 'Lijst maandelijkse archieven',
+    'Recent posts' => 'Recente berichten',
+    'Search form' => 'Zoekformulier',
+    'Subscribe to feed' => 'Inschrijven op feed',
+);
+1;
+
