ed8d0ae9fc46a69948aec531de14c46127c0c321
Jay Allen <info@endevver.com>
2006-06-24 00:10:04 +0000

    r1728@mt-master (orig r32212):  mschenk | 2006-06-23 17:02:51 -0700  First Spanish translations for WidgetManager

---
+++ plugins/WidgetManager/lib/WidgetManager/L10N/es.pm
@@ -0,0 +1,26 @@
+ # WidgetManager plugin for Movable Type
+# Author: Byrne Reese, Six Apart (http://www.sixapart.com)
+# Released under the Artistic License
+#
+package WidgetManager::L10N::es;
+
+use strict;
+use base 'WidgetManager::L10N::en_us';
+use vars qw( %Lexicon );
+
+## The following is the translation table.
+
+%Lexicon = (
+    'Technorati Search' => 'Búsquedas en Technorati',
+    'Calendar' => 'Calendario',
+    'Category list (nested)' => 'Lista de categorías (anidada)',
+    'Creative Commons' => 'Creative Commons',
+    'Monthly archive list' => 'Lista de archivos mensuales',
+    'Recent posts' => 'Entradas recientes',
+    'Search form' => 'Formulario de búsqueda',
+    'Subscribe to feed' => 'Suscribirse a la fuente',
+    'Recent comments' => 'Comentarios recientes',
+    'Tag cloud (sidebar)' => 'Nube de etiquetas (barra lateral)',
+);
+1;
+
