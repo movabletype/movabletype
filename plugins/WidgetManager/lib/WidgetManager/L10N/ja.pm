aecf6826fa4e6b3468c2e7242aa8c1e21fcbc976
Jay Allen <info@endevver.com>
2006-06-27 10:30:08 +0000

    r1890@mt-master (orig r32450):  fyoshimatsu | 2006-06-27 03:24:27 -0700  yet another japanese localization

--- plugins/WidgetManager/lib/WidgetManager/L10N/ja.pm
+++ plugins/WidgetManager/lib/WidgetManager/L10N/ja.pm
@@ -64,6 +64,7 @@ use vars qw( %Lexicon );
     'Select a Month...' => '月を選択',
     "Can't duplicate the existing '[_1]' Widget Manager. Please go back and enter a unique name." => 'Widget Manager [_1] が重複しています。戻って別の名前を付けてください。',
     'You already have a widget manager named [_1]. Please use a unique name for this widget manager.' => '[_1] という名前のWidget Managerがすでに存在します。別の名前を付けてください。',
+    'Tag cloud (sidebar)' => 'タグ クラウド (サイドバー)',
 );
 
 1;
