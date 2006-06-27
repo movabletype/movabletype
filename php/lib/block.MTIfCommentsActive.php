cf82562b9b90a7f7b004427358fa9a9a74654397
Jay Allen <info@endevver.com>
2006-06-27 23:40:07 +0000

    r1944@mt-master (orig r32550):  bchoate | 2006-06-27 16:33:10 -0700  Ported logic from static publishing for IfCommentsActive. BugId: 31269

--- php/lib/block.MTIfCommentsActive.php
+++ php/lib/block.MTIfCommentsActive.php
@@ -3,16 +3,17 @@ function smarty_block_MTIfCommentsActive($args, $content, &$ctx, &$repeat) {
     # status: complete
     if (!isset($content)) {
         $blog = $ctx->stash('blog');
-        $blog_active = ($blog['blog_allow_unreg_comments']
-	                || ($blog['blog_allow_reg_comments']
-			    && $blog['blog_remote_auth_token']))
-	               && $ctx->mt->config['AllowComments'];
+        $blog_accepts_comments = ($blog['blog_allow_reg_comments'] &&
+            $blog['blog_remote_auth_token']) ||
+            $blog['blog_allow_unreg_comments'];
         $entry = $ctx->stash('entry');
         if ($entry) {
-            $active = $ctx->mt->db->entry_comment_count($entry['entry_id']) > 0;
-            $active or $active = $entry['entry_allow_comments'];
+            if ($blog_accepts_comments && $entry['entry_allow_comments'] && $ctx->mt->config['AllowComments'])
+                $active = 1;
+            if ($ctx->mt->db->entry_comment_count($entry['entry_id']))
+                $active = 1;
         } else {
-            $active = $blog_active;
+            $active = $blog_accepts_comments && $ctx->mt->config['AllowComments'];
         }
         return $ctx->_hdlr_if($args, $content, $ctx, $repeat, $active);
     } else {
