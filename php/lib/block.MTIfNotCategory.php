bad2ebe03ff12546e3b513a84c75111a9abb1192
Brad Choate <bchoate@sixapart.com>
2006-07-15 20:16:01 +0000

    Removed; BugId: 32694

--- php/lib/block.MTIfNotCategory.php
+++
@@ -1,6 +0,0 @@
-<?php
-function smarty_block_MTIfNotCategory($args, $content, &$ctx, &$repeat) {
-    require_once("block.MTIfCategory.php");
-    return smarty_block_MTIfCategory($args, $context, $ctx, $repeat);
-}
-?>
