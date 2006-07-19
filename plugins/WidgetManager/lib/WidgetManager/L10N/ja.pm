a26e13dbd49b62126d9da73596d5256de32a0de3
Fumiaki Yoshimatsu <fumiakiy@gmail.com>
2006-07-19 01:03:47 +0000

    Added phrase in L10N file for added phrase. BugID: 32747

--- plugins/WidgetManager/lib/WidgetManager/L10N/ja.pm
+++ plugins/WidgetManager/lib/WidgetManager/L10N/ja.pm
@@ -66,6 +66,7 @@ use vars qw( %Lexicon );
     'You already have a widget manager named [_1]. Please use a unique name for this widget manager.' => '[_1] という名前のWidget Managerがすでに存在します。別の名前を付けてください。',
     'Tag cloud (sidebar)' => 'タグ クラウド (サイドバー)',
     'Tag cloud' => 'タグ クラウド',
+    'Powered by' => 'Powered by',
 );
 
 1;
