1c7b838f0ffe300df2912545c4e625c888df6e03
David Davis <xantus@xantus.org>
2008-01-14 23:05:29 +0000

    Add a class to category-field when the category selector is active.  BugzID: 65213

--- mt-static/mt.js
+++ mt-static/mt.js
@@ -2547,6 +2547,7 @@ MT.App.CategorySelector = new Class( Component, {
            if ( closeEl ) 
                DOM.removeClassName( closeEl, "hidden" );
         }
+        DOM.addClassName( "cateogry-field", "selector-active" );
         DOM.removeClassName( this.element, "hidden" );
         this.redraw();
     },
@@ -2566,6 +2567,7 @@ MT.App.CategorySelector = new Class( Component, {
         if ( el )
             DOM.addClassName( el, "hidden" );
         DOM.addClassName( this.element, "hidden" );
+        DOM.removeClassName( "cateogry-field", "selector-active" );
         if ( this.openingEl )
             DOM.removeClassName( this.openingEl, "hidden" );
     },
