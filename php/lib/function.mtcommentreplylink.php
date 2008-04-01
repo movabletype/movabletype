f180cc79e9a91ef76da8f9a3d0897bb5b3f9a768
Brad Choate <bchoate@sixapart.com>
2008-04-01 22:55:09 +0000

    Created php version of MTCommentReplyLink. BugId:74319

---
+++ php/lib/function.mtcommentreplylink.php
@@ -0,0 +1,24 @@
+<?php
+# Movable Type (r) Open Source (C) 2001-2008 Six Apart, Ltd.
+# This program is distributed under the terms of the
+# GNU General Public License, version 2.
+#
+# $Id$
+
+function smarty_function_mtcommentreplylink($args, &$ctx) {
+    $comment = $ctx->stash('comment');
+    if (!$comment) return '';
+
+    global $mt;
+    $label = $args['label'];
+    $label or $label = $args['text'];
+    $label or $label = $mt->translate("Reply");
+
+    $comment_author = $comment['comment_author'];
+    require_once("MTUtil.php");
+    $comment_author = encode_js($comment_author);
+
+    return sprintf("<a title\"%s\" href=\"javascript:void(0);\" onclick=\"replyComment(%d, '%s')\">%s</a>",
+        $label, $comment['comment_id'], $comment_author, $label);
+}
+?>
