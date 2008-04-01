ade053ea4eb30779fbc6993e27942f8e053bf127
Brad Choate <bchoate@sixapart.com>
2008-04-01 16:55:56 +0000

    Added copyright block.

--- lib/MT/Touch.pm
+++ lib/MT/Touch.pm
@@ -1,3 +1,9 @@
+# Movable Type (r) Open Source (C) 2001-2008 Six Apart, Ltd.
+# This program is distributed under the terms of the
+# GNU General Public License, version 2.
+#
+# $Id$
+
 package MT::Touch;
 
 use strict;
