87faed210922dd3c6b2130472dc958e67a5262ff
Brad Choate <bchoate@sixapart.com>
2008-04-08 17:29:46 +0000

    Memoize mtlink tag.

--- php/lib/function.mtlink.php
+++ php/lib/function.mtlink.php
@@ -8,13 +8,20 @@
 function smarty_function_mtlink($args, &$ctx) {
     // status: incomplete
     // parameters: template, entry_id
+    static $_template_links = array();
     if (isset($args['template'])) {
         $name = $args['template'];
-        $tmpl = $ctx->mt->db->load_index_template($ctx, $name);
-        $blog = $ctx->stash('blog');
-        $site_url = $blog['blog_site_url'];
-        if (!preg_match('!/$!', $site_url)) $site_url .= '/';
-        $link = $site_url . $tmpl['template_outfile'];
+        $cache_key = $ctx->__stash['blog_id'] . ';' . $name;
+        if (isset($_template_links[$cache_key])) {
+            $link = $_template_links[$cache_key];
+        } else {
+            $tmpl = $ctx->mt->db->load_index_template($ctx, $name);
+            $blog = $ctx->stash('blog');
+            $site_url = $blog['blog_site_url'];
+            if (!preg_match('!/$!', $site_url)) $site_url .= '/';
+            $link = $site_url . $tmpl['template_outfile'];
+            $_template_links[$cache_key] = $link;
+        }
         if (!$args['with_index']) {
             $link = _strip_index($link, $blog);
         }
@@ -38,4 +45,3 @@ function smarty_function_mtlink($args, &$ctx) {
     }
     return '';
 }
-?>
