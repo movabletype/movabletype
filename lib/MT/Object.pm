66e97121501f1887a4d12cf46733a185316b708b
Brad Choate <bchoate@sixapart.com>
2008-04-17 01:09:02 +0000

    Decreased the type column to 75 bytes due to UTF8 index constraints.

--- lib/MT/Object.pm
+++ lib/MT/Object.pm
@@ -465,7 +465,7 @@ sub meta_args {
         key         => $class->datasource,
         column_defs => {
             $id_field         => 'integer not null',
-            type              => 'string(100) not null',
+            type              => 'string(75) not null',
             vchar             => 'string(255)',
             vchar_indexed     => 'string(255)',
             vdatetime         => 'datetime',
