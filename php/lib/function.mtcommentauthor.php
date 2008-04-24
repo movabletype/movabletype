ad85ae45ff44c68949b33b74dcf75d345e4ee37d
Brad Choate <bchoate@sixapart.com>
2008-04-24 21:15:53 +0000

    Display auth'd commenter name for comment author tag when comment was posted by auth'd user. BugId:79475

--- php/lib/function.mtcommentauthor.php
+++ php/lib/function.mtcommentauthor.php
@@ -7,14 +7,16 @@
 
 function smarty_function_mtcommentauthor($args, &$ctx) {
     $c = $ctx->stash('comment');
-    if (!$c) {
+    if (!$c)
         return $ctx->error("No comment available");
-    }
     $a = isset($c['comment_author']) ? $c['comment_author'] : '';
-    if (isset($args['default'])) {
-        $a or $a = $args['default'];
+    if ($c['comment_commenter_id']) {
+        $commenter = $ctx->stash('commenter');
+        if ($commenter)
+            $a = $commenter['author_nickname'];
     }
+    if (isset($args['default']))
+        $a or $a = $args['default'];
     $a or $a = '';
     return strip_tags($a);
 }
-?>
