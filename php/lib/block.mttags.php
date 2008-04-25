ab9901bf229490171a4f6ac486114693a2bcdbbc
Brad Choate <bchoate@sixapart.com>
2008-04-25 03:51:54 +0000

    Support 'top' attribute for MTTags for dynamic publishing. BugId:68999

--- php/lib/block.mttags.php
+++ php/lib/block.mttags.php
@@ -14,6 +14,12 @@ function smarty_block_mttags($args, $content, &$ctx, &$repeat) {
         $ctx->stash('blog_ids', $args['blog_ids']);
         $blog_id = $ctx->stash('blog_id');
         $args['blog_id'] = $ctx->stash('blog_id');
+        if (isset($args['top'])) {
+            $post_sort_by = $args['sort_by'];
+            $post_sort_order = $args['sort_order'];
+            $args['sort_by'] = 'rank';
+            $args['sort_order'] = 'descend';
+        }
         if (isset($args['sort_by'])) {
             $s = $args['sort_by'];
             if (($s == 'rank') || ($s == 'count')) { // Aliased
@@ -47,6 +53,30 @@ function smarty_block_mttags($args, $content, &$ctx, &$repeat) {
             }
             if (isset($args['limit'])) {
                 $tags = array_slice($tags, 0, $args['limit']);
+            }
+
+            // Handle ordering based on 'top' attribute
+            // implies sorting by rank/descend and limit by # requested
+            // then, resort based on attributes or sane defaults
+            if (isset($args['top'])) {
+                $tags = array_slice($tags, 0, $args['top']);
+                // now, resort by original sort order
+                $post_sort_by or $post_sort_by = 'name';
+                if ($post_sort_by == 'name') {
+                    $post_sort_order or $post_sort_order = 'ascend';
+                    require_once("MTUtil.php");
+                    usort($tags, 'tagarray_name_sort');
+                    if ($post_sort_order && ($post_sort_order == 'descend')) {
+                        $tags = array_reverse($tags);
+                    }
+                } elseif (($post_sort_by == 'rank') || ($post_sort_by == 'count')) {
+                    $post_sort_order or $post_sort_order = 'descend';
+                    // we're already sorted by rank; just check if
+                    // order is not descending
+                    if ($post_sort_order != 'descend') {
+                        $tags = array_reverse($tags);
+                    }
+                }
             }
         }
         $ctx->stash('tag_min_count', $min);
@@ -82,4 +112,3 @@ function smarty_block_mttags($args, $content, &$ctx, &$repeat) {
     }
     return $content;
 }
-?>
