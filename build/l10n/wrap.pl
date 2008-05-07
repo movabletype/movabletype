7d81c0ffe6be5a2c3c0d9b30c91b4dd385bcdb66
Fumiaki Yoshimatsu <fumiakiy@gmail.com>
2008-05-07 01:29:28 +0000

    Some of the translation related scripts have been distributed in the wrong license.  BugId:79642

--- build/l10n/wrap.pl
+++ build/l10n/wrap.pl
@@ -12,8 +12,10 @@ BEGIN {
     my $LANG = $ARGV[0];
     shift @ARGV unless (-e $LANG);
     print <<EOF;
-# Copyright 2003-2008 Six Apart. This code cannot be redistributed without
-# permission from www.sixapart.com.
+# Movable Type (r) Open Source (C) 2005-2008 Six Apart, Ltd.
+# This program is distributed under the terms of the
+# GNU General Public License, version 2.
+#
 #
 # \$Id:\$
 
