4141dd3d39d36ba28962f79ea9e3937d41c13401
Atsushi Uno <auno@sixapart.com>
2008-05-14 09:26:21 +0000

    Fixed misspelling. BugzID:79735

--- php/lib/function.mtcommentlink.php
+++ php/lib/function.mtcommentlink.php
@@ -7,7 +7,7 @@
 
 function smarty_function_mtcommentlink($args, &$ctx) {
     $args['no_anchor'] = 1;
-    $entry_link = $ctx->tag('EntryPermaink', $args);
+    $entry_link = $ctx->tag('EntryPermalink', $args);
     $entry_link .= '#comment-' . $ctx['comment_id'];
     return $link;
 }
