b5c229e86a3b3a4e8e4ca49e10d100313200faf7
Yuji Takayama <ytakayama@sixapart.com>
2008-08-29 06:01:59 +0000

    Fixed BugId:81384 * Applied patch

--- php/lib/block.mtcommentparent.php
+++ php/lib/block.mtcommentparent.php
@@ -6,7 +6,7 @@
 # $Id$
 
 function smarty_block_mtcommentparent($args, $content, &$ctx, &$repeat) {
-    $localvars = array('comment');
+    $localvars = array('comment', 'commenter', 'current_timestamp');
     if (!isset($content)) {
         $comment = $ctx->stash('comment');
         if (!$comment) { $repeat = false; return '';}
@@ -14,7 +14,22 @@ function smarty_block_mtcommentparent($args, $content, &$ctx, &$repeat) {
         $parent = $ctx->mt->db->fetch_comment_parent($args);
         if (!$parent) { $repeat = false; return ''; }
         $ctx->localize($localvars);
-        $ctx->stash('comment', $parent[0]);
+        $parent_comment = $parent[0];
+        $ctx->stash('comment', $parent_comment);
+        $ctx->stash('current_timestamp', $parent_comment['comment_created_on']);
+        if ($parent_comment['comment_commenter_id']) {
+            $commenter = $ctx->mt->db->fetch_author($parent_comment['comment_commenter_id']);
+            if (empty($commenter)) {
+                $ctx->__stash['commenter'] = null;
+            } else {
+                $permission = $ctx->mt->db->fetch_permission(array('blog_id' => $parent_comment['comment_blog_id'], 'id' => $parent_comment['comment_commenter_id']));
+                if (!empty($permission))
+                    $commenter = array_merge($commenter, $permission[0]);
+                $ctx->stash('commenter', $commenter);
+            }
+        } else {
+            $ctx->__stash['commenter'] = null;
+        }
         $counter = 0;
     } else {
         $ctx->restore($localvars);
