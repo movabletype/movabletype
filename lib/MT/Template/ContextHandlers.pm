bad74b182471411f7ee8df82789d5ffae830d782
Brad Choate <bchoate@sixapart.com>
2008-09-16 18:19:13 +0000

    Prevent loading ObjectTags for an empty list of tag_ids. BugId:55719

--- lib/MT/Template/ContextHandlers.pm
+++ lib/MT/Template/ContextHandlers.pm
@@ -7972,7 +7972,7 @@ sub _hdlr_entries {
                     fetchonly => ['tag_id'],
                     no_triggers => 1
                 };
-                my @ot_ids = MT::ObjectTag->load($terms, $args);
+                my @ot_ids = MT::ObjectTag->load($terms, $args) if @tag_ids;
                 $map{$_->tag_id} = 1 for @ot_ids;
                 \%map;
             };
@@ -15953,7 +15953,7 @@ sub _hdlr_assets {
                     fetchonly => ['tag_id'],
                     no_triggers => 1,
                 };
-                my @ot_ids = MT::ObjectTag->load( $terms, $args );
+                my @ot_ids = MT::ObjectTag->load( $terms, $args ) if @tag_ids;
                 my %map;
                 $map{ $_->tag_id } = 1 for @ot_ids;
                 \%map;
