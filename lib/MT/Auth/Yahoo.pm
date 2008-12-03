3f98d4ae5610d92404a578b188ea2766a49b0332
Fumiaki Yoshimatsu <fumiakiy@gmail.com>
2008-12-03 02:55:08 +0000

    Refactored Yahoo auth module a bit.

--- lib/MT/Auth/Yahoo.pm
+++ lib/MT/Auth/Yahoo.pm
@@ -9,18 +9,15 @@ package MT::Auth::Yahoo;
 use strict;
 use base qw( MT::Auth::OpenID );
 
-sub url_for_userid {
-    my $class = shift;
-    my ($uid) = @_;
-    return "https://me.yahoo.com/$uid";
-};
-
 sub get_nickname {
     my $class = shift;
-    my ($vident, $blog_id) = @_;
+    my ($vident) = @_;
 
     my $url = $vident->url;
     if ( $url =~ m(^https?://me.yahoo.com/([^/]+)/?$) ) {
+        return $1;
+    }
+    elsif ( $url =~ m(^http://www.flickr.com/photos/(.+)$) ) {
         return $1;
     }
 
