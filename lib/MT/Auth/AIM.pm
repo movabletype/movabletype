35b3d1cd36e49d7ce429d293c0d3139d4df0e285
Brad Choate <bchoate@sixapart.com>
2008-12-04 00:36:11 +0000

    Adding AIM OpenID support. Thanks to Minh Nguyen for the contribution. BugId:85687

---
+++ lib/MT/Auth/AIM.pm
@@ -0,0 +1,28 @@
+# Movable Type (r) Open Source (C) 2001-2008 Six Apart, Ltd.
+# This program is distributed under the terms of the
+# GNU General Public License, version 2.
+#
+# $Id$
+
+package MT::Auth::AIM;
+
+use strict;
+use base qw( MT::Auth::OpenID );
+
+sub url_for_userid {
+    my $class = shift;
+    my ($uid) = @_;
+    return "http://openid.aol.com/$uid";
+};
+
+sub get_nickname {
+    my $class = shift;
+    my ($vident, $blog_id) = @_;
+    my $url = $vident->url;
+    if ( $url =~ m(^http://openid\.aol\.com\/([^/]+).*$) ) {
+        return $1;
+    }
+    return $class->SUPER::get_nickname(@_);
+}
+
+1;
