61fec639632dc93d29aeb3511de081142ddb01d5
Mark Paschal <mpaschal@sixapart.com>
2008-12-17 16:39:52 +0000

    spacing

--- lib/MT/ObjectDriver/DDL.pm
+++ lib/MT/ObjectDriver/DDL.pm
@@ -53,7 +53,7 @@ sub add_column {
     my $ddl = shift;
     my @stmts = $ddl->add_column_sql(@_);
     push @stmts, $ddl->index_column_sql(@_);
-    return @stmts; 
+    return @stmts;
 }
 
 sub alter_column {
@@ -347,7 +347,7 @@ sub drop_column_sql {
     return "ALTER TABLE $table_name DROP ${field_prefix}_$name";
 }
 
-sub column_sql { 
+sub column_sql {
     my $ddl = shift;
     my ($class, $name) = @_;
 
