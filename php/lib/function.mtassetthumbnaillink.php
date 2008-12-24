080bca46e363ec52b830b3eaefee7a08c7c3ccd9
Yuji Takayama <ytakayama@sixapart.com>
2008-12-24 10:33:45 +0000

    Fixed BugID:80800 * Added to support square modifier

--- php/lib/function.mtassetthumbnaillink.php
+++ php/lib/function.mtassetthumbnaillink.php
@@ -15,18 +15,7 @@ function smarty_function_mtassetthumbnaillink($args, &$ctx) {
 
     require_once('MTUtil.php');
 
-    $width = 0;
-    $height = 0;
-    $scale = 0;
-
-    if (isset($args['width']))
-        $width = $args['width'];
-    if (isset($args['height']))
-        $height = $args['height'];
-    if (isset($args['scale']))
-        $scale = $args['scale'];
-
-    list($thumb, $thumb_w, $thumb_h) = get_thumbnail_file($asset, $blog, $width, $height, $scale);
+    list($thumb, $thumb_w, $thumb_h) = get_thumbnail_file($asset, $blog, $args);
     if (empty($thumb)) {
         return '';
     }
--- php/lib/function.mtassetthumbnailurl.php
+++ php/lib/function.mtassetthumbnailurl.php
@@ -14,18 +14,7 @@ function smarty_function_mtassetthumbnailurl($args, &$ctx) {
 
     require_once('MTUtil.php');
 
-    $width = 0;
-    $height = 0;
-    $scale = 0;
-
-    if (isset($args['width']))
-        $width = $args['width'];
-    if (isset($args['height']))
-        $height = $args['height'];
-    if (isset($args['scale']))
-        $scale = $args['scale'];
-
-    list($thumb) = get_thumbnail_file($asset, $blog, $width, $height, $scale);
+    list($thumb) = get_thumbnail_file($asset, $blog, $args);
 
     return $thumb;
 }
