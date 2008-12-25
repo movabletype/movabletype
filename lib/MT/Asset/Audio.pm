45a89406519582fb88280e016e9eb4b54f6f7a98
Yuji Takayama <ytakayama@sixapart.com>
2008-12-25 05:42:27 +0000

    Fixed BugID:81973 * Added flac and m4a

--- lib/MT/Asset/Audio.pm
+++ lib/MT/Asset/Audio.pm
@@ -12,7 +12,7 @@ use base qw( MT::Asset );
 __PACKAGE__->install_properties( { class_type => 'audio', } );
 
 # List of supported file extensions (to aid the stock 'can_handle' method.)
-sub extensions { [qr/mp3/i, qr/ogg/i, qr/aiff?/i, qr/wav/i, qr/wma/i, qr/aac/i ] }
+sub extensions { [qr/mp3/i, qr/ogg/i, qr/aiff?/i, qr/wav/i, qr/wma/i, qr/aac/i, qr/flac/i, qr/m4a/i ] }
 
 sub class_label {
     MT->translate('Audio');
