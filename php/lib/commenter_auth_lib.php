b5b3d2f414da43a213ae87a429f7d4a500f4da75
Yuji Takayama <ytakayama@sixapart.com>
2009-01-20 09:07:49 +0000

    Fixed BugID:90593 * Added class for dynamic side   * Google, Yahoo, AIM, WordPress, YahooJapan, livedoor, Hatena

--- php/lib/commenter_auth_lib.php
+++ php/lib/commenter_auth_lib.php
@@ -14,6 +14,20 @@ $provider = new LiveJournalCommenterAuth();
 $_commenter_auths[$provider->get_key()] = $provider;
 $provider = new TypeKeyCommenterAuth();
 $_commenter_auths[$provider->get_key()] = $provider;
+$provider = new GoogleCommenterAuth();
+$_commenter_auths[$provider->get_key()] = $provider;
+$provider = new YahooCommenterAuth();
+$_commenter_auths[$provider->get_key()] = $provider;
+$provider = new AIMCommenterAuth();
+$_commenter_auths[$provider->get_key()] = $provider;
+$provider = new WordPressCommenterAuth();
+$_commenter_auths[$provider->get_key()] = $provider;
+$provider = new YahooJPCommenterAuth();
+$_commenter_auths[$provider->get_key()] = $provider;
+$provider = new LivedoorCommenterAuth();
+$_commenter_auths[$provider->get_key()] = $provider;
+$provider = new HatenaCommenterAuth();
+$_commenter_auths[$provider->get_key()] = $provider;
 
 function _auth_icon_url($static_path, $author) {
     $auth_type = $author["author_auth_type"];
@@ -103,6 +117,111 @@ class TypeKeyCommenterAuth extends BaseCommenterAuthProvider {
     }
     function get_logo_small() {
         return 'images/comment/typepad_logo.png';
+    }
+}
+
+class GoogleCommenterAuth extends BaseCommenterAuthProvider {
+    function get_key() {
+        return 'Google';
+    }
+    function get_label() {
+        return 'Google Commenter Authenticator';
+    }
+    function get_logo() {
+        return 'images/comment/google.png';
+    }
+    function get_logo_small() {
+        return 'images/comment/google_logo.png';
+    }
+}
+
+class YahooCommenterAuth extends BaseCommenterAuthProvider {
+    function get_key() {
+        return 'Yahoo';
+    }
+    function get_label() {
+        return 'Yahoo Commenter Authenticator';
+    }
+    function get_logo() {
+        return 'images/comment/yahoo.png';
+    }
+    function get_logo_small() {
+        return 'images/comment/favicon_yahoo.png';
+    }
+}
+
+class AIMCommenterAuth extends BaseCommenterAuthProvider {
+    function get_key() {
+        return 'AIM';
+    }
+    function get_label() {
+        return 'AIM Commenter Authenticator';
+    }
+    function get_logo() {
+        return 'images/comment/aim.png';
+    }
+    function get_logo_small() {
+        return 'images/comment/aim_logo.png';
+    }
+}
+
+class WordPressCommenterAuth extends BaseCommenterAuthProvider {
+    function get_key() {
+        return 'WordPress';
+    }
+    function get_label() {
+        return 'WordPress Commenter Authenticator';
+    }
+    function get_logo() {
+        return 'images/comment/wordpress.png';
+    }
+    function get_logo_small() {
+        return 'images/comment/wordpress_logo.png';
+    }
+}
+
+class YahooJPCommenterAuth extends BaseCommenterAuthProvider {
+    function get_key() {
+        return 'YahooJP';
+    }
+    function get_label() {
+        return 'Yahoo Japan Commenter Authenticator';
+    }
+    function get_logo() {
+        return 'images/comment/yahoo.png';
+    }
+    function get_logo_small() {
+        return 'images/comment/favicon_yahoo.png';
+    }
+}
+
+class LivedoorCommenterAuth extends BaseCommenterAuthProvider {
+    function get_key() {
+        return 'livedoor';
+    }
+    function get_label() {
+        return 'livedoor Commenter Authenticator';
+    }
+    function get_logo() {
+        return 'images/comment/signin_livedoor.png';
+    }
+    function get_logo_small() {
+        return 'images/comment/livedoor_logo.png';
+    }
+}
+
+class HatenaCommenterAuth extends BaseCommenterAuthProvider {
+    function get_key() {
+        return 'Hatena';
+    }
+    function get_label() {
+        return 'Hatena Commenter Authenticator';
+    }
+    function get_logo() {
+        return 'images/comment/signin_hatena.png';
+    }
+    function get_logo_small() {
+        return 'images/comment/hatena_logo.png';
     }
 }
 
