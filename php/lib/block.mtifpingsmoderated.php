b3744ba59b4c2bf25e4b72fe15dd668f827498d6
Fumiaki Yoshimatsu <fumiakiy@gmail.com>
2009-03-05 09:21:27 +0000

    Typo.  Thanks Kawasaki-san for the good catch!  BugId:99571

--- php/lib/block.mtifpingsmoderated.php
+++ php/lib/block.mtifpingsmoderated.php
@@ -9,7 +9,7 @@ function smarty_block_mtifpingsmoderated($args, $content, &$ctx, &$repeat) {
     # status: complete
     if (!isset($content)) {
         $blog = $ctx->stash('blog');
-        $moderated = ($blog['blog_moderate_pings'];
+        $moderated = ($blog['blog_moderate_pings']);
         return $ctx->_hdlr_if($args, $content, $ctx, $repeat, $moderated);
     } else {
         return $ctx->_hdlr_if($args, $content, $ctx, $repeat);
