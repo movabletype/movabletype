937cd0b8723505c3b556a2fca69f2c6201bd8c3e
Jared Marcotte <jmarcotte@sixapart.com>
2009-05-12 18:06:46 +0000

    Patch by Steve Cook: Changes MT::Author's remove_sessions behavior. Reduced to a simple MT::Session->remove call without the need to load and deserialize any rows.

--- lib/MT/Author.pm
+++ lib/MT/Author.pm
@@ -108,15 +108,10 @@ sub set_defaults {
 
 sub remove_sessions {
     my $auth = shift;
+    return if ( ! $auth or ! $auth->id );
     require MT::Session;
-    my $sess_iter = MT::Session->load_iter({ kind => 'US' });
-    my @sess;
-    while (my $sess = $sess_iter->()) {
-        my $id = $sess->get('author_id');
-        next unless $id == $auth->id;
-        push @sess, $sess;
-    }
-    $_->remove foreach @sess;
+    my $sess_iter = MT::Session->remove({ kind => 'US', name => $auth->id });
+    return 1;
 }
 
 sub set_password {
