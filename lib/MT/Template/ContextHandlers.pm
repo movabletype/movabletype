99d9c94a72885893b739bdaf975ddad737929d48
Paolo Lim <plim@sixapart.com>
2009-07-23 14:31:16 +0000

    Patch by Byrne Reese to allow mt:Else to work with mt:Assets.  BugId: 86618

--- lib/MT/Template/ContextHandlers.pm
+++ lib/MT/Template/ContextHandlers.pm
@@ -16538,6 +16538,9 @@ sub _hdlr_assets {
         $row_count = 0 if $row_count > $per_row;
         $i++;
     }
+    if (!@assets) {
+        return _hdlr_pass_tokens_else(@_);
+    }
 
     $res;
 }
