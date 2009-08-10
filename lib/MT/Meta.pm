54021da8397f02210c92e938bbe3214ace7cffa6
Kevin Shay <kshay@sixapart.com>
2009-08-10 16:02:51 +0000

    Fixing $Id$ placeholder

--- lib/MT/Meta.pm
+++ lib/MT/Meta.pm
@@ -2,7 +2,7 @@
 # This program is distributed under the terms of the
 # GNU General Public License, version 2.
 #
-# $Id: Meta.pm 71460 2008-01-18 18:01:06Z ykerherve $
+# $Id$
 
 package MT::Meta;
 
