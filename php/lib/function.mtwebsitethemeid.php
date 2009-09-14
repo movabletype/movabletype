f94b87fab537e2dba05d7dcd804e51fa155d2293
Akira Sawada <asawada@sixapart.com>
2009-09-14 05:57:54 +0000

    Added php tag for display blog/website themeID. bugzid:102375.

---
+++ php/lib/function.mtwebsitethemeid.php
@@ -0,0 +1,14 @@
+<?php
+# Movable Type (r) Open Source (C) 2001-2009 Six Apart, Ltd.
+# This program is distributed under the terms of the
+# GNU General Public License, version 2.
+#
+# $Id: function.mtwebsitelabel.php 4196 2009-09-04 07:46:50Z takayama $
+
+function smarty_function_mtwebsitethemeid($args, &$ctx) {
+    // status: complete
+    // parameters: none
+    require_once('function.mtblogthemeid.php');
+    return smarty_function_mtblogthemeid($args, $ctx);
+}
+?>
