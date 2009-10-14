a48f4c4cf7429a05b87e52662b99ee133ad48c36
Fumiaki Yoshimatsu <fumiakiy@gmail.com>
2009-10-14 01:01:05 +0000

    Updated version module to the latest version (0.7702).

--- extlib/version.pm
+++ extlib/version.pm
@@ -4,9 +4,10 @@ package version;
 use 5.005_04;
 use strict;
 
-use vars qw(@ISA $VERSION $CLASS *qv);
+use vars qw(@ISA $VERSION $CLASS *declare *qv);
 
-$VERSION = 0.76;
+$VERSION = '0.7702';
+$VERSION = eval $VERSION;
 
 $CLASS = 'version';
 
@@ -17,32 +18,83 @@ if ( $@ ) { # don't have the XS version installed
     push @ISA, "version::vpp";
     local $^W;
     *version::qv = \&version::vpp::qv;
+    *version::declare = \&version::vpp::declare;
+    *version::_VERSION = \&version::vpp::_VERSION;
     if ($] > 5.009001 && $] <= 5.010000) {
 	no strict 'refs';
 	*{'version::stringify'} = \*version::vpp::stringify;
 	*{'version::(""'} = \*version::vpp::stringify;
+	*{'version::new'} = \*version::vpp::new;
     }
 }
 else { # use XS module
     push @ISA, "version::vxs";
     local $^W;
+    *version::declare = \&version::vxs::declare;
     *version::qv = \&version::vxs::qv;
-    if ($] > 5.009001 && $] <= 5.010000) {
+    *version::_VERSION = \&version::vxs::_VERSION;
+    if ($] > 5.009001 && $] < 5.010000) {
 	no strict 'refs';
 	*{'version::stringify'} = \*version::vxs::stringify;
 	*{'version::(""'} = \*version::vxs::stringify;
     }
+    elsif ($] == 5.010000) {
+	no strict 'refs';
+	*{'version::stringify'} = \*version::vxs::stringify;
+	*{'version::(""'} = \*version::vxs::stringify;
+	*version::new = \&version::vxs::new;
+	*version::parse = \&version::vxs::parse;
+    }
+
 }
 
 # Preloaded methods go here.
 sub import {
-    my ($class) = shift;
-    my $callpkg = caller();
     no strict 'refs';
+    my ($class) = shift;
+
+    # Set up any derived class
+    unless ($class eq 'version') {
+	local $^W;
+	*{$class.'::declare'} =  \&version::declare;
+	*{$class.'::qv'} = \&version::qv;
+    }
+
+    my %args;
+    if (@_) { # any remaining terms are arguments
+	map { $args{$_} = 1 } @_
+    }
+    else { # no parameters at all on use line
+    	%args = 
+	(
+	    qv => 1,
+	    'UNIVERSAL::VERSION' => 1,
+	);
+    }
+
+    my $callpkg = caller();
     
-    *{$callpkg."::qv"} = 
-	    sub {return bless version::qv(shift), $class }
-	unless defined(&{"$callpkg\::qv"});
+    if (exists($args{declare})) {
+	*{$callpkg."::declare"} = 
+	    sub {return $class->declare(shift) }
+	  unless defined(&{$callpkg.'::declare'});
+    }
+
+    if (exists($args{qv})) {
+	*{$callpkg."::qv"} =
+	    sub {return $class->qv(shift) }
+	  unless defined(&{"$callpkg\::qv"});
+    }
+
+    if (exists($args{'UNIVERSAL::VERSION'})) {
+	local $^W;
+	*UNIVERSAL::VERSION 
+		= \&version::_VERSION;
+    }
+
+    if (exists($args{'VERSION'})) {
+	*{$callpkg."::VERSION"} = \&version::_VERSION;
+    }
 }
 
 1;
