6e3ba47cffd4da71497d780c4e87d01e25139049
Yuji Takayama <ytakayama@sixapart.com>
2009-11-09 07:02:56 +0000

    * Does not anything if object is empty. bugid:103014

--- php/lib/block.mtentries.php
+++ php/lib/block.mtentries.php
@@ -126,44 +126,45 @@ function smarty_block_mtentries($args, $content, &$ctx, &$repeat) {
     if ($lastn ? ($counter < $lastn) : ($counter < count($entries))) {
         $blog_id = $ctx->stash('blog_id');
         $entry = $entries[$counter];
-
-        if ($blog_id != $entry->entry_blog_id) {
-            $blog_id = $entry->entry_blog_id;
-            $ctx->stash('blog_id', $blog_id);
-            $ctx->stash('blog', $entry->blog());
+        if (!empty($entry)) {
+            if ($blog_id != $entry->entry_blog_id) {
+                $blog_id = $entry->entry_blog_id;
+                $ctx->stash('blog_id', $blog_id);
+                $ctx->stash('blog', $entry->blog());
+            }
+            if ($counter > 0) {
+                $last_entry_created_on = $entries[$counter-1]->entry_authored_on;
+            } else {
+                $last_entry_created_on = '';
+            }
+            if ($counter < count($entries)-1) {
+                $next_entry_created_on = $entries[$counter+1]->entry_authored_on;
+            } else {
+                $next_entry_created_on = '';
+            }
+            $ctx->stash('DateHeader', !(substr($entry->entry_authored_on, 0, 8) == substr($last_entry_created_on, 0, 8)));
+            $ctx->stash('DateFooter', (substr($entry->entry_authored_on, 0, 8) != substr($next_entry_created_on, 0, 8)));
+            $ctx->stash('entry', $entry);
+            $ctx->stash('current_timestamp', $entry->entry_authored_on);
+            $ctx->stash('current_timestamp_end', null);
+            $ctx->stash('modification_timestamp', $entry->entry_modified_on);
+            $ctx->stash('_entries_counter', $counter + 1);
+            $_REQUEST['entry_ids_published'][$entry->entry_id] = 1;
+            $glue = $ctx->stash('_entries_glue');
+            if (isset($glue) && !empty($content)) {
+                if ($out)
+                    $content = $glue . $content;
+                else
+                    $ctx->stash('__out', true);
+            }
+            $count = $counter + 1;
+            $ctx->__stash['vars']['__counter__'] = $count;
+            $ctx->__stash['vars']['__odd__'] = ($count % 2) == 1;
+            $ctx->__stash['vars']['__even__'] = ($count % 2) == 0;
+            $ctx->__stash['vars']['__first__'] = $count == 1;
+            $ctx->__stash['vars']['__last__'] = ($count == count($entries));
+            $repeat = true;
         }
-        if ($counter > 0) {
-            $last_entry_created_on = $entries[$counter-1]->entry_authored_on;
-        } else {
-            $last_entry_created_on = '';
-        }
-        if ($counter < count($entries)-1) {
-            $next_entry_created_on = $entries[$counter+1]->entry_authored_on;
-        } else {
-            $next_entry_created_on = '';
-        }
-        $ctx->stash('DateHeader', !(substr($entry->entry_authored_on, 0, 8) == substr($last_entry_created_on, 0, 8)));
-        $ctx->stash('DateFooter', (substr($entry->entry_authored_on, 0, 8) != substr($next_entry_created_on, 0, 8)));
-        $ctx->stash('entry', $entry);
-        $ctx->stash('current_timestamp', $entry->entry_authored_on);
-        $ctx->stash('current_timestamp_end', null);
-        $ctx->stash('modification_timestamp', $entry->entry_modified_on);
-        $ctx->stash('_entries_counter', $counter + 1);
-        $_REQUEST['entry_ids_published'][$entry->entry_id] = 1;
-        $glue = $ctx->stash('_entries_glue');
-        if (isset($glue) && !empty($content)) {
-            if ($out)
-                $content = $glue . $content;
-            else
-                $ctx->stash('__out', true);
-        }
-        $count = $counter + 1;
-        $ctx->__stash['vars']['__counter__'] = $count;
-        $ctx->__stash['vars']['__odd__'] = ($count % 2) == 1;
-        $ctx->__stash['vars']['__even__'] = ($count % 2) == 0;
-        $ctx->__stash['vars']['__first__'] = $count == 1;
-        $ctx->__stash['vars']['__last__'] = ($count == count($entries));
-        $repeat = true;
     } else {
         $glue = $ctx->stash('_entries_glue');
         if (isset($glue) && $out && !empty($content))
