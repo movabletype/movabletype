fb12f1b66f8583f8c6312f89cbce7416afa42e89
Fumiaki Yoshimatsu <fumiakiy@gmail.com>
2009-11-24 00:49:55 +0000

    Oops.

--- lib/MT.pm
+++ lib/MT.pm
@@ -1,4 +1,4 @@
-c Movable Type (r) Open Source (C) 2001-2009 Six Apart, Ltd.
+# Movable Type (r) Open Source (C) 2001-2009 Six Apart, Ltd.
 # This program is distributed under the terms of the
 # GNU General Public License, version 2.
 #
