694851849d847e52f7e3ee9e62b41b3861cf1a22
Yuji Takayama <ytakayama@sixapart.com>
2010-08-09 13:14:27 +0000

    * Added input validation for Blog/Archive path. bugid:104270, 104266

--- mt-static/jquery/jquery.mt.js
+++ mt-static/jquery/jquery.mt.js
@@ -243,12 +243,14 @@ $.mtUseAbsolute = function() {
             $obj.find('.relative-site_path-hint').hide();
             $obj.find('.absolute-site_path-hint').show();
             $obj.find('.absolute-site_path').find(':input').removeClass('ignore-validate');
+            $obj.find('.relative-site_path').find(':input').addClass('ignore-validate');
         } else {
             var $obj = $(this).removeAttr('checked').parents('.field-content');
             $obj.find('.relative-site_path').show();
             $obj.find('.absolute-site_path').hide();
             $obj.find('.relative-site_path-hint').show();
             $obj.find('.absolute-site_path-hint').hide();
+            $obj.find('.relative-site_path').find(':input').removeClass('ignore-validate');
             $obj.find('.absolute-site_path').find(':input').addClass('ignore-validate');
         }
     });
