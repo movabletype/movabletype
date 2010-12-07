08040dbae1b3ded4aaa807ec6df85e8eb972f56b
Yuji Takayama <ytakayama@sixapart.com>
2010-12-07 06:12:52 +0000

    * removed debug code.

--- lib/MT/Comment.pm
+++ lib/MT/Comment.pm
@@ -798,10 +798,6 @@ sub to_hash {
 
 sub visible {
     my $comment = shift;
-    if ( @_ ) {
-        use Carp;
-        Carp::confess "Why?";
-    }
     return $comment->SUPER::visible unless @_;
 
     ## Note transitions in visibility in the object, so that
