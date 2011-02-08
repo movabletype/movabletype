0949aa8add9e86c914e0d75705263fe169a9703c
Akira Sawada <asawada@sixapart.com>
2011-02-08 17:06:56 +0900

    Added sort logic of 'Target' column of TBPing list screen. bugzid:105203.

--- lib/MT/TBPing.pm
+++ lib/MT/TBPing.pm
@@ -196,6 +196,40 @@ sub list_props {
                 }
                 @res;
             },
+            sort => sub {
+                my $prop = shift;
+                my ( $terms, $args ) = @_;
+                $args->{joins} ||= [];
+                push @{ $args->{joins} },
+                    MT->model('trackback')->join_on(
+                        undef, undef,
+                        {
+                            condition => { id => \'= tbping_tb_id' },
+                            type => 'Left',
+                        },
+
+                    ),
+                    MT->model('entry')->join_on(
+                        undef, undef,
+                        {   sort      => 'title',
+                            condition => { id => \'= trackback_entry_id', },
+                            direction => ( $args->{direction} || 'ascend' ),
+                            type      => 'left',
+                        },
+                    ),
+                    MT->model('category')->join_on(
+                        undef, undef,
+                        {   sort      => 'label',
+                            condition => { id => \'= trackback_category_id', },
+                            direction => ( $args->{direction} || 'ascend' ),
+                            type      => 'left',
+                        },
+                    );
+
+
+                $args->{sort} = [];
+                return;
+            },
         },
         created_on => {
             base    => '__virtual.created_on',
