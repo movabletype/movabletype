ef90736188df3817aea96776a251071d9d85101e
Yuji Takayama <taka.swordbreaker@gmail.com>
2011-04-11 12:50:57 +0900

    Do not set blog_id argument. bugid:104232

--- php/lib/function.mtblogcommentcount.php
+++ php/lib/function.mtblogcommentcount.php
@@ -6,7 +6,18 @@
 # $Id$
 
 function smarty_function_mtblogcommentcount($args, &$ctx) {
-    $args['blog_id'] = $ctx->stash('blog_id');
+    if (!(
+        isset($args['include_blogs']) ||
+        isset($args['include_websites']) ||
+        isset($args['exclude_blogs']) ||
+        isset($args['exclude_websites']) ||
+        isset($args['blog_ids']) ||
+        isset($args['site_ids']) ||
+        isset($args['blog_id'])
+    )) {
+        # If blogid modifier was not found then should use current blogid
+        $args['blog_id'] = $ctx->stash('blog_id');
+    }
     $count = $ctx->mt->db()->blog_comment_count($args);
     return $ctx->count_format($count, $args);
 }
