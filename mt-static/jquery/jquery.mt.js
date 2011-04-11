85e6fc2c3afb9c8941d056f99df594009de8e684
Akira Sawada <asawada@sixapart.com>
2011-04-11 11:58:45 +0900

    Implemented placeholder again, for old browsers. bugzid:106010.

--- mt-static/jquery/jquery.mt.js
+++ mt-static/jquery/jquery.mt.js
@@ -1120,4 +1120,51 @@ $('select').live('change', function () {
     $(this).mtValid({ focus: false });
 });
 
+/*
+ * mtPlaceholder
+ *
+ * Usage:
+ *   jQuery('[placeholder]').mtPlaceholder()
+ *
+ */
+
+$.fn.mtPlaceholder = function() {
+    if ( 'placeholder' in $('<input />').get(0) ) return this;
+    this.each( function () {
+        var that = this;
+        var $that = $(that);
+        var placeholder_text = $that.attr('placeholder');
+        var original_color = $that.css('color');
+        if ( 1 > $that.val().length ) {
+            $that
+                .val(placeholder_text)
+                .css('color', 'GrayText')
+                .attr('data-showing-placeholder', 'showing');
+        }
+        $that
+            .focus( function () {
+                if ( $that.attr('data-showing-placeholder') ) {
+                    $that
+                        .val('')
+                        .css('color', original_color)
+                        .removeAttr('data-showing-placeholder');
+                }
+            })
+            .blur( function () {
+                if ( 1 > $that.val().length) {
+                    $that.val(placeholder_text)
+                        .css('color', 'GrayText')
+                        .attr('data-showing-placeholder', 'showing');
+                }
+            })
+            .parents('form').submit( function () {
+                if ( $that.attr('data-showing-placeholder') ) {
+                    $that.removeAttr('data-showing-placeholder');
+                    $that.val('');
+                }
+            });
+    });
+    return this;
+};
+
 })(jQuery);
