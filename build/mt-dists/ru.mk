ddaf805c68039df21366897d3656db1778f215da
Andrey Serebryakov <saahov@saahov.ru>
2011-05-25 21:03:33 +0400

    One file for build ru

---
+++ build/mt-dists/ru.mk
@@ -0,0 +1,14 @@
+BUILD_LANGUAGE = ru
+
+VERSION_SUFFIX = -ru
+PORTAL_URL = http://movable-type.ru/
+SUPPORT_URL = http://movable-type.ru/forums/
+NEWS_URL = http://movable-type.ru/
+NEWSBOX_URL = http://movable-type.ru/mtnewsbox.html
+LEARNINGNEWS_URL = http://learning.movabletype.org/newsbox.html
+CATEGORY_NAME_NODASH = 0
+DEFAULT_TIMEZONE = 3
+MAIL_ENCODING = UTF-8
+LOG_EXPORT_ENCODING = UTF-8
+EXPORT_ENCODING = UTF-8
+PUBLISH_CHARSET = UTF-8
