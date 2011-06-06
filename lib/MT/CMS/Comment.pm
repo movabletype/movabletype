5a65cc8baf047c617f607390130f8b599e6580cb
Akira Sawada <asawada@sixapart.com>
2011-06-06 12:47:26 +0900

    Permission check for modify commenter status. bugzid:106248.

--- lib/MT/CMS/Comment.pm
+++ lib/MT/CMS/Comment.pm
@@ -694,9 +694,20 @@ sub save_commenter_perm {
     my @ids     = $params ? @$params : $app->param('commenter_id');
     my $blog_id = $q->param('blog_id');
     my $author  = $app->user;
-
+    my %permissions;
     foreach my $id (@ids) {
         ( $id, $blog_id ) = @$id if ref $id eq 'ARRAY';
+        my $perm_blog_id = MT->config->SingleCommunity ? 0 : $blog_id;
+        if ( $perm_blog_id ) {
+            my $perm = $permissions{ $perm_blog_id } ||= $author->permissions($perm_blog_id);
+            if ( !$perm->can_manage_feedback && !$perm->can_administer ) {
+                return $app->errtrans( "Permission denied." );
+            }
+        }
+        else {
+            return $app->errtrans( "Permission denied." )
+                unless $author->is_superuser;
+        }
 
         my $cmntr = MT::Author->load($id)
           or return $app->errtrans( "No such commenter [_1].", $id );
