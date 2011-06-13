5af967e58fbc0eba645261f867d6c0bda817eeba
Shmuel Fomberg <owner@semuel.co.il>
2011-06-13 11:52:41 +0900

    MT::Util::encode_xml: now replace control characters with a single space, bugid:106214

--- lib/MT/Util.pm
+++ lib/MT/Util.pm
@@ -822,6 +822,7 @@ sub decode_url {
             # re-replace &amp;#nnnn => &#nnnn
             $str =~ s/&amp;((\#([0-9]+)|\#x([0-9a-fA-F]+)).*?);/&$1;/g;
         }
+        $str =~ tr/\0-\x1F\x7F/  /;
         $str;
     }
 
