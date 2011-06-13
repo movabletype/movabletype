dc563e0657284d3096506bf8dc91ba2e14ed3ce8
Yuji Takayama <taka.swordbreaker@gmail.com>
2011-06-13 14:34:54 +0900

    Changed version number.

--- lib/MT.pm
+++ lib/MT.pm
@@ -32,13 +32,13 @@ our $plugins_installed;
 BEGIN {
     $plugins_installed = 0;
 
-    ( $VERSION, $SCHEMA_VERSION ) = ( '5.11', '5.0024' );
+    ( $VERSION, $SCHEMA_VERSION ) = ( '5.12', '5.0024' );
     (   $PRODUCT_NAME, $PRODUCT_CODE, $PRODUCT_VERSION,
         $VERSION_ID,   $PORTAL_URL
         )
         = (
         '__PRODUCT_NAME__', 'MT',
-        '5.11',              '__PRODUCT_VERSION_ID__',
+        '5.12',              '__PRODUCT_VERSION_ID__',
         '__PORTAL_URL__'
         );
 
