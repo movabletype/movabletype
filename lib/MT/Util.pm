a126aa11c756368e77eaf10f2a524b555c6e8ff5
Yuji Takayama <taka.swordbreaker@gmail.com>
2011-06-17 19:16:14 +0900

    Added libxml_parser into the EXPORT.

--- lib/MT/Util.pm
+++ lib/MT/Util.pm
@@ -26,7 +26,7 @@ our @EXPORT_OK
     start_background_task launch_background_tasks substr_wref
     extract_urls extract_domain extract_domains is_valid_date
     epoch2ts ts2epoch escape_unicode unescape_unicode
-    sax_parser expat_parser trim ltrim rtrim asset_cleanup caturl multi_iter
+    sax_parser expat_parser libxml_parser trim ltrim rtrim asset_cleanup caturl multi_iter
     weaken log_time make_string_csv browser_language sanitize_embed
     extract_url_path break_up_text dir_separator deep_do deep_copy );
 
