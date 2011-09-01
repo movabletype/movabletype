59124cd79fc80815a740f317cb5a816447c9f988
Yuji Takayama <taka.swordbreaker@gmail.com>
2011-09-01 17:04:32 +0900

    Changed to check permission with object's blog:106364

--- lib/MT/CMS/Comment.pm
+++ lib/MT/CMS/Comment.pm
@@ -445,14 +445,13 @@ sub handle_junk {
         $app->validate_magic() or return;
     }
 
-    my $perm_checked = $app->can_do('handle_junk');
-
     foreach my $id (@ids) {
         next unless $id;
 
         my $obj = $class->load($id) or die "No $class $id";
         my $perms = $app->user->permissions($obj->blog_id)
             or return $app->permission_denied();
+        my $perm_checked = $perms->can_do('handle_junk');
         my $old_visible = $obj->visible || 0;
         unless ($perm_checked) {
             if ( $obj->isa('MT::TBPing') && $obj->parent->isa('MT::Entry') ) {
@@ -511,14 +510,14 @@ sub not_junk {
     my $class = $app->model($type);
     my %rebuild_set;
 
-    my $perm_checked = $app->can_do('handle_not_junk');
-
     foreach my $id (@ids) {
         next unless $id;
         my $obj = $class->load($id)
             or next;
         my $perms = $app->user->permissions( $obj->blog_id )
             or return $app->permission_denied();
+        my $perm_checked = $perms->can_do('handle_not_junk');
+
         unless ($perm_checked) {
             if ( $obj->isa('MT::TBPing') && $obj->parent->isa('MT::Entry') ) {
                 return $app->permission_denied()
