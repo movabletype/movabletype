4adfd1176bb8387d4dd74633989805b8413629e0
Yuji Takayama <taka.swordbreaker@gmail.com>
2011-10-08 22:57:57 +0900

    Changed phrase to Gecko/ instead of Firefox/. bugid:106570

--- mt-static/js/common/Editor/Iframe.js
+++ mt-static/js/common/Editor/Iframe.js
@@ -25,7 +25,7 @@ Editor.Iframe = new Class( Component, {
         this.window = this.element.contentWindow;
         this.document = this.element.contentDocument || this.element.contentWindow.document;
 
-        if( defined( this.document.body.contentEditable ) && navigator.userAgent.indexOf("Firefox/") == -1 )
+        if( defined( this.document.body.contentEditable ) && navigator.userAgent.indexOf("Gecko/") == -1 )
             this.document.body.contentEditable = true;
         else
             this.document.designMode = "on";
