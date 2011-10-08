54a6eb4521d7a5ff1f144bad866972b361d320da
Yuji Takayama <taka.swordbreaker@gmail.com>
2011-10-08 22:28:30 +0900

    If browser is Firefox then using designmode instead of contenteditable. bugid:106570

--- mt-static/js/common/Editor/Iframe.js
+++ mt-static/js/common/Editor/Iframe.js
@@ -24,8 +24,8 @@ Editor.Iframe = new Class( Component, {
         this.editor = editor;
         this.window = this.element.contentWindow;
         this.document = this.element.contentDocument || this.element.contentWindow.document;
-        
-        if( defined( this.document.body.contentEditable ) )
+
+        if( defined( this.document.body.contentEditable ) && navigator.userAgent.indexOf("Firefox/") == -1 )
             this.document.body.contentEditable = true;
         else
             this.document.designMode = "on";
