32669c2c97d8b9e5e1022c0a586f536980381469
Akira Sawada <asawada@sixapart.com>
2011-12-27 15:57:49 +0900

    Added one time token exchanging step in commenter sign-in flow. bugzid 107268.

--- lib/MT/Session.pm
+++ lib/MT/Session.pm
@@ -155,6 +155,10 @@ Active commenter sessions are held in SI sessions.
 The public key of the remote comment authentication service is held in
 the I<single> record of kind 'KY'.
 
+=item OT
+
+The one time token that to exchange with commenter session.
+
 =item AN
 
 "Nonce" values (used for certain authentication systems) are stored
@@ -216,7 +220,7 @@ Periodic Task indicates when a certain scheduled task is run the last time.
 =item CA
 
 CaptchA session stores the captcha token that is asked for a particular challenge.
-It expires after 10 minutes.
+It expiers after 10 minutes.
 
 =item TC
 
