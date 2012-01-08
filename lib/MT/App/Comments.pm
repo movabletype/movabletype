1b2116aee68d41e8b1cc83c1b6a5f7fdfba0ee3b
Yuji Takayama <taka.swordbreaker@gmail.com>
2012-01-08 19:10:51 +0900

    If commenter session was not fetched, do not check it. bugid:107284

--- lib/MT/App/Comments.pm
+++ lib/MT/App/Comments.pm
@@ -1484,7 +1484,7 @@ sub handle_sign_in {
                 }
             );
             $sess = $cmtr_sess->id
-                if $cmtr_sess->get('author_id') == $result->id;
+                if $cmtr_sess && $cmtr_sess->get('author_id') == $result->id;
         }
     }
 
