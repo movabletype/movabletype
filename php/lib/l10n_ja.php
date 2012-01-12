e1950724114a4330804e72fbdbe8b669595a85a6
Yuji Takayama <taka.swordbreaker@gmail.com>
2012-01-12 15:54:09 +0900

    L10N: Phase 5-2. bugid:107263, 106175

--- php/lib/l10n_ja.php
+++ php/lib/l10n_ja.php
@@ -8,12 +8,12 @@
 global $Lexicon_ja;
 $Lexicon_ja = array(
 
-## php/mt.php
-	'Page not found - [_1]' => '[_1]が見つかりませんでした。',
+## php/lib/MTUtil.php
+	'userpic-[_1]-%wx%h%x' => 'userpic-[_1]-%wx%h%x',
 
 ## php/lib/archive_lib.php
-	'Page' => 'ウェブページ',
 	'Individual' => 'ブログ記事',
+	'Page' => 'ウェブページ',
 	'Yearly' => '年別',
 	'Monthly' => '月別',
 	'Daily' => '日別',
@@ -25,64 +25,31 @@ $Lexicon_ja = array(
 	'Author Daily' => 'ユーザー 日別',
 	'Author Weekly' => 'ユーザー 週別',
 	'Category Yearly' => 'カテゴリ 年別',
-	'Category' => 'カテゴリ',
 	'Category Monthly' => 'カテゴリ 月別',
 	'Category Daily' => 'カテゴリ 日別',
 	'Category Weekly' => 'カテゴリ 週別',
 
-## php/lib/function.mtsetvar.php
+## php/lib/block.mtassets.php
+	'sort_by="score" must be used in combination with namespace.' => 'sort_by="score"を指定するときはnamespaceも指定しなければなりません。',
+
+## php/lib/block.mtauthorhasentry.php
+	'No author available' => 'ユーザーが見つかりません。',
+
+## php/lib/block.mtauthorhaspage.php
+
+## php/lib/block.mtentries.php
+
+## php/lib/block.mtif.php
 	'You used a [_1] tag without a valid name attribute.' => '[_1]タグではname属性は必須です。',
-	'\'[_1]\' is not a hash.' => '[_1]はハッシュではありません。',
-	'Invalid index.' => '不正なインデックスです。',
-	'\'[_1]\' is not an array.' => '[_1]は配列ではありません。',
 	'[_1] [_2] [_3] is illegal.' => '[_1] [_2] [_3]は不正です。',
-	'\'[_1]\' is not a valid function.' => '[_1]という関数はサポートされていません。',
-
-## php/lib/function.mtproductname.php
-	'[_1] [_2]' => '[_1] [_2]',
-
-## php/lib/function.mtcommentauthor.php
-	'Anonymous' => '匿名',
-
-## php/lib/block.mtsetvarblock.php
 
 ## php/lib/block.mtsethashvar.php
 
-## php/lib/function.mtcommentauthorlink.php
-
-## php/lib/function.mtvar.php
-	'\'[_1]\' is not a valid function for a hash.' => '[_1]はハッシュで利用できる関数ではありません。',
-	'\'[_1]\' is not a valid function for an array.' => '[_1]は配列で利用できる関数ではありません。',
-
-## php/lib/function.mtwidgetmanager.php
-	'Error: widgetset [_1] is empty.' => 'ウィジェットセット[_1]に中身がありません。',
-	'Error compiling widgetset [_1]' => 'ウィジェットセット[_1]をコンパイルできませんでした。',
-
-## php/lib/block.mtauthorhaspage.php
-	'No author available' => 'ユーザーが見つかりません。',
-
-## php/lib/block.mtentries.php
-	'sort_by="score" must be used in combination with namespace.' => 'sort_by="score"を指定するときはnamespaceも指定しなければなりません。',
-
-## php/lib/block.mtif.php
-
-## php/lib/block.mtassets.php
-
-## php/lib/thumbnail_lib.php
-	'GD support has not been available. Please install GD support.' => 'GDを利用できないようです。インストールしてください。',
-
-## php/lib/function.mtentryclasslabel.php
-	'page' => 'ウェブページ',
-	'entry' => 'ブログ記事',
-	'Entry' => 'ブログ記事',
-
-## php/lib/MTUtil.php
-	'userpic-[_1]-%wx%h%x' => 'userpic-[_1]-%wx%h%x',
-
-## php/lib/function.mtauthordisplayname.php
-
-## php/lib/function.mtremotesigninlink.php
-	'TypeKey authentication is not enabled in this blog.  MTRemoteSignInLink can\'t be used.' => 'ブログでTypeKey認証を有効にしていないので、MTRemoteSignInLinkは利用できません。',
+## php/lib/block.mtsetvarblock.php
+	'\'[_1]\' is not a hash.' => '[_1]はハッシュではありません。',
+	'Invalid index.' => '不正なインデックスです。',
+	'\'[_1]\' is not an array.' => '[_1]は配列ではありません。',
+	'\'[_1]\' is not a valid function.' => '[_1]という関数はサポートされていません。',
 
 ## php/lib/captcha_lib.php
 	'Captcha' => 'Captcha',
@@ -98,15 +65,42 @@ $Lexicon_ja = array(
 	'video' => 'ビデオ',
 	'Video' => 'ビデオ',
 
-## php/lib/block.mtauthorhasentry.php
+## php/lib/function.mtauthordisplayname.php
+
+## php/lib/function.mtcommentauthor.php
+	'Anonymous' => '匿名',
+
+## php/lib/function.mtcommentauthorlink.php
+
+## php/lib/function.mtcommenternamethunk.php
+	'This \'[_1]\' tag has been deprecated. Please use \'[_2]\' instead.' => 'テンプレートタグ \'[_1]\' は廃止されました。代わりに \'[_2]\'を使用してください。',
 
 ## php/lib/function.mtcommentreplytolink.php
 	'Reply' => '返信',
 
-## php/mt.php.pre
+## php/lib/function.mtentryclasslabel.php
+	'page' => 'ウェブページ',
+	'entry' => 'ブログ記事',
+	'Entry' => 'ブログ記事',
 
-## php/lib/function.mtcommenternamethunk.php
-	'This \'[_1]\' tag has been deprecated. Please use \'[_2]\' instead.' => 'テンプレートタグ \'[_1]\' は廃止されました。代わりに \'[_2]\'を使用してください。',
+## php/lib/function.mtproductname.php
+	'[_1] [_2]' => '[_1] [_2]',
+
+## php/lib/function.mtremotesigninlink.php
+	'TypePad authentication is not enabled in this blog.  MTRemoteSignInLink can\'t be used.' => 'ブログでTypePad認証を有効にしていないので、MTRemoteSignInLinkは利用できません。',
+
+## php/lib/function.mtsetvar.php
+
+## php/lib/function.mtvar.php
+	'\'[_1]\' is not a valid function for a hash.' => '[_1]はハッシュで利用できる関数ではありません。',
+	'\'[_1]\' is not a valid function for an array.' => '[_1]は配列で利用できる関数ではありません。',
+
+## php/lib/function.mtwidgetmanager.php
+	'Error: widgetset [_1] is empty.' => 'ウィジェットセット[_1]に中身がありません。',
+	'Error compiling widgetset [_1]' => 'ウィジェットセット[_1]をコンパイルできませんでした。',
+
+## php/mt.php
+	'Page not found - [_1]' => '[_1]が見つかりませんでした。',
 
 );
 function translate_phrase($str, $params = null) {
