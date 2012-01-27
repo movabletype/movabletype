18e19976f17e91dff7f4ab3d3e2271a9a30a1712
Yuji Takayama <taka.swordbreaker@gmail.com>
2012-01-27 15:42:34 +0900

    Changed message type from error. bugid:107343

--- lib/MT/App/Comments.pm
+++ lib/MT/App/Comments.pm
@@ -571,7 +571,7 @@ sub do_register {
             $msg .= '&nbsp;'.$app->translate(
                 '<a href="[_1]">Return to the original page.</a>', $static);
         }
-        return $app->forward( 'signup', error => $msg );
+        return $app->forward( 'signup', message => $msg );
     }
     $sess->remove;
 
@@ -586,7 +586,7 @@ sub do_register {
             $msg .= '&nbsp;'.$app->translate(
                 '<a href="[_1]">Return to the original page.</a>', $static);
         }
-        return $app->forward( 'signup', error => $msg );
+        return $app->forward( 'signup', message => $msg );
     }
 
     my $blog_id = $sess->get('blog_id');
