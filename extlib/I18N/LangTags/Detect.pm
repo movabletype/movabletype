387b7ae4b0aa2ce8032f37f8e814ee05ac54b00c
Taku AMANO <taku@toi-planning.net>
2012-03-22 11:22:34 +0900

    Fix the deprecated statement in I18N::LangTags::Detect. bugid: 107685
    Since the I18N::LangTags::Detect is included as core module now,
    I18N::LangTags::Detect as cpan module won't be updated. But perl 5.8.1 that
    is supported by Movable Type does not include I18N::LangTags::Detect.
    So, it is necessary to fixwith a patch.

--- extlib/I18N/LangTags/Detect.pm
+++ extlib/I18N/LangTags/Detect.pm
@@ -137,7 +137,7 @@ sub _try_use {   # Basically a wrapper around "require Modulename"
   my $module = $_[0];   # ASSUME sane module name!
   { no strict 'refs';
     return($tried{$module} = 1)
-     if defined(%{$module . "::Lexicon"}) or defined(@{$module . "::ISA"});
+     if %{$module . "::Lexicon"} or @{$module . "::ISA"};
     # weird case: we never use'd it, but there it is!
   }
 
