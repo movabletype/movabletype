28b2d3dbb53bf833b3c772a3f8aed4e001461ae2
Taku AMANO <taku@toi-planning.net>
2012-03-23 16:57:48 +0900

    Revert "Always render all lines even in 2.22.  bugid: 107463"
    This reverts commit 89f2aa97a2f9c32cc7fcfe35419314c1592f7480.

--- mt-static/codemirror/lib/codemirror.js
+++ mt-static/codemirror/lib/codemirror.js
@@ -861,8 +861,8 @@ var CodeMirror = (function() {
 
     function visibleLines() {
       var lh = textHeight(), top = scroller.scrollTop - paddingTop();
-      var from_height = 0;
-      var to_height = doc.height;
+      var from_height = Math.max(0, Math.floor(top / lh));
+      var to_height = Math.ceil((top + scroller.clientHeight) / lh);
       return {from: lineAtHeight(doc, from_height),
               to: lineAtHeight(doc, to_height)};
     }
