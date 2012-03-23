afdfbab6e4784ef4ad7eab6dae2abf782dc77162
Taku AMANO <taku@toi-planning.net>
2012-03-24 08:50:08 +0900

    Update the CodeMirror from 2.16 to 2.22 again.  bugid: 107686
    Reset "max-height" property for "pre" element inside CodeMirror.

--- mt-static/codemirror/lib/util/simple-hint.js
+++ mt-static/codemirror/lib/util/simple-hint.js
@@ -1,17 +1,4 @@
 (function() {
-  // Minimal event-handling wrapper.
-  function stopEvent() {
-    if (this.preventDefault) {this.preventDefault(); this.stopPropagation();}
-    else {this.returnValue = false; this.cancelBubble = true;}
-  }
-  function connect(node, type, handler) {
-    function wrapHandler(event) {handler(event || window.event);}
-    if (typeof node.addEventListener == "function")
-      node.addEventListener(type, wrapHandler, false);
-    else
-      node.attachEvent("on" + type, wrapHandler);
-  }
-
   CodeMirror.simpleHint = function(editor, getHints) {
     // We want a single cursor position.
     if (editor.somethingSelected()) return;
@@ -57,19 +44,19 @@
       close();
       setTimeout(function(){editor.focus();}, 50);
     }
-    connect(sel, "blur", close);
-    connect(sel, "keydown", function(event) {
+    CodeMirror.connect(sel, "blur", close);
+    CodeMirror.connect(sel, "keydown", function(event) {
       var code = event.keyCode;
       // Enter
-      if (code == 13) {stopEvent(event); pick();}
+      if (code == 13) {CodeMirror.e_stop(event); pick();}
       // Escape
-      else if (code == 27) {stopEvent(event); close(); editor.focus();}
+      else if (code == 27) {CodeMirror.e_stop(event); close(); editor.focus();}
       else if (code != 38 && code != 40) {
         close(); editor.focus();
         setTimeout(function(){CodeMirror.simpleHint(editor, getHints);}, 50);
       }
     });
-    connect(sel, "dblclick", pick);
+    CodeMirror.connect(sel, "dblclick", pick);
 
     sel.focus();
     // Opera sometimes ignores focusing a freshly created node
