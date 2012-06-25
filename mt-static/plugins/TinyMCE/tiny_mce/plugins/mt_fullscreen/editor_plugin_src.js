5052ce5aaca4a256eb02a009246f51e84de49232
Taichi Kaminogoya <kaminogoya@gmail.com>
2012-06-25 19:14:39 +0900

    changed style of tab in the editor. bugid:108180

--- mt-static/plugins/TinyMCE/tiny_mce/plugins/mt_fullscreen/editor_plugin_src.js
+++ mt-static/plugins/TinyMCE/tiny_mce/plugins/mt_fullscreen/editor_plugin_src.js
@@ -48,6 +48,7 @@
                     var $outer = $parent.find('table:visible');
                     var $inner = $parent.find('.mceIframeContainer:visible');
 
+                    var window_width  = $window.width();
                     var offset_width  = $outer.width() - $inner.width();
                     var offset_height =
                         $outer.height() - $inner.height() + header_height;
@@ -90,7 +91,6 @@
                         $('#' + this.id + '_resize').hide();
                     });
 
-                    $tabs.css('width', (100 / $tabs.length) + '%');
 
                     enabled = true;
                     ed.execCommand('mtFullScreenUpdateFitToWindow');
