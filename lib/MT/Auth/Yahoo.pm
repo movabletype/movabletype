4dbabc0876c3b5cf8299045cacc08e12b61c5bcb
Yuji Takayama <taka.swordbreaker@gmail.com>
2012-07-18 11:01:49 +0900

    Oops, wrong branch... Revert "Get nickname by OpenID AX. bugid:108034"
    This reverts commit 9aadefd4f168b7a93c8f3c56c9b178e805e85c4c.

--- lib/MT/Auth/Yahoo.pm
+++ lib/MT/Auth/Yahoo.pm
@@ -9,30 +9,10 @@ package MT::Auth::Yahoo;
 use strict;
 use base qw( MT::Auth::OpenID );
 
-sub set_extension_args {
-    my $class = shift;
-    my ($claimed_identity) = @_;
-
-    $claimed_identity->set_extension_args(
-        MT::Auth::OpenID::NS_OPENID_AX(),
-        {   'mode'          => 'fetch_request',
-            'required'      => 'nickname',
-            'type.nickname' => 'http://axschema.org/namePerson/friendly',
-        }
-    );
-}
-
 sub get_nickname {
     my $class = shift;
     my ($vident) = @_;
 
-    # If AX data found, use that as nickname.
-    my $fields
-        = $vident->extension_fields( MT::Auth::OpenID::NS_OPENID_AX() );
-    my $nick = $fields->{'value.nickname'};
-    return $nick if $nick;
-
-    # No, Profile URL as nickname
     my $url = $vident->url;
     if ( $url =~ m(^https?://me.yahoo.com/([^/]+)/?$) ) {
         return $1;
