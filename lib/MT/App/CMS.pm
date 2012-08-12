3d5b86e73897ad7d2104d0700670765c6ed9ed8f
Yuji Takayama <taka.swordbreaker@gmail.com>
2012-08-12 10:50:23 +0900

    Revert "clone blog option should not appear in system dashboard, bugid:107129"
    This reverts commit 97710c24d528977cf2275048ffba3cb1b49b688a.

--- lib/MT/App/CMS.pm
+++ lib/MT/App/CMS.pm
@@ -1385,9 +1385,6 @@ sub core_list_actions {
                 permit_action => 'clone_blog',
                 max           => 1,
                 dialog        => 1,
-                condition     => sub {
-                    return $app->blog ? 1 : 0;
-                },
             },
             'delete' => {
                 label         => 'Delete',
