76efccd833758330ffbebb3aee584c935b7a0b86
Taku AMANO <taku@toi-planning.net>
2012-10-18 09:46:01 +0900

    Remove the "mt_source_template" button at the "Create/Edit Page" screen.
    bugid:108911

--- mt-static/plugins/FormattedTextForTinyMCE/extension.js
+++ mt-static/plugins/FormattedTextForTinyMCE/extension.js
@@ -1,15 +1,28 @@
 (function($) {
 
 var config  = MT.Editor.TinyMCE.config;
-var buttons = config.plugin_mt_wysiwyg_buttons1 || '';
-if (buttons) {
-    buttons += ',|,';
-}
-buttons += 'template';
+var wisiwyg_buttons = config.plugin_mt_wysiwyg_buttons1 + ',|,template';
+var source_buttons  = (function(buttons) {
+    var replaced        = false;
+    var before          = /,mt_insert_image/;
+    var template_button = ',|,mt_source_template';
+
+    buttons = buttons.replace(before, function($0) {
+        replaced = true;
+        return $0 + template_button;
+    });
+
+    if (! replaced) {
+        buttons += template_button;
+    }
+
+    return buttons;
+})(config.plugin_mt_source_buttons1);
 
 $.extend(config, {
     plugins: config.plugins + ',template,mt_formatted_text',
-    plugin_mt_wysiwyg_buttons1: buttons
+    plugin_mt_wysiwyg_buttons1: wisiwyg_buttons,
+    plugin_mt_source_buttons1: source_buttons
 });
 
 tinymce.create('tinymce.plugins.MTFormattedText', {
