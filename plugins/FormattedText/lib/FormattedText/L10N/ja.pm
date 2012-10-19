80e6a586cbf2eb1b8ab120ae389d1a089f0de9e0
Yuji Takayama <taka.swordbreaker@gmail.com>
2012-10-19 08:02:39 -0400

    L10N ja, Also Standardize some stings. bugid:108874, 108897, 108836

--- plugins/FormattedText/lib/FormattedText/L10N/ja.pm
+++ plugins/FormattedText/lib/FormattedText/L10N/ja.pm
@@ -7,27 +7,42 @@ use base 'FormattedText::L10N::en_us';
 use vars qw( %Lexicon );
 
 %Lexicon = (
-    'Manage formatted text.' => '定型文を管理します。',
 
-    'Formatted Texts'   => '定型文',
-    'Formatted Text'    => '定型文',
-    'Text'              => '内容',
-    'My Formatted Text' => '自分の定型文',
+## plugins/FormattedText/config.yaml
+	'Formatted Texts' => '定型文',
+	'Formatted Text' => '定型文',
+	'Text' => '内容',
+	'My Formatted Text' => '自分の定型文',
+	'Edit Formatted Text' => '定型文の編集',
+	'Create Formatted Text' => '定型文の作成',
+	'Manage formatted text.' => '定型文を管理します。',
 
-    'Edit Formatted Text'       => '定型文の編集',
-    'Create Formatted Text'     => '定型文の作成',
+## plugins/FormattedText/lib/FormattedText/App.pm
+	'Are you sure you want to delete the selected FormattedTexts?' => '定型文を削除してもよろしいですか？',
+	'FormattedText' => '定型文',
+	'My Formatted Text' => '自分の定型文',
 
-    'Save changes to this formatted text (s)' =>
-        '定型文への変更を保存 (s)',
-
-    'Are you sure you want to delete the selected Formatted Texts?' =>
+## plugins/FormattedText/lib/FormattedText/FormattedText.pm
+	'FormattedTexts' => '定型文',
+	'Are you sure you want to delete the selected Formatted Texts?' =>
         '定型文を削除してもよろしいですか？',
+## plugins/FormattedText/tmpl/cms/edit_formatted_text.tmpl
+	'Edit FormattedText' => '定型文の編集',
+	'Create FormattedText' => '定型文の作成',
+	'This formatted text has been saved.' => '定型文を保存しました。',
+	'Save changes to this formatted text (s)' => '定型文への変更を保存 (s)',
 
-    'The formatted text has been deleted from the database.' =>
-        '定型文を削除しました',
+## plugins/FormattedText/tmpl/cms/list_formatted_text.tmpl
+	'The formatted text has been deleted from the database.' => '定型文を削除しました',
 
-    'This formatted text has been saved.' =>
-        '定型文を保存しました。',
+## plugins/FormattedTextForTinyMCE/config.yaml
+	'Add the "Insert Formatted Text" button to the TinyMCE.' => 'TinyMCE に「定型文の挿入」ボタンを追加します。',
+
+## plugins/FormattedTextForTinyMCE/lib/FormattedTextForTinyMCE/App.pm
+	'Cannot load formatted text.' => '記事テンプレートをロードできませんでした。',
+
+## plugins/FormattedTextForTinyMCE/tmpl/extension.tmpl
+
 );
 
 1;
--- plugins/FormattedTextForTinyMCE/lib/FormattedTextForTinyMCE/L10N/ja.pm
+++ plugins/FormattedTextForTinyMCE/lib/FormattedTextForTinyMCE/L10N/ja.pm
@@ -7,12 +7,14 @@ use base 'FormattedTextForTinyMCE::L10N::en_us';
 use vars qw( %Lexicon );
 
 %Lexicon = (
-    'Add the "Insert Formatted Text" button to the TinyMCE.' =>
-        'TinyMCE に「定型文の挿入」ボタンを追加します。',
-    'Cannot load formatted text.' =>
-        '記事テンプレートをロードできませんでした。',
-    'Insert Formatted Text'   => '定型文の挿入',
-    'Select a Formatted Text' => '定型文を選択...',
+
+## mt-static/plugins/FormattedTextForTinyMCE/extension.js
+	'Insert Formatted Text' => '定型文の挿入',
+
+## mt-static/plugins/FormattedTextForTinyMCE/langs/template.js
+	'Formatted Text' => '定型文',
+	'Select a Formatted Text' => '定型文を選択...',
+
 );
 
 1;
