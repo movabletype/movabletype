a1a46a87366acb63a33451751036d69e77617bd2
Taku AMANO <taku@taaas.jp>
2013-05-21 06:21:00 +0900

    Remove namespace pollution.

--- mt-static/data-api/v1/js/app.js
+++ mt-static/data-api/v1/js/app.js
@@ -449,6 +449,7 @@ Cookie.remove = function( name ) {
 };
 
 
+var JSON = window.JSON;
 /*
     json2.js
     2012-10-08
@@ -620,27 +621,6 @@ if (typeof JSON !== 'object') {
     function f(n) {
         // Format integers to have at least two digits.
         return n < 10 ? '0' + n : n;
-    }
-
-    if (typeof Date.prototype.toJSON !== 'function') {
-
-        Date.prototype.toJSON = function (key) {
-
-            return isFinite(this.valueOf())
-                ? this.getUTCFullYear()     + '-' +
-                    f(this.getUTCMonth() + 1) + '-' +
-                    f(this.getUTCDate())      + 'T' +
-                    f(this.getUTCHours())     + ':' +
-                    f(this.getUTCMinutes())   + ':' +
-                    f(this.getUTCSeconds())   + 'Z'
-                : null;
-        };
-
-        String.prototype.toJSON      =
-            Number.prototype.toJSON  =
-            Boolean.prototype.toJSON = function (key) {
-                return this.valueOf();
-            };
     }
 
     var cx = /[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,
