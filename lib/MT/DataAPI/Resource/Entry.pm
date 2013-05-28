00d37bc2f6e27213d380c514b2a0f0cfdf94d16c
Taku AMANO <taku@taaas.jp>
2013-05-28 10:18:57 +0900

    Load only published comments as a entry's sub resource, for an anonymous user.

--- lib/MT/DataAPI/Resource/Entry.pm
+++ lib/MT/DataAPI/Resource/Entry.pm
@@ -18,8 +18,7 @@ sub updatable_fields {
             keywords
             tags
             ),
-        {
-            name => 'date',
+        {   name      => 'date',
             condition => sub {
                 MT->instance->can_do('edit_entry_basename');
             },
@@ -145,28 +144,80 @@ sub fields {
             from_object => sub {
                 my ($obj) = @_;
                 my $app   = MT->instance;
-                my $args  = undef;
+                my $user  = $app->user;
+
+                my $terms = undef;
+                if ( !$user->is_superuser ) {
+                    my $perm = $app->model('permission')->load(
+                        {   author_id => $user->id,
+                            blog_id   => $obj->blog_id,
+                        },
+                    );
+                    if (!$perm
+                        || !(
+                            $perm->can_do('view_all_comments')
+                            || (   $perm->can_do('view_own_entry_comment')
+                                && $obj->author_id == $user->id )
+                        )
+                        )
+                    {
+                        require MT::Comment;
+                        $terms = {
+                            visible     => 1,
+                            junk_status => MT::Comment::NOT_JUNK(),
+                        };
+                    }
+                }
+
+                my $args = undef;
                 if ( $app->can('param')
                     && defined( $app->param('maxComments') ) )
                 {
                     $args = { limit => int( $app->param('maxComments') ), };
                 }
                 MT::DataAPI::Resource->from_object(
-                    $obj->comments( undef, $args ) );
+                    $obj->comments( $terms, $args ) );
             },
         },
         {   name        => 'trackbacks',
             from_object => sub {
                 my ($obj) = @_;
                 my $app   = MT->instance;
-                my $args  = undef;
+                my $user  = $app->user;
+
+                my $terms = undef;
+                if ( !$user->is_superuser ) {
+                    my $perm = $app->model('permission')->load(
+                        {   author_id => $user->id,
+                            blog_id   => $obj->blog_id,
+                        },
+                    );
+                    if (!$perm
+                        || !(
+
+                            $perm->can_do('manage_feedback')
+                            || $perm->can_do('manage_pages')
+                            || (   $perm->can_do('create_post')
+                                && $obj->author_id == $user->id )
+                        )
+                        )
+                    {
+                        require MT::TBPing;
+                        $terms = {
+                            visible     => 1,
+                            junk_status => MT::TBPing::NOT_JUNK(),
+                        };
+                    }
+                }
+
+                my $args = undef;
                 if ( $app->can('param')
                     && defined( $app->param('maxTrackbacks') ) )
                 {
                     $args = { limit => int( $app->param('maxTrackbacks') ), };
                 }
                 MT::DataAPI::Resource->from_object(
-                    $obj->pings( undef, $args ) || [] );
+                    $obj->pings( $terms, $args ) || [] );
             },
         },
         {   name        => 'assets',
