9cb288e92480f7211ba48effe464b54eac1cfe7f
Masahiro Iuchi <miuchi@sixapart.com>
2013-06-20 16:36:24 +0900

    Implemented "with_domain" modifier to mt:SupportDirectoryURL tag in dynamic publishing. bugid:109998

--- php/lib/function.mtsupportdirectoryurl.php
+++ php/lib/function.mtsupportdirectoryurl.php
@@ -8,6 +8,19 @@
 function smarty_function_mtsupportdirectoryurl($args, &$ctx) {
     require_once "MTUtil.php";
     $url = support_directory_url();
+    if (isset($args['with_domain']) && $args['with_domain'] && !preg_match("!^http!", $url)) {
+        $blog = $ctx->stash('blog');
+        if (!isset($blog)) {
+            return $url;
+        }
+        $host = $blog->site_url();
+        if (!preg_match("!^/!", $url)) {
+            $url = '/' . $url;
+        }
+        if (preg_match("!(https?://[^/:]+)(:\d+)?/?!", $host, $matches)) {
+            $url = $matches[1] . $url;
+        }
+    }
     return $url;
 }
 ?>
