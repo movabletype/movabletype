57704d67ab9b6d22d6e5057a8416457515b4fbbd
Yuji Takayama <taka.swordbreaker@gmail.com>
2013-07-08 13:52:57 +0900

    Changed product version to 6.0. bugid:110184

--- php/mt.php
+++ php/mt.php
@@ -11,7 +11,7 @@
 require_once('lib/class.exception.php');
 
 define('VERSION', '6.0');
-define('PRODUCT_VERSION', '6.0.0');
+define('PRODUCT_VERSION', '6.0');
 
 $PRODUCT_NAME = '__PRODUCT_NAME__';
 if($PRODUCT_NAME == '__PRODUCT' . '_NAME__')
