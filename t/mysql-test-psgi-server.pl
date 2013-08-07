2e9acc825aec3d33c6a16d5098b39a9d3d65d3f9
Taku AMANO <taku@taaas.jp>
2013-08-07 11:36:56 +0900

    Add a script to run PSGI server with specic plugin path. bugid:110332

---
+++ t/mysql-test-psgi-server.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use lib $ENV{MT_HOME} ? "$ENV{MT_HOME}/lib"    : 'lib';
+use lib $ENV{MT_HOME} ? "$ENV{MT_HOME}/extlib" : 'extlib';
+use lib $ENV{MT_HOME} ? "$ENV{MT_HOME}/t/lib"  : 't/lib';
+
+BEGIN {
+    $ENV{MT_CONFIG} ||= 'mysql-test.cfg';
+}
+
+use Plack::Loader;
+use Getopt::Long;
+
+use MT;
+eval(
+    $ENV{SKIP_REINITIALIZE_DATABASE}
+    ? "use MT::Test;"
+    : "use MT::Test qw(:db :data);"
+);
+
+
+my $port         = 5000;
+my @plugin_paths = ();
+GetOptions(
+    'port=i'       => \$port,
+    'plugin-path=s' => \@plugin_paths,
+) or exit(1);
+
+
+my $mt = MT->new;
+$mt->_init_plugins_core( {}, 1, \@plugin_paths ) if @plugin_paths;
+
+
+eval "use MT::PSGI;";
+my $app = MT::PSGI->new()->to_app();
+my $loader = Plack::Loader->load(
+    'Starman',
+    port        => $port,
+    max_workers => 1
+);
+$loader->run($app);
