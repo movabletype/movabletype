4320f57e84a104ea26d23dd5f0b6b487f47184e7
Seiichiro Hazama <shazama@sixapart.com>
2013-10-07 16:47:21 +0900

    Fixed UUV. BugzID: 110668

--- lib/MT/Template/Tags/Comment.pm
+++ lib/MT/Template/Tags/Comment.pm
@@ -670,10 +670,11 @@ sub _hdlr_comment_replies {
     my $iter = MT::Comment->load_iter( \%terms, \%args );
     my %entries;
     my $blog = $ctx->stash('blog');
-    my $so 
-        = lc( $args->{sort_order} )
-        || ( $blog ? $blog->sort_order_comments : undef )
-        || 'ascend';
+    my $so
+        = $args->{sort_order}
+        ? lc( $args->{sort_order} )
+        : ( $blog ? $blog->sort_order_comments : undef ) || 'ascend';
+
     my $n = $args->{lastn};
     my @comments;
 
