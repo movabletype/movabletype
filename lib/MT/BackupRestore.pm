76721c5b6e573eae7ab304d6407d0495df7fe519
Masahiro Iuchi <miuchi@sixapart.com>
2013-11-07 11:52:20 +0900

    Revert "Add secondary sorting by template_id when backing up templates. bugid:110906"
    This reverts commit 1bd88b78af77a135c5201add1db9349aec5a7124.

--- lib/MT/BackupRestore.pm
+++ lib/MT/BackupRestore.pm
@@ -1538,13 +1538,13 @@ sub backup_terms_args {
     if ( defined($blog_ids) && scalar(@$blog_ids) ) {
         return {
             terms => { 'blog_id' => $blog_ids },
-            args => { sort => [ { column => 'type' }, { column => 'id' } ] },
+            args  => { sort => 'type' },
         };
     }
     else {
         return {
             terms => undef,
-            args  => { sort => [ { column => 'type' }, { column => 'id' } ] },
+            args  => { sort => 'type' },
         };
     }
 }
