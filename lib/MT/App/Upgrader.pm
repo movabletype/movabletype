c61ce9ae669773e0242b76ab564d667148ba1d79
Taku AMANO <taku@taaas.jp>
2014-01-20 11:38:00 +0900

    Validate initial user's values by MT::CMS::User::save_filter. bugid:108187

--- lib/MT/App/Upgrader.pm
+++ lib/MT/App/Upgrader.pm
@@ -362,6 +362,26 @@ sub init_user {
         }
     }
 
+    {
+        my $eh   = MT::ErrorHandler->new;
+        my $user = MT::Author->new;
+        $user->set_values(
+            {   name        => $initial_user,
+                nickname    => $initial_nickname,
+                email       => $initial_email,
+                lang        => $initial_lang,
+                external_id => $initial_external_id,
+            }
+        );
+        $user->set_password($initial_password);
+        require MT::CMS::User;
+        if (!MT::CMS::User::save_filter( $eh, $app, $user, $user->clone, 1 ) )
+        {
+            $param{error} = $eh->errstr;
+            return $app->build_page( 'install.tmpl', \%param );
+        }
+    }
+
     $initial_use_system = 1
         if $param{use_system_email};
 
--- lib/MT/CMS/User.pm
+++ lib/MT/CMS/User.pm
@@ -1506,7 +1506,7 @@ sub can_delete {
 }
 
 sub save_filter {
-    my ( $eh, $app, $obj, $original ) = @_;
+    my ( $eh, $app, $obj, $original, $skip_encode_html ) = @_;
     my $accessor = sub {
         if ($obj) {
             my $k = shift;
@@ -1515,6 +1515,9 @@ sub save_filter {
         else {
             $app->param(@_);
         }
+    };
+    my $encode_html = sub {
+        $skip_encode_html ? $_[0] : encode_html( $_[0] );
     };
 
     my $name = $accessor->('name');
@@ -1557,7 +1560,7 @@ sub save_filter {
                 $app->translate(
                     "[_1] contains an invalid character: [_2]",
                     $app->translate("Username"),
-                    encode_html($1)
+                    $encode_html->($1)
                 )
             );
         }
@@ -1577,7 +1580,7 @@ sub save_filter {
                 $app->translate(
                     "[_1] contains an invalid character: [_2]",
                     $app->translate("Display Name"),
-                    encode_html($1)
+                    $encode_html->($1)
                 )
             );
         }
@@ -1603,7 +1606,11 @@ sub save_filter {
     return 1 if ( $pref ne 'MT' );
     if ( !$accessor->('id') ) {    # it's a new object
         return $eh->error( $app->translate("User requires password") )
-            if ( 0 == length( scalar $app->param('pass') ) );
+            if (
+            0 == length(
+                $obj ? $accessor->('password') : scalar $app->param('pass')
+            )
+            );
     }
     my $email = $accessor->('email');
     return $eh->error(
@@ -1618,7 +1625,7 @@ sub save_filter {
             $app->translate(
                 "[_1] contains an invalid character: [_2]",
                 $app->translate("Email Address"),
-                encode_html($1)
+                $encode_html->($1)
             )
         );
     }
