56574a374f701b1a97fdfc91d710498776a70d11
Taku AMANO <taku@taaas.jp>
2014-01-22 18:46:24 +0900

    Delete disabled authenticator. bugid:111361

--- lib/MT/CMS/Blog.pm
+++ lib/MT/CMS/Blog.pm
@@ -62,6 +62,9 @@ sub edit {
             my %auth = %{ $cmtauth_reg->{$auth} };
             $cmtauth{$auth} = \%auth;
             if ( my $c = $cmtauth_reg->{$auth}->{condition} ) {
+                delete $cmtauth{$auth}, next
+                    if $cmtauth_reg->{$auth}->{disable};
+
                 $c = $app->handler_to_coderef($c);
                 if ($c) {
                     my $reason;
