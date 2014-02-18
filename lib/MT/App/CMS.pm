6d251e752180ef59b5028f000d20b76b85c6e525
Taku AMANO <taku@taaas.jp>
2014-02-18 13:06:46 +0900

    Apply "class" filter even for blog selector. bugid:111419

--- lib/MT/App/CMS.pm
+++ lib/MT/App/CMS.pm
@@ -3029,7 +3029,8 @@ sub build_blog_selector {
 
         # This user can access over 6 blogs.
         if (@fav_blogs) {
-            @blogs = $blog_class->load( { id => \@fav_blogs } );
+            @blogs
+                = $blog_class->load( { class => 'blog', id => \@fav_blogs } );
         }
         else {
             @blogs = ();
