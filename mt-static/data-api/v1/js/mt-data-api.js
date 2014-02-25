e6559fb3daa164acb52929041292dbdc634c52f3
Taku AMANO <taku@taaas.jp>
2014-02-25 18:02:11 +0900

    Update mt-data-api.js.
    Process one time token in initializing by default. bugid:111411

--- mt-static/data-api/v1/js/mt-data-api.js
+++ mt-static/data-api/v1/js/mt-data-api.js
@@ -92,6 +92,7 @@ var DataAPI = function(options) {
         timeout: undefined,
         cache: true,
         withoutAuthorization: false,
+        processOneTimeTokenOnInitialize: true,
         loadPluginEndpoints: true,
         suppressResponseCodes: undefined,
         crossOrigin: undefined,
@@ -122,6 +123,10 @@ var DataAPI = function(options) {
         this.loadEndpoints({
             excludeComponents: 'core'
         });
+    }
+
+    if (this.o.processOneTimeTokenOnInitialize) {
+        this._storeOneTimeToken();
     }
 
     this.trigger('initialize');
@@ -2391,6 +2396,31 @@ DataAPI.prototype = {
         return defaultToken;
     },
 
+    _hasOneTimeToken: function() {
+        return window.location && window.location.hash.indexOf('#_ott_') === 0;
+    },
+
+    _storeOneTimeToken: function() {
+        var token, m;
+
+        if (! window.location) {
+            return undefined;
+        }
+
+        m = window.location.hash.match(/^#_ott_(.*)/);
+        if (! m) {
+            return undefined;
+        }
+
+        token = {
+            oneTimeToken: m[1]
+        };
+        window.location.hash = '#_login';
+
+        this.storeTokenData(token);
+        return token;
+    },
+
     /**
      * Get token data via current session store.
      * @method getTokenData
@@ -2398,8 +2428,7 @@ DataAPI.prototype = {
      * @category core
      */
     getTokenData: function() {
-        var token = this.tokenData,
-            m;
+        var token = this.tokenData;
 
         if (! token) {
             if (window.location) {
@@ -2410,11 +2439,8 @@ DataAPI.prototype = {
                     catch (e) {
                     }
                 }
-                else if (m = window.location.hash.match(/^#_ott_(.*)/)) {
-                    token = {
-                        oneTimeToken: m[1]
-                    };
-                    window.location.hash = '#_login';
+                else if (this._hasOneTimeToken()) {
+                    this._storeOneTimeToken();
                 }
             }
 
