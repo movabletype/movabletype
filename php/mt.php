8557260f60c8c90ce9eb10e1089c976a4cd90dba
Masahiro Iuchi <miuchi@sixapart.com>
2014-03-07 10:51:54 +0900

    Do not catch E_WARNING warnings in dynamic publishing. bugid:110627

--- php/mt.php
+++ php/mt.php
@@ -811,7 +811,7 @@ class MT {
     }
 
     function error_handler($errno, $errstr, $errfile, $errline) {
-        if ($errno & (E_ALL ^ E_NOTICE)) {
+        if ($errno & (E_ALL ^ E_NOTICE ^ E_WARNING)) {
             if ( !empty( $this->db ) ) {
                 $errstr = encode_html_entities($errstr, ENT_COMPAT);
                 $errfile = encode_html_entities($errfile, ENT_COMPAT);
