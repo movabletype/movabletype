bcfd46b22d7a247b2a88fb157b5ebc7b20f3268a
Masahiro Iuchi <miuchi@sixapart.com>
2014-09-08 13:10:22 +0900

    Separate v2 resource definition. bugid:111942

---
+++ lib/MT/DataAPI/Resource/Entry/v2.pm
@@ -0,0 +1,57 @@
+# Movable Type (r) (C) 2001-2014 Six Apart, Ltd. All Rights Reserved.
+# This code cannot be redistributed without permission from www.sixapart.com.
+# For more information, consult your Movable Type license.
+#
+# $Id$
+package MT::DataAPI::Resource::Entry::v2;
+
+use strict;
+use warnings;
+
+use base qw( MT::DataAPI::Resource::Entry );
+
+# updatable_fields is the same as v1.
+
+sub fields {
+
+    # Overwrite categories field.
+    [   @{ __PACAKGE__->SUPER::fields() },
+        {   name        => 'categories',
+            from_object => sub {
+                my ($obj) = @_;
+                my $rows = $obj->__load_category_data or return;
+
+                my $primary = do {
+                    my @rows = grep { $_->[1] } @$rows;
+                    @rows ? $rows[0]->[0] : 0;
+                };
+
+                my $cats = MT::Category->lookup_multi(
+                    [ map { $_->[0] } @$rows ] );
+
+                MT::DataAPI::Resource->from_object(
+                    [   sort {
+                                  $a->id == $primary ? -1
+                                : $b->id == $primary ? 1
+                                : $a->label cmp $b->label
+                        } @$cats
+                    ]
+                );
+            },
+        }
+    ];
+}
+
+1;
+
+__END__
+            
+=head1 NAME 
+        
+MT::DataAPI::Resource::Entry::v2 - Movable Type class for resources definitions of the MT::Entry.
+            
+=head1 AUTHOR & COPYRIGHT
+            
+Please see the I<MT> manpage for author, copyright, and license information.
+        
+=cut  
