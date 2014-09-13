c68927fbea5e50923b0027e7187f77d36cef1eb7
Yuji Takayama <taka.swordbreaker@gmail.com>
2014-09-13 14:26:51 +0900

    add debug code

--- lib/MT/App/CMS.pm
+++ lib/MT/App/CMS.pm
@@ -621,6 +621,7 @@ print STDERR Dumper( $plugin );
 print STDERR "Update Requreired? " . $app->{upgrade_required} . "\n";
 
     if ( $app->{upgrade_required} ) {
+        delete $app->{upgrade_required};
         $app->{requires_login} = 0;
         $app->mode('upgrade');
     }
