d922d4bfdde8929e2df18d48df7d98726d01f246
Masahiro Iuchi <miuchi@sixapart.com>
2014-09-28 10:31:40 +0900

    Fix and refactor save_filter callback of site. bugid:112416

--- lib/MT/DataAPI/Callback/Website.pm
+++ lib/MT/DataAPI/Callback/Website.pm
@@ -9,74 +9,10 @@ package MT::DataAPI::Callback::Website;
 use strict;
 use warnings;
 
-use File::Spec;
-use MT::CMS::Common;
+use MT::DataAPI::Callback::Blog;
 
-# Implemented filtering processes in MT::CMS::Common::save().
 sub save_filter {
-    my ( $eh, $app, $obj, $original ) = @_;
-
-    # Check empty fields.
-    my @not_empty_fields = (
-        { field => 'name',      parameter => 'name' },
-        { field => 'site_url',  parameter => 'url' },
-        { field => 'site_path', parameter => 'sitePath' },
-        { field => 'theme_id',  parameter => 'themeId' },
-    );
-    for (@not_empty_fields) {
-        my $field = $_->{field};
-        if ( !( defined $obj->$field ) || $obj->$field eq '' ) {
-            return $app->errtrans( 'A parameter "[_1]" is required.',
-                $_->{parameter} );
-        }
-    }
-
-    # Check positive interger fields.
-    my @positive_inteter_columns
-        = qw( max_revisions_entry max_revisions_template );
-    for my $col (@positive_interger_columns) {
-        if ( !( $obj->$col && $obj->$col =~ m/^\d+$/ ) ) {
-            return $eh->error(
-                $app->translate(
-                    "The number of revisions to store must be a positive integer."
-                )
-            );
-        }
-    }
-
-    # Check whether blog has a preferred archive type or not.
-    if ( _blog_has_archive_type($obj) && !$obj->archive_type_preferred ) {
-        return $eh->error(
-            $app->translate("Please choose a preferred archive type.") );
-    }
-
-    # Check whether theme_id is valid or not.
-    require MT::Theme;
-    if ( !MT::Theme->load( $obj->theme_id ) ) {
-        return $app->errtrans( 'Invalid theme_id: [_1]', $obj->theme_id );
-    }
-
-    # Check whether site_path is within BaseSitePath or not.
-    my $site_path = $obj->column('site_path');
-    if ( $site_path and $app->config->BaseSitePath ) {
-        my $l_path = $app->config->BaseSitePath;
-        unless (
-            MT::CMS::Common::is_within_base_sitepath( $app, $site_path ) )
-        {
-            return $app->errtrans(
-                "The website root directory must be within [_1]", $l_path );
-        }
-    }
-
-    # Check whether site_path is absolute or not.
-    if ( $site_path
-        and not File::Spec->file_name_is_absolute($site_path) )
-    {
-        return $app->errtrans(
-            "The website root directory must be absolute: [_1]", $site_path );
-    }
-
-    return 1;
+    MT::DataAPI::Callback::Blog::save_filter(@_);
 }
 
 1;
