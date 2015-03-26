024bfb114b09c031f9f0457d11ffa1546da0e76c
Seiichiro Hazama <shazama@sixapart.com>
2015-03-26 07:34:14 +0000

    Run perltidy.

--- lib/MT/App/Wizard.pm
+++ lib/MT/App/Wizard.pm
@@ -247,8 +247,8 @@ sub init_core_registry {
                     'This module is needed if you would like to be able to use NetPBM as the image driver for MT.',
             },
             'Storable' => {
-                
-link => 'http://search.cpan.org/dist/Storable',
+
+                link => 'http://search.cpan.org/dist/Storable',
                 label =>
                     'This module is required by certain MT plugins available from third parties.',
             },
