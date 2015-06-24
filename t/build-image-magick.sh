f5dcd44aa8c50e88720f7c08d5800e0cd716b86f
Masahiro Iuchi <miuchi@sixapart.com>
2015-06-24 22:09:27 +0900

    Revert "Do "make install" every time."
    This reverts commit 35900b8b3db3e3328c6e5d506d408c3bdcc90544.

--- t/build-image-magick.sh
+++ t/build-image-magick.sh
@@ -5,27 +5,16 @@
 # ImageMagick-6.9.1-6 causes segmentation fault when testing,
 # so the latest version of ImageMagick is not used.
 
-installed=[[ -f ${HOME}/image-magick/ImageMagick-6.9.0-10.tar.xz ]] && true || false
-
-if [[ ! ${installed} ]]; then
+if [[ ! -f ${HOME}/image-magick/ImageMagick-6.9.0-10.tar.xz ]]; then
   if [[ ! -d ${HOME}/image-magick ]]; then
     mkdir ${HOME}/image-magick
   fi
-fi
-
-pushd ${HOME}/image-magick
-
-if [[ ! ${installed} ]]; then
+  pushd ${HOME}/image-magick
   wget http://www.imagemagick.org/download/releases/ImageMagick-6.9.0-10.tar.xz
   tar Jxf ImageMagick-6.9.0-10.tar.xz
-fi
-
-cd ImageMagick-*
-
-if [[ ! ${installed} ]]; then
+  cd ImageMagick-*
   ./configure --prefix=${HOME}/image-magick --with-perl=${HOME}/perl5/perlbrew/perls/${TRAVIS_PERL_VERSION}/bin/perl
   make
+  make install
+  popd
 fi
-
-make install
-popd
