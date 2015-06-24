35900b8b3db3e3328c6e5d506d408c3bdcc90544
Masahiro Iuchi <miuchi@sixapart.com>
2015-06-24 20:56:48 +0900

    Do "make install" every time.

--- t/build-image-magick.sh
+++ t/build-image-magick.sh
@@ -5,16 +5,27 @@
 # ImageMagick-6.9.1-6 causes segmentation fault when testing,
 # so the latest version of ImageMagick is not used.
 
-if [[ ! -f ${HOME}/image-magick/ImageMagick-6.9.0-10.tar.xz ]]; then
+installed=[[ -f ${HOME}/image-magick/ImageMagick-6.9.0-10.tar.xz ]] && true || false
+
+if [[ ! ${installed} ]]; then
   if [[ ! -d ${HOME}/image-magick ]]; then
     mkdir ${HOME}/image-magick
   fi
-  pushd ${HOME}/image-magick
+fi
+
+pushd ${HOME}/image-magick
+
+if [[ ! ${installed} ]]; then
   wget http://www.imagemagick.org/download/releases/ImageMagick-6.9.0-10.tar.xz
   tar Jxf ImageMagick-6.9.0-10.tar.xz
-  cd ImageMagick-*
+fi
+
+cd ImageMagick-*
+
+if [[ ! ${installed} ]]; then
   ./configure --prefix=${HOME}/image-magick --with-perl=${HOME}/perl5/perlbrew/perls/${TRAVIS_PERL_VERSION}/bin/perl
   make
-  make install
-  popd
 fi
+
+make install
+popd
