d416e58d26a80c00b2f02e1dd126dc5a900af5c1
Masahiro Iuchi <miuchi@sixapart.com>
2015-06-26 04:27:59 +0000

    Set Test::mysqld parameters.

--- t/lib/MTTestDB.pm
+++ t/lib/MTTestDB.pm
@@ -2,12 +2,27 @@ package MTTestDB;
 use strict;
 use warnings;
 
-sub prepare { }
+# Set 'mysql_install_db' and 'mysqld' to Test::mysqld instance.
+# These parameters cannot be set from App::Prove::Plugin::MySQLPool.
+{
+    use Test::mysqld;
+    use Test::mysqld::Pool;
+    my $new              = \&Test::mysqld::new;
+    my $_launch_instance = \&Test::mysqld::Pool::_launch_instance;
 
-sub my_cnf {
-    +{  mysql_install_db => '/usr/bin/mysql_install_db',
-        mysqld           => '/usr/bin/mysqld_safe',
+    no warnings 'redefine';
+    *Test::mysqld::Pool::_launch_instance = sub {
+        local *Test::mysqld::new = sub {
+            my ( $class, %args ) = @_;
+            $args{mysql_install_db} = '/usr/bin/mysql_install_db';
+            $args{mysqld}           = '/usr/bin/mysqld_safe';
+            $new->( $class, %args );
+        };
+
+        $_launch_instance->(@_);
     };
 }
+
+sub prepare { }
 
 1;
