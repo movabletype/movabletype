dd85d08806f9758dd0bbb61aebcea78efde1b191
Yuji Takayama <taka.swordbreaker@gmail.com>
2015-07-10 14:31:37 +0900

    Bump up version number.

--- php/mt.php
+++ php/mt.php
@@ -10,9 +10,9 @@
  */
 require_once('lib/class.exception.php');
 
-define('VERSION', '6.1');
-define('PRODUCT_VERSION', '6.1.2');
-define('DATA_API_DEFAULT_VERSION', '2');
+define('VERSION', '6.2');
+define('PRODUCT_VERSION', '6.2');
+define('DATA_API_DEFAULT_VERSION', '0');
 
 $PRODUCT_NAME = '__PRODUCT_NAME__';
 if($PRODUCT_NAME == '__PRODUCT' . '_NAME__')
