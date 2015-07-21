df538dc99f1f8b2db47d56feb5623692bf7b3996
Masahiro Iuchi <miuchi@sixapart.com>
2015-07-22 08:47:48 +0900

    Fix that undo/redo image is broken.

--- mt-static/js/image_editor/image_editor.js
+++ mt-static/js/image_editor/image_editor.js
@@ -50,150 +50,17 @@
     });
 
     // History plugin
-    Darkroom.plugins.history = Darkroom.Plugin.extend({
-        initialize: function InitDarkroomHistoryPlugin() {
-            this.backHistoryStack = [];
-            this.forwardHistoryStack = [];
+    var initialize = Darkroom.plugins.history.prototype.initialize;
+    Darkroom.plugins.history.prototype.initialize = function() {
+        (initialize.bind(this))();
+        this.postUpdate = this.options.postUpdate || function() {};
+    };
 
-            this.postUpdate = this.options.postUpdate || function() {};
-
-            this._snapshotImage();
-
-            this.darkroom.addEventListener('image:change', this._onImageChange.bind(this));
-        },
-
-        goBack: function() {
-            if (this.backHistoryStack.length === 0) {
-                return;
-            }
-
-            this.forwardHistoryStack.push({
-                image: this.currentImage,
-                width: this.currentWidth,
-                height: this.currentHeight,
-                angle: this.currentAngle,
-                flipX: this.currentFlipX,
-                flipY: this.currentFlipY
-            });
-
-            var history = this.backHistoryStack.pop();
-
-            this.currentImage = history.image;
-            this._applyImage(this.currentImage);
-
-            this.currentWidth = history.width;
-            this.currentHeight = history.height;
-            this.darkroom.getPlugin('resize').resize(this.currentWidth, this.currentHeight);
-
-            this.currentAngle = history.angle;
-            this.darkroom.image.setAngle(this.currentAngle);
-
-            this.currentFlipX = history.flipX;
-            this.darkroom.image.setFlipX(this.currentFlipX);
-
-            this.currentFlipY = history.flipY;
-            this.darkroom.image.setFlipY(this.currentFlipY);
-
-            // Dispatch an event, so listeners will know the
-            // currently viewed image has been changed.
-            this.darkroom.dispatchEvent('history:navigate');
-        },
-
-        goForward: function() {
-            if (this.forwardHistoryStack.length === 0) {
-                return;
-            }
-
-            this.backHistoryStack.push({
-                image: this.currentImage,
-                width: this.currentWidth,
-                height: this.currentHeight,
-                angle: this.currentAngle,
-                flipX: this.currentFlipX,
-                flipY: this.currentFlipY
-            });
-
-            var history = this.forwardHistoryStack.pop();
-
-            this.currentImage = history.image;
-            this._applyImage(this.currentImage);
-
-            this.currentWidth = history.width;
-            this.currentHeight = history.height;
-            this.darkroom.getPlugin('resize').resize(this.currentWidth, this.currentHeight);
-
-            this.currentAngle = history.angle;
-            this.darkroom.image.setAngle(this.currentAngle);
-
-            this.currentFlixX = history.flipX;
-            this.darkroom.image.setFlipX(this.currentFlipX);
-
-            this.currentFlipY = history.flipY;
-            this.darkroom.image.setFlipY(this.currentFlipY);
-
-            // Dispatch an event, so listeners will know the
-            // currently viewed image has been changed.
-            this.darkroom.dispatchEvent('history:navigate');
-        },
-
-        _snapshotImage: function() {
-            var _this = this;
-            var image = new Image();
-            image.src = this.darkroom.snapshotImage();
-
-            var $resizable = jQuery(this.darkroom.container).find('div.canvas-container');
-
-            this.currentImage = image;
-            this.currentWidth = $resizable.width();
-            this.currentHeight = $resizable.height();
-            this.currentAngle = this.darkroom.image.getAngle();
-            this.currentFlipX = this.darkroom.image.getFlipX();
-            this.currentFlipY = this.darkroom.image.getFlipY();
-        },
-
-        _onImageChange: function() {
-            this.backHistoryStack.push({
-                image: this.currentImage,
-                width: this.currentWidth,
-                height: this.currentHeight,
-                angle: this.currentAngle,
-                flipX: this.currentFlipX,
-                flipY: this.currentFlipY
-            });
-            this._snapshotImage();
-            this.forwardHistoryStack.length = 0;
-
-            this.postUpdate();
-        },
-
-        // Apply image to the canvas
-        _applyImage: function(image) {
-            var canvas = this.darkroom.canvas;
-
-            var imgInstance = new fabric.Image(image, {
-                // options to make the image static
-                selectable: false,
-                evented: false,
-                lockMovementX: true,
-                lockMovementY: true,
-                lockRotation: true,
-                lockScalingX: true,
-                lockScalingY: true,
-                lockUniScaling: true,
-                hasControls: false,
-                hasBorders: false
-            });
-
-            // Update canvas size
-            canvas.setWidth(image.width);
-            canvas.setHeight(image.height);
-
-            // Add image
-            this.darkroom.image.remove();
-            this.darkroom.image = imgInstance;
-            canvas.add(imgInstance);
-        }
-    });
+    var _onImageChange = Darkroom.plugins.history.prototype._onImageChange;
+    Darkroom.plugins.history.prototype._onImageChange = function() {
+        (_onImageChange.bind(this))();
+        this.postUpdate();
+    };
 
     // Resize plugin
     Darkroom.plugins.resize = Darkroom.Plugin.extend({
