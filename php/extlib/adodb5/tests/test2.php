5153efebf3b910bd7b8a0702ffe5143c27cd0bf0
ryota masuko <ryota.masuko@gmail.com>
2016-03-15 22:02:09 +0900

    updated adodb5.18 -> 5.20.3

--- php/extlib/adodb5/tests/test2.php
+++ php/extlib/adodb5/tests/test2.php
@@ -9,13 +9,13 @@
 	$access = 'd:\inetpub\wwwroot\php\NWIND.MDB';
 	$myDSN =  'PROVIDER=Microsoft.Jet.OLEDB.4.0;'
 		. 'DATA SOURCE=' . $access . ';';
-		
+
 	echo "<p>PHP ",PHP_VERSION,"</p>";
-	
+
 	$db->Connect($myDSN) || die('fail');
-	
+
 	print_r($db->ServerInfo());
-	
+
 	try {
 	$rs = $db->Execute("select $db->sysTimeStamp,* from adoxyz where id>02xx");
 	print_r($rs->fields);
@@ -23,4 +23,3 @@
 	print_r($e);
 	echo "<p> Date m/d/Y =",$db->UserDate($rs->fields[4],'m/d/Y');
 	}
-?>
--- php/extlib/adodb5/tests/testpear.php
+++ php/extlib/adodb5/tests/testpear.php
@@ -1,11 +1,13 @@
 <?php
-/* 
-V4.80 8 Mar 2006  (c) 2000-2012 John Lim (jlim#natsoft.com). All rights reserved.
-  Released under both BSD license and Lesser GPL library license. 
-  Whenever there is any discrepancy between the two licenses, 
-  the BSD license will take precedence. 
+/*
+@version   v5.20.3  01-Jan-2016
+@copyright (c) 2000-2013 John Lim (jlim#natsoft.com). All rights reserved.
+@copyright (c) 2014      Damien Regad, Mark Newnham and the ADOdb community
+  Released under both BSD license and Lesser GPL library license.
+  Whenever there is any discrepancy between the two licenses,
+  the BSD license will take precedence.
   Set tabs to 4 for best viewing.
-	
+
   Latest version is available at http://adodb.sourceforge.net
 */
 
@@ -31,4 +33,3 @@ while ($arr = $rs->FetchRow()) {
 }
 
 if ($cnt != 50) print "<b>Error in \$cnt = $cnt</b>";
-?>
