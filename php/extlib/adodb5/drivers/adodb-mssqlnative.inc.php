bc8127738909ac30fafde77500730dc89bb24939
Yuji Takayama <taka.swordbreaker@gmail.com>
2016-08-17 13:53:32 +0900

    Should not call ErrorNo before ErrorMsg if databse is MSSQL. Also, add support UTF8. bugid:114115

--- php/extlib/adodb5/drivers/adodb-mssqlnative.inc.php
+++ php/extlib/adodb5/drivers/adodb-mssqlnative.inc.php
@@ -127,6 +127,7 @@ class ADODB_mssqlnative extends ADOConnection {
 	var $connectionInfo = array();
 	var $sequences = false;
 	var $mssql_version = '';
+    var $is_utf = false;
 
 	function __construct()
 	{
@@ -482,7 +483,9 @@ class ADODB_mssqlnative extends ADOConnection {
 		$connectionInfo["Database"]=$argDatabasename;
 		$connectionInfo["UID"]=$argUsername;
 		$connectionInfo["PWD"]=$argPassword;
-		
+        if ( $this->is_utf )
+            $connectionInfo['CharacterSet'] = 'UTF-8';
+
 		foreach ($this->connectionParameters as $parameter=>$value)
 		    $connectionInfo[$parameter] = $value;
 		
@@ -830,6 +833,12 @@ class ADODB_mssqlnative extends ADOConnection {
 		# end adg
 		return $retarr;
 	}
+
+    function Execute($sql,$inputarr=false) {
+        if ( $this->is_utf )
+            $sql = preg_replace( '/(\'.*\')/', 'N$1', $sql);
+        return parent::Execute( $sql, $inputarr );
+    }
 
 }
 
