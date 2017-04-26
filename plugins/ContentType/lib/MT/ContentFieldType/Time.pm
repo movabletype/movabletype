05e9a067baf26d7789f75d1a4256f2cd89f58352
Masahiro Iuchi <miuchi@sixapart.com>
2017-04-26 14:21:00 +0900

    Fix time type filter

--- plugins/ContentType/lib/MT/ContentFieldType/Time.pm
+++ plugins/ContentType/lib/MT/ContentFieldType/Time.pm
@@ -2,6 +2,10 @@ package MT::ContentFieldType::Time;
 use strict;
 use warnings;
 
+use MT;
+use MT::ContentData;
+use MT::ContentField;
+use MT::ContentFieldIndex;
 use MT::Util ();
 
 sub html {
@@ -66,6 +70,114 @@ sub ss_validator {
         my $err = MT->translate( "Invalid time: '[_1]'", $time );
         return $app->error($err) if $err && $app;
     }
+}
+
+sub filter_tmpl {
+    my $prop = shift;
+
+    my $tmpl     = 'filter_form_future_time';
+    my $label    = '<mt:var name="label">';
+    my $opts     = '<mt:var name="future_blank_time_filter_options">';
+    my $contents = '<mt:var name="future_time_filter_contents">';
+
+    return MT->translate( '<mt:var name="[_1]"> [_2] [_3] [_4]',
+        $tmpl, $label, $opts, $contents );
+}
+
+sub terms {
+    my $prop = shift;
+    my ( $args, $db_terms, $db_args ) = @_;
+
+    my $option    = $args->{option};
+    my $data_type = $prop->{data_type};
+
+    my $query = generate_query( $prop, @_ );
+
+    my $join = MT::ContentFieldIndex->join_on(
+        undef,
+        { "value_${data_type}" => $query },
+        {   type      => 'left',
+            condition => {
+                content_data_id  => \'= cd_id',
+                content_field_id => $prop->content_field_id,
+            },
+        },
+    );
+
+    my @cd_ids
+        = map { $_->id }
+        MT::ContentData->load( $db_terms,
+        { join => $join, fetchonly => { id => 1 } } );
+
+    { id => @cd_ids ? \@cd_ids : 0 };
+}
+
+sub generate_query {
+    my $prop = shift;
+    my ( $args, $db_terms, $db_args ) = @_;
+
+    my $option   = $args->{option};
+    my $boundary = $args->{boundary};
+    my $now      = MT::Util::epoch2ts( undef, time );
+    my $from     = $args->{from} || '';
+    my $to       = $args->{to} || '';
+    my $origin   = $args->{origin} || '';
+    $from =~ s/\D//g;
+    $to =~ s/\D//g;
+    $origin =~ s/\D//g;
+    $from   = '19700101' . $from   if $from;
+    $to     = '19700101' . $to     if $to;
+    $origin = '19700101' . $origin if $origin;
+
+    my $query;
+    if ( 'range' eq $option ) {
+        $query = [
+            '-and',
+            { op => '>=', value => $from },
+            { op => '<=', value => $to },
+        ];
+    }
+    elsif ( 'hours' eq $option ) {
+        my $hours = $args->{hours};
+        my $origin_time
+            = substr MT::Util::epoch2ts( undef, time - $hours * 60 * 60 ), 8;
+        $query = [
+            '-and',
+            { op => '>', value => "19700101${origin_time}" },
+            { op => '<', value => $now },
+        ];
+    }
+    elsif ( 'before' eq $option ) {
+        my $op = $boundary ? '<=' : '<';
+        $query = {
+            op    => $op,
+            value => $origin,
+        };
+    }
+    elsif ( 'after' eq $option ) {
+        my $op = $boundary ? '>=' : '>';
+        $query = {
+            op    => $op,
+            value => $origin,
+        };
+    }
+    elsif ( 'future' eq $option ) {
+        $query = {
+            op    => '>',
+            value => $now
+        };
+    }
+    elsif ( 'past' eq $option ) {
+        $query = {
+            op    => '<',
+            value => $now
+        };
+    }
+    elsif ( 'blank' eq $option ) {
+        $query = \'IS NULL';
+    }
+
+    $query;
 }
 
 1;
