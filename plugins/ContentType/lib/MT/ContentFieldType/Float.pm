644870fbb0659181f5606a8120c79a2f27e3ecd3
Masahiro Iuchi <miuchi@sixapart.com>
2017-05-12 17:02:31 +0900

    Implement server-side float field validation. bugid:114440

--- plugins/ContentType/lib/MT/ContentFieldType/Float.pm
+++ plugins/ContentType/lib/MT/ContentFieldType/Float.pm
@@ -28,5 +28,55 @@ sub field_html {
     qq{<input type="number" step="${step}" name="content-field-${field_id}" class="text short" value="${value}" mt:watch-change="1" mt:raw-name="1" ${required} ${max} ${min} style="width: 8em"/>};
 }
 
+sub ss_validator {
+    my ( $app, $field_id ) = @_;
+    my $value = $app->param("content-field-${field_id}");
+    return
+        unless defined $value
+        && $value ne '';    # Do not check empty value here.
+
+    my $content_field = MT::ContentField->load($field_id);
+    my $options       = $content_field->options;
+
+    my $decimal_places = $options->{decimal_places} || 0;
+    my $field_label    = $options->{label};
+    my $max_value      = $options->{max_value};
+    my $min_value      = $options->{min_value};
+
+    if ( $value !~ /^[+\-]?\d+(\.\d+)?$/ ) {
+        return $app->errtrans( '"[_1]" field value must be float.',
+            $field_label );
+    }
+
+    if ($decimal_places) {
+        my ( $int, $frac ) = split /\./, $value;
+        if ( length $frac > $decimal_places ) {
+            my $trimmed_frac = substr $frac, 0, $decimal_places;
+            my $trimmed_value = "${int}.${trimmed_frac}";
+            if ( $trimmed_value != $value ) {
+                return $app->errtrans(
+                    '"[_1]" field value has invalid precision.',
+                    $field_label );
+            }
+        }
+    }
+
+    if ( defined $max_value && $max_value ne '' ) {
+        if ( $value > $max_value ) {
+            return $app->errtrans(
+                '"[_1]" field value must be less than or equal to [_2].',
+                $field_label, $max_value );
+        }
+    }
+
+    if ( defined $min_value && $min_value ne '' ) {
+        if ( $value < $min_value ) {
+            return $app->errtrans(
+                '"[_1]" field value must be greater than or equal to [_2]',
+                $field_label, $min_value );
+        }
+    }
+}
+
 1;
 
