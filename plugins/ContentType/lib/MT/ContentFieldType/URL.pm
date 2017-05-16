5cd943718ccf856afbe65d3cfeed012fad0589ee
Masahiro Iuchi <miuchi@sixapart.com>
2017-05-16 15:04:20 +0900

    Refactor client-side validation of url field.

--- plugins/ContentType/lib/MT/ContentFieldType/URL.pm
+++ plugins/ContentType/lib/MT/ContentFieldType/URL.pm
@@ -3,46 +3,7 @@ use strict;
 use warnings;
 
 use MT;
-use MT::ContentField;
 use MT::Util ();
-
-sub field_html {
-    my ( $app, $field_id, $value ) = @_;
-    $value = '' unless defined $value;
-
-    my $content_field = MT::ContentField->load($field_id);
-    my $options       = $content_field->options;
-    my $field_label   = $options->{label};
-    my $required      = $options->{required} ? 'required' : '';
-
-    my $error_message
-        = $app->translate( '"[_1]" field is invalid URL', $field_label );
-
-    my $html
-        = qq{<input type="text" name="content-field-${field_id}" class="text long" value="${value}" mt:watch-change="1" mt:raw-name="1" ${required}/>};
-
-    $html .= <<"__JS__";
-<script>
-(function () {
-  var url = jQuery('input[name=content-field-${field_id}]').get(0);
-
-  function validateUrl () {
-    if (jQuery.mtValidateRules['.url'](jQuery(url))) {
-      url.setCustomValidity('');
-    } else {
-      url.setCustomValidity('${error_message}');
-    }
-  }
-
-  jQuery(url).on('change', validateUrl);
-
-  validateUrl();
-})();
-</script>
-__JS__
-
-    $html;
-}
 
 sub ss_validator {
     my ( $app, $id ) = @_;
