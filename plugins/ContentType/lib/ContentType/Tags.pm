a2b21d7e4e5be014099eedb54511fbf254405600
Masahiro Iuchi <miuchi@sixapart.com>
2017-05-31 11:53:37 +0900

    Implement mt:ContentAuthorLink tag. bugid:114487

--- plugins/ContentType/lib/ContentType/Tags.pm
+++ plugins/ContentType/lib/ContentType/Tags.pm
@@ -350,4 +350,67 @@ sub _hdlr_content_author_id {
     return $a ? $a->id || '' : '';
 }
 
+sub _hdlr_content_author_link {
+    my ( $ctx, $args, $cond ) = @_;
+    my $cd = $ctx->stash('content')
+        or return $ctx->_no_content_error();
+    my $a = $cd->author;
+    return '' unless $a;
+
+    my $type = $args->{type} || '';
+
+    my $displayname = MT::Util::encode_html(
+        MT::Util::remove_html( $a->nickname || '' ) );
+    my $show_email = $args->{show_email} ? 1 : 0;
+    my $show_url;
+    $show_url = 1 unless exists $args->{show_url} && !$args->{show_url};
+
+    # Open the link in a new window if requested (with new_window="1").
+    my $target = $args->{new_window} ? ' target="_blank"' : '';
+    unless ($type) {
+        if ( $show_url && $a->url && ( $displayname ne '' ) ) {
+            $type = 'url';
+        }
+        elsif ( $show_email && $a->email && ( $displayname ne '' ) ) {
+            $type = 'email';
+        }
+    }
+    if ( $type eq 'url' ) {
+        if ( $a->url && ( $displayname ne '' ) ) {
+
+            # Add vcard properties to link if requested (with hcard="1")
+            my $hcard = $args->{show_hcard} ? ' class="fn url"' : '';
+            return sprintf qq(<a%s href="%s"%s>%s</a>), $hcard,
+                MT::Util::encode_html( $a->url ), $target, $displayname;
+        }
+    }
+    elsif ( $type eq 'email' ) {
+        if ( $a->email && ( $displayname ne '' ) ) {
+
+            # Add vcard properties to email if requested (with hcard="1")
+            my $hcard = $args->{show_hcard} ? ' class="fn email"' : '';
+            my $str = "mailto:" . MT::Util::encode_html( $a->email );
+            $str = MT::Util::spam_protect($str) if $args->{'spam_protect'};
+            return sprintf qq(<a%s href="%s">%s</a>), $hcard, $str,
+                $displayname;
+        }
+    }
+    elsif ( $type eq 'archive' ) {
+        require MT::Author;
+        if ( $a->type == MT::Author::AUTHOR() ) {
+            local $ctx->{__stash}{author} = $a;
+            local $ctx->{current_archive_type} = undef;
+            if (my $link = $ctx->invoke_handler(
+                    'archivelink', { type => 'Author' }, $cond
+                )
+                )
+            {
+                return sprintf qq{<a href="%s"%s>%s</a>}, $link, $target,
+                    $displayname;
+            }
+        }
+    }
+    return $displayname;
+}
+
 1;
