bd84c58bf6a71756f0e3d27c683a4ad5523e7fc5
Seiichiro Hazama <shazama@sixapart.com>
2017-08-01 23:46:21 +0900

    Fixed the archive_file of the contenttype category based ArchiveTypes. BugzID: 114442

--- lib/MT/ArchiveType/ContentTypeCategoryYearly.pm
+++ lib/MT/ArchiveType/ContentTypeCategoryYearly.pm
@@ -39,6 +39,61 @@ sub template_params {
 sub archive_file {
     my $obj = shift;
     my ( $ctx, %param ) = @_;
+    my $timestamp    = $param{Timestamp};
+    my $file_tmpl    = $param{Template};
+    my $blog         = $ctx->{__stash}{blog};
+    my $cat          = $ctx->{__stash}{cat} || $ctx->{__stash}{category};
+    my $content_data = $ctx->{__stash}{content};
+    my $file;
+
+    my $this_cat;
+    if ($cat) {
+        $this_cat = $cat;
+    }
+    elsif ($content_data) {
+        my @cat_cfs = MT::ContentField->load(
+            {   type            => 'categories',
+                content_type_id => $content_data->content_type_id,
+            }
+        );
+        foreach my $cat_cf (@cat_cfs) {
+            my @obj_cats = MT::ObjectCategory->load(
+                {   object_ds => 'content_field',
+                    object_id => $cat_cf->id,
+                }
+            );
+            foreach my $obj_cat (@obj_cats) {
+                my ($category) = MT::Category->load( $obj_cat->category_id );
+                push @$cat, $category;
+            }
+        }
+        $this_cat = $cat;
+    }
+    else {
+        $this_cat = undef;
+    }
+
+    if ($file_tmpl) {
+        ( $ctx->{current_timestamp}, $ctx->{current_timestamp_end} )
+            = start_end_year( $timestamp, $blog );
+        $ctx->stash( 'archive_category', $this_cat );
+        $ctx->{inside_mt_categories} = 1;
+        $ctx->{__stash}{category} = $this_cat;
+    }
+    else {
+        if ( !$this_cat ) {
+            return "";
+        }
+        my $label = '';
+        $label = dirify( $this_cat->label );
+        if ( $label !~ /\w/ ) {
+            $label = $this_cat ? "cat" . $this_cat->id : "";
+        }
+        my $start = start_end_year( $timestamp, $blog );
+        my ($year) = unpack 'A4', $start;
+        $file = sprintf( "%s/%04d/index", $this_cat->category_path, $year );
+    }
+    $file;
 }
 
 sub archive_title {
