4b931788d36396c477593dfec5e1121743861456
Masahiro Iuchi <miuchi@sixapart.com>
2017-08-03 16:55:57 +0900

    Implement dynamic mt:SiteLanguage tag and add its test. bugid:114565

---
+++ php/lib/function.mtsitelanguage.php
@@ -0,0 +1,11 @@
+<?php
+# Movable Type (r) (C) 2001-2017 Six Apart, Ltd. All Rights Reserved.
+# This code cannot be redistributed without permission from www.sixapart.com.
+# For more information, consult your Movable Type license.
+#
+# $Id$
+
+function smarty_function_mtsitelanguage($args, &$ctx) {
+    return smarty_function_mtbloglanguage($args, $ctx);
+}
+?>
