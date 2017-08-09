07dd67cf02404e3ffc05ff9117ecae77d51ff999
Masahiro Iuchi <miuchi@sixapart.com>
2017-08-09 13:45:38 +0900

    Fix failed tests

--- lib/MT/Template/Tags/Blog.pm
+++ lib/MT/Template/Tags/Blog.pm
@@ -38,6 +38,32 @@ would iterate over only the blogs with IDs 1, 12, 19, 37 and 112.
 
 =cut
 
+=head2 ChildSites
+
+A container tag which iterates over a list of all of the child sites in the
+system. You can use any of the child site tags (L<SiteName>, L<SiteURL>, etc -
+anything starting with MTSite) inside of this tag set.
+
+B<Attributes:>
+
+=over 4
+
+=item * blog_ids
+
+This attribute allows you to limit the set of child sites iterated over by
+L<Blogs>. Multiple child sites are specified in a comma-delimited fashion.
+For example:
+
+    <mt:ChildSites blog_ids="1,12,19,37,112">
+
+would iterate over only the child sites with IDs 1, 12, 19, 37 and 112.
+
+=back
+
+=for tags multiblog, loop, child sites
+
+=cut
+
 sub _hdlr_blogs {
     my ( $ctx, $args, $cond ) = @_;
     my ( %terms, %args );
