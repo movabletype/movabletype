3d9774f5cafcbb4522b93e3e5e21d29fe8c4c0d6
Seiichiro Hazama <shazama@sixapart.com>
2017-08-18 13:35:02 +0900

    Added MT::ArchiveType::ContentTypeDate::does_publish_file. BugzID: 114442

--- lib/MT/ArchiveType/ContentTypeDate.pm
+++ lib/MT/ArchiveType/ContentTypeDate.pm
@@ -7,6 +7,32 @@ package MT::ArchiveType::ContentTypeDate;
 
 use base qw( MT::ArchiveType::Date );
 
+sub archive_contents_count {
+    my $obj = shift;
+    my ( $blog, $at, $content_data, $timestamp ) = @_;
+    return $obj->SUPER::archive_contents_count(
+        {   Blog        => $blog,
+            ArchiveType => $at,
+        }
+    ) unless $content_data;
+    return $obj->SUPER::archive_contents_count(
+        {   Blog        => $blog,
+            ArchiveType => $at,
+            Timestamp   => $timestamp,
+        }
+    );
+}
+
+sub does_publish_file {
+    my $obj    = shift;
+    my %params = %{ shift() };
+
+    $obj->archive_contents_count(
+        $params{Blog},        $params{ArchiveType},
+        $params{ContentData}, $params{Start}
+    );
+}
+
 sub target_dt {
     my $archiver = shift;
     my ( $content_data, $map ) = @_;
