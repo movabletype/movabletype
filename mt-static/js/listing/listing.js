fe039d19695ebb6b1c4a06565604ca86a7046312
Masahiro Iuchi <miuchi@sixapart.com>
2017-08-21 11:02:36 +0900

    Update compiled file and remove debug code

--- mt-static/js/listing/listing.js
+++ mt-static/js/listing/listing.js
@@ -628,7 +628,7 @@ riot.tag2('list-filter-save-modal', '<div id="save-filter" class="modal fade" ta
     }.bind(this)
 });
 
-riot.tag2('list-pagination', '<nav aria-label="Page Navigation"> <ul class="pagination"> <li class="page-item"> <a href="javascript:void(0);" class="page-link" disabled="{store.page <= 1}" data-page="{store.page - 1}" onclick="{movePage}"> {trans(\'Previous\')} </a> </li> <virtual if="{store.page - 2 >= 1}"> <li class="page-item first-last"> <a href="javascript:void(0);" class="page-link" data-page="{1}" onclick="{movePage}"> 1 </a> </li> <li class="page-item" aria-hidden="true"> ... </li> </virtual> <li if="{store.page - 1 >= 1}" class="{\'page-item\': true, \'first-last\': store.page - 1 == 1}"> <a href="javascript:void(0);" class="page-link" data-page="{store.page - 1}" onclick="{movePage}"> {store.page - 1} </a> </li> <li class="page-item active"> <a class="page-link"> {store.page} <span class="sr-only">(current)</span> </a> </li> <li if="{store.page + 1 <= store.pageMax}" class="{\'page-item\': true, \'first-last\': store.page + 1 == store.pageMax}"> <a href="javascript:void(0);" class="page-link" data-page="{store.page + 1}" onclick="{movePage}"> {store.page + 1} </a> </li> <virtual if="{store.page + 2 <= store.pageMax}"> <li class="page-item" aria-hidden="true"> ... </li> <li class="page-item first-last"> <a href="javascript:void(0);" class="page-link" data-page="{store.pageMax}" onclick="{movePage}"> {store.pageMax} </a> </li> </virtual> <li class="page-item"> <a href="javascript:void(0);" class="page-link" disabled="{store.page >= store.pageMax}" data-page="{store.page + 1}" onclick="{movePage}"> {trans(\'Next\')} </a> </li> </ul> </nav>', '', '', function(opts) {
+riot.tag2('list-pagination', '<nav aria-label="{store.listClient.objectType + \' list\'}"> <ul class="pagination"> <li class="page-item"> <a href="javascript:void(0);" class="page-link" disabled="{store.page <= 1}" data-page="{store.page - 1}" onclick="{movePage}"> {trans(\'Previous\')} </a> </li> <virtual if="{store.page - 2 >= 1}"> <li class="page-item first-last"> <a href="javascript:void(0);" class="page-link" data-page="{1}" onclick="{movePage}"> 1 </a> </li> <li class="page-item" aria-hidden="true"> ... </li> </virtual> <li if="{store.page - 1 >= 1}" class="{\'page-item\': true, \'first-last\': store.page - 1 == 1}"> <a href="javascript:void(0);" class="page-link" data-page="{store.page - 1}" onclick="{movePage}"> {store.page - 1} </a> </li> <li class="page-item active"> <a class="page-link"> {store.page} <span class="sr-only">(current)</span> </a> </li> <li if="{store.page + 1 <= store.pageMax}" class="{\'page-item\': true, \'first-last\': store.page + 1 == store.pageMax}"> <a href="javascript:void(0);" class="page-link" data-page="{store.page + 1}" onclick="{movePage}"> {store.page + 1} </a> </li> <virtual if="{store.page + 2 <= store.pageMax}"> <li class="page-item" aria-hidden="true"> ... </li> <li class="page-item first-last"> <a href="javascript:void(0);" class="page-link" data-page="{store.pageMax}" onclick="{movePage}"> {store.pageMax} </a> </li> </virtual> <li class="page-item"> <a href="javascript:void(0);" class="page-link" disabled="{store.page >= store.pageMax}" data-page="{store.page + 1}" onclick="{movePage}"> {trans(\'Next\')} </a> </li> </ul> </nav>', '', '', function(opts) {
     this.mixin('listTop')
 
     this.movePage = function(e) {
@@ -648,7 +648,8 @@ riot.tag2('list-pagination', '<nav aria-label="Page Navigation"> <ul class="pagi
       if (!nextPage) {
         return false
       }
-      this.store.trigger('move_page', nextPage)
+      const moveToPagination = true
+      this.store.trigger('move_page', nextPage, moveToPagination)
       return false
     }.bind(this)
 });
@@ -709,9 +710,12 @@ riot.tag2('list-top', '<div data-is="display-options"></div> <div class="row"> <
 
     const self = this
 
-    opts.store.on('refresh_view', function () {
+    opts.store.on('refresh_view', function (moveToPagination) {
       self.update()
       self.updateSubFields()
+      if (moveToPagination) {
+        window.document.body.scrollTop = window.document.body.scrollHeight
+      }
     })
 
     this.on('mount', function () {
