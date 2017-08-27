5938a009d95df80a9a7e21cd5492c1312758f983
Yuji Takayama <taka.swordbreaker@gmail.com>
2017-08-27 11:56:58 +0900

    Reset dashboard widget list at upgrade. bugid:114592

--- lib/MT/Upgrade/v7.pm
+++ lib/MT/Upgrade/v7.pm
@@ -3,6 +3,78 @@ use strict;
 use warnings;
 
 sub upgrade_functions {
+    return {
+        'v5_reset_default_widget' => {
+            version_limit => 7.0007,
+            priority      => 5.0,
+            updater       => {
+                type  => 'author',
+                label => "Reset default widget...",
+                code  => \&_v7_reset_default_widget,
+            },
+        },
+    };
+}
+
+sub _v7_reset_default_widget {
+    my $user = shift;
+    my $widgets = $user->widgets;
+    my $new_widgets = {};
+
+    # System
+    $new_widgets->{'dashboard:system'}->{'notification_dashboard'} = {
+        order => 0,
+        set   => 'main',
+    };
+    $new_widgets->{'dashboard:system'}->{'system_information'} = {
+        order => 100,
+        set   => 'main',
+    };
+    $new_widgets->{'dashboard:system'}->{'mt_news'} = {
+        order => 0,
+        set   => 'sidebar',
+    };
+    $new_widgets->{'dashboard:system'}->{'activity_log'} = {
+        order => 100,
+        set   => 'sidebar',
+    };
+
+    # User Dashboard
+    $new_widgets->{'dashboard:user'}->{'mt_news'} = {
+        order => 0,
+        set   => 'sidebar',
+    };
+    $new_widgets->{'dashboard:user'}->{'notification_dashboard'} = {
+        order => 0,
+        set   => 'main',
+    };
+    $new_widgets->{'dashboard:user'}->{'activity_log'} = {
+        order => 100,
+        set   => 'sidebar',
+    };
+
+    # Site
+    foreach my $key ( keys %$widgets ) {
+        if ( $key =~ m/^dashboard:blog:/ ) {
+            $new_widgets->{$key}->{'site_stats'} = {
+                order => 200,
+                set   => 'main',
+            };
+            $new_widgets->{$key}->{'activity_log'} = {
+                order => 100,
+                set   => 'sidebar',
+            };
+            $new_widgets->{$key}->{'site_list'} = {
+                order => 100,
+                set   => 'main',
+            };
+
+        }
+    }
+
+    # Reset user settings
+    $user->widgets( $new_widgets );
+    $user->save;
 }
 
 1;
