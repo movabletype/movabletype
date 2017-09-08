d809c19120600e10892ff4678a361e9fe50bb9c1
Kenichi Ishigaki <ishigaki@cpan.org>
2017-09-08 09:13:59 +0900

    Fix app->param usage in MT::CMS::Common::save_snapshot

--- lib/MT/CMS/Common.pm
+++ lib/MT/CMS/Common.pm
@@ -2158,9 +2158,8 @@ sub list_revision {
 # Currently, not in use.
 sub save_snapshot {
     my $app   = shift;
-    my $q     = $app->param;
-    my $type  = $q->param('_type');
-    my $id    = $q->param('id');
+    my $type  = $app->param('_type');
+    my $id    = $app->param('id');
     my $param = {};
 
     return $app->errtrans("Invalid request.")
@@ -2200,7 +2199,7 @@ sub save_snapshot {
 
     my $original = $obj->clone();
     my $names    = $obj->column_names;
-    my %values   = map { $_ => ( scalar $q->param($_) ) } @$names;
+    my %values   = map { $_ => scalar $app->param($_) } @$names;
 
     if ( ( 'entry' eq $type ) || ( 'page' eq $type ) ) {
         $app->_translate_naughty_words($obj);
@@ -2232,7 +2231,8 @@ sub save_snapshot {
             my $max = $blog->$col;
             $obj->handle_max_revisions($max);
         }
-        my $revision = $obj->save_revision( $q->param('revision-note') );
+        my $revision_note = $app->param('revision-note');
+        my $revision      = $obj->save_revision($revision_note);
         $app->add_return_arg( r => $revision );
         if ($id) {
             my $obj_revision = $original->revision || 0;
