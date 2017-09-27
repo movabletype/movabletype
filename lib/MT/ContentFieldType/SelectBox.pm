6879b664e6cd3098204c9a3f35b4878593461bf8
Yuji Takayama <taka.swordbreaker@gmail.com>
2017-09-27 10:09:50 +0900

    Add file header. bugid:114626

--- lib/MT/ContentFieldType/SelectBox.pm
+++ lib/MT/ContentFieldType/SelectBox.pm
@@ -1,3 +1,8 @@
+# Movable Type (r) (C) 2001-2017 Six Apart, Ltd. All Rights Reserved.
+# This code cannot be redistributed without permission from www.sixapart.com.
+# For more information, consult your Movable Type license.
+#
+# $Id$
 package MT::ContentFieldType::SelectBox;
 use strict;
 use warnings;
