9eb8035f4a4532e598e1336505097a8566ebe2e8
Masahiro Iuchi <miuchi@sixapart.com>
2017-09-28 15:16:53 +0900

    Fix that saved radio_button field values are not output. bugid:114810
    https://github.com/movabletype/internal-movabletype/commit/2861cc7964c2e6338fcecdcd3c8a1bad6f877aa1

--- lib/MT/ContentFieldType/RadioButton.pm
+++ lib/MT/ContentFieldType/RadioButton.pm
@@ -10,15 +10,27 @@ use warnings;
 sub field_html_params {
     my ( $app, $field_data ) = @_;
     my $value = $field_data->{value};
-    $value = '' unless defined $value;
 
     my $options_values = $field_data->{options}{values} || [];
-    @{$options_values} = map {
-        {   l => $_->{label},
-            v => $_->{value},
-            ( $_->{checked} ? ( checked => 'checked="checked"' ) : () ),
-        }
-    } @{$options_values};
+
+    if ( defined $value ) {
+        @$options_values = map {
+            {   l => $_->{label},
+                v => $_->{value},
+                ( $_->{value} eq $value )
+                ? ( checked => 'checked="checked"' )
+                : (),
+            }
+        } @$options_values;
+    }
+    else {
+        @{$options_values} = map {
+            {   l => $_->{label},
+                v => $_->{value},
+                ( $_->{checked} ? ( checked => 'checked="checked"' ) : () ),
+            }
+        } @{$options_values};
+    }
 
     my $required = $field_data->{options}{required} ? 'required' : '';
 
