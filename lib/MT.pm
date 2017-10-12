0818e778b9c76fba3263355363006911da11dd64
Yuji Takayama <taka.swordbreaker@gmail.com>
2017-10-12 19:56:08 +0900

    Bump version. bugid:114967

--- lib/MT.pm
+++ lib/MT.pm
@@ -39,7 +39,7 @@ BEGIN {
         )
         = (
         '__PRODUCT_NAME__',   'MT',
-        '6.3.5',              '__PRODUCT_VERSION_ID__',
+        '6.3.6',              '__PRODUCT_VERSION_ID__',
         '__RELEASE_NUMBER__', '__PORTAL_URL__'
         );
 
@@ -56,7 +56,7 @@ BEGIN {
     }
 
     if ( $RELEASE_NUMBER eq '__RELEASE' . '_NUMBER__' ) {
-        $RELEASE_NUMBER = 5;
+        $RELEASE_NUMBER = 6;
     }
 
     $DebugMode = 0;
