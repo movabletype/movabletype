ae0588d371ea4ba0f131b1f894f2e40e104e1e7c
Seiichiro Hazama <shazama@sixapart.com>
2017-10-18 16:35:13 +0900

    content_type modifier is applied to only local context in mt:ArchiveList. BugzID: 114991

--- lib/MT/Template/Tags/Archive.pm
+++ lib/MT/Template/Tags/Archive.pm
@@ -243,22 +243,31 @@ sub _hdlr_archives {
     # Isn't $order used?
     #my $order = $sort_order eq 'ascend' ? 'asc' : 'desc';
 
+    my $content_type;
     if ( my $unique_id = $args->{content_type} ) {
-        my ($content_type)
+        ($content_type)
             = MT->model('content_type')->load( { unique_id => $unique_id } )
             or return $ctx->_no_content_type_error;
-        local $ctx->{__stash}{content_type} = $content_type;
     }
     my $group_iter = $archiver->archive_group_iter( $ctx, $args );
     return $ctx->error( MT->translate("Group iterator failed.") )
         unless defined($group_iter);
 
-    my ( $cnt, %curr ) = $group_iter->();
+    my ( $cnt, %curr );
+    {
+        local $ctx->{__stash}{content_type} = $content_type if $content_type;
+        ( $cnt, %curr ) = $group_iter->();
+    }
     my %save = map { $_ => $ctx->{__stash}{$_} } keys %curr;
     my $vars = $ctx->{__stash}{vars} ||= {};
     while ( defined($cnt) ) {
         $i++;
-        my ( $next_cnt, %next ) = $group_iter->();
+        my ( $next_cnt, %next );
+        {
+            local $ctx->{__stash}{content_type} = $content_type
+                if $content_type;
+            ( $next_cnt, %next ) = $group_iter->();
+        }
         my $last;
         $last = 1 if $n && ( $i >= $n );
         $last = 1 unless defined $next_cnt;
