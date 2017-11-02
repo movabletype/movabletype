6ed0e1e91db24d4788f98696288831138402fc93
Kenichi Ishigaki <ishigaki@cpan.org>
2017-11-02 12:56:56 +0900

    Add code to load/save fixture to MT::Test::Env

--- t/lib/MT/Test/Env.pm
+++ t/lib/MT/Test/Env.pm
@@ -5,6 +5,7 @@ use warnings;
 use Test::More;
 use File::Spec;
 use Cwd ();
+use Fcntl qw/:flock/;
 use File::Path 'mkpath';
 use File::Temp 'tempdir';
 use File::Basename 'dirname';
@@ -223,6 +224,240 @@ sub dbh {
             ShowErrorStatement => 1,
         }
     ) or die $DBI::errstr;
+}
+
+sub prepare_fixture {
+    my $self = shift;
+    my ( $id, $code );
+    my $fixture_dir = "$MT_HOME/t/fixture";
+    if ( ref $_[0] eq 'CODE' ) {
+        $code = shift;
+        $id   = (caller)[1];    # file
+        $id =~ s!^($MT_HOME/)?!!;
+        $id =~ s!\.t$!!;
+        $id =~ s!^(?:(.*?)/)?t/!!;
+        if ($1) {
+            $fixture_dir = "$MT_HOME/$1/t/fixture";
+            mkpath $fixture_dir unless -d $fixture_dir;
+        }
+    }
+    else {
+        $id = shift;
+        if ( $id eq 'db' ) {
+            $code = sub {
+                MT::Test->init_db;
+            };
+        }
+        elsif ( $id eq 'db_data' ) {
+            $code = sub {
+                MT::Test->init_db;
+                MT::Test->init_data;
+            };
+        }
+        else {
+            $code = shift;
+        }
+    }
+    my $driver  = lc $self->{driver};
+    my $schema  = "$MT_HOME/t/fixture/schema.$driver.sql";
+    my $fixture = "$fixture_dir/$id.json";
+    if ( !$ENV{MT_TEST_UPDATE_FIXTURE} and !$ENV{MT_TEST_IGNORE_FIXTURE} ) {
+        $self->load_schema_and_fixture( $schema, $fixture ) or $code->();
+    }
+    else {
+        $code->();
+    }
+    if ( $ENV{MT_TEST_UPDATE_FIXTURE} ) {
+        $self->save_schema($schema);
+        $self->save_fixture($fixture);
+    }
+
+    require MT::Theme;
+    my $blog = MT::Blog->load(1);
+    if ($blog) {
+        MT::Theme->load('classic_blog');
+    }
+    else {
+        MT::Theme->load('classic_website');
+    }
+
+    $ENV{MT_TEST_LOADED_FIXTURE} = 1;
+}
+
+sub _slurp {
+    my $file = shift;
+    open my $fh, '<', $file or die "$file: $!";
+    local $/;
+    <$fh>;
+}
+
+sub load_schema_and_fixture {
+    my ( $self, $schema_file, $fixture_file ) = @_;
+    return unless -f $schema_file;
+    return unless -f $fixture_file;
+    return
+        unless
+        eval { require SQL::Maker; SQL::Maker->load_plugin('InsertMulti'); 1 };
+    my $root = $self->{root};
+    my ( $s, $m, $h, $d, $mo, $y ) = gmtime;
+    my $now = sprintf( "%04d%02d%02d%02d%02d%02d",
+        $y + 1900, $mo + 1, $d, $h, $m, $s );
+    my $schema  = _slurp($schema_file)  or return;
+    my $fixture = _slurp($fixture_file) or return;
+    $fixture =~ s/\b__MT_HOME__\b/$MT_HOME/g;
+    $fixture =~ s/\b__TEST_ROOT__\b/$root/g;
+    $fixture =~ s/\b__NOW__\b/$now/g;
+    require JSON;
+    $fixture = eval { JSON::decode_json($fixture) };
+
+    if ( $@ or !%$fixture ) {
+        warn "Fixture is empty or broken";
+        return;
+    }
+    my $dbh = $self->dbh;
+    $dbh->begin_work;
+    eval {
+        for my $sql ( split /;\n/s, $schema ) {
+            chomp $sql;
+            next unless $sql;
+            $dbh->do($sql);
+        }
+        my $sql_maker = SQL::Maker->new( driver => $self->{driver} );
+        for my $table ( keys %$fixture ) {
+            my $data = $fixture->{$table};
+            my ( $sql, @bind )
+                = $sql_maker->insert_multi( $table, @$data{qw/cols rows/} );
+            $sql =~ s/\n/ /g;
+            $dbh->do( $sql, undef, @bind );
+        }
+    };
+    if ($@) {
+        warn $@;
+        $dbh->rollback;
+        return;
+    }
+    $dbh->commit;
+
+    # for init_db
+    require MT::Test;
+    MT::Test->_load_classes;
+
+    return 1;
+}
+
+sub save_schema {
+    my ( $self, $file ) = @_;
+    eval { require SQL::Translator } or return;
+
+    # skip if updated quite recently
+    # return if -f $file and (stat($file))[9] - time < 300;
+
+    my $driver = lc $self->{driver};
+    my $dbh    = $self->dbh;
+    my %translator_args;
+    if ( $driver eq 'mysql' ) {
+        %translator_args = (
+            filters       => [ \&_sql_translator_filter_mysql ],
+            producer      => 'MySQL',
+            producer_args => { mysql_version => 5 },
+        );
+    }
+    my $translator = SQL::Translator->new(
+        add_drop_table => 1,
+        parser         => 'DBI',
+        parser_args    => { dbh => $dbh },
+        %translator_args,
+    );
+    my $schema = $translator->translate or die $translator->error;
+
+    my $dir = dirname($file);
+    mkpath $dir unless -d $dir;
+    open my $fh, '>', $file or die $!;
+    flock $fh, LOCK_EX;
+    print $fh $schema;
+    close $fh;
+}
+
+sub _sql_translator_filter_mysql {
+    my $schema = shift;
+    for my $table ( $schema->get_tables ) {
+        my $options = $table->options;
+        my $i       = 0;
+        while ( $i < @$options ) {
+            my ( $key, $value ) = %{ $options->[$i] };
+            $options->[$i]{$key} = 'utf8' if $key eq 'CHARACTER SET';
+            splice @$options, $i, 1 if $key eq 'AUTO_INCREMENT';
+            $i++;
+        }
+
+        # Some of the PHP tests assume that float has no explicit size
+        for my $field ( $table->get_fields ) {
+            if ( lc $field->data_type eq 'float' ) {
+                $field->size(0);
+            }
+        }
+    }
+}
+
+sub save_fixture {
+    my ( $self, $file ) = @_;
+    eval { require Time::Piece; require Time::Seconds; 1 } or return;
+
+    my $driver = lc $self->{driver};
+    my $dbh    = $self->dbh;
+    my @tables;
+    if ( $driver eq 'mysql' ) {
+        @tables = map { $_->[0] } $dbh->selectall_array('SHOW TABLES');
+    }
+    my $root = $self->{root};
+    my %data;
+    for my $table (@tables) {
+        my $rows = $dbh->selectall_arrayref( "SELECT * FROM $table",
+            { Slice => +{} } );
+        next unless @{ $rows || [] };
+        my @keys = keys %{ $rows->[0] };
+        my @rows_modified;
+        for my $row (@$rows) {
+            my @data;
+            for my $key (@keys) {
+                my $value = $row->{$key};
+                if ( defined $value ) {
+                    if ( $key =~ /(?:created|modified)_on$/ ) {
+                        my $t = Time::Piece->strptime( $value,
+                            '%Y-%m-%d %H:%M:%S' );
+                        my $now = Time::Piece->new;
+                        if ( $now - $t < Time::Seconds::ONE_DAY() ) {
+                            $value = '__NOW__';
+                        }
+                    }
+                    else {
+                        $value =~ s/^$root/__TEST_ROOT__/;
+                        $value =~ s/^$MT_HOME/__MT_HOME__/;
+                    }
+                }
+                push @data, $value;
+            }
+            push @rows_modified, \@data;
+        }
+        $data{$table} = {
+            cols => \@keys,
+            rows => \@rows_modified,
+        };
+    }
+
+    if ( !%data ) {
+        Carp::cluck "Fixture is empty";
+        unlink $file if -f $file;
+        return;
+    }
+
+    require JSON;
+    my $dir = dirname($file);
+    mkpath $dir unless -d $dir;
+    open my $fh, '>', $file or die $!;
+    flock $fh, LOCK_EX;
+    print $fh JSON->new->pretty->canonical->utf8->encode( \%data );
+    close $fh;
 }
 
 1;
