1316ed97d0e0ceeb98a5b7f2dc131a81d94554be
Kenichi Ishigaki <ishigaki@cpan.org>
2017-11-15 12:06:14 +0900

    Use different fixtures depending on the available addons and plugins

--- t/lib/MT/Test/Env.pm
+++ t/lib/MT/Test/Env.pm
@@ -10,6 +10,7 @@ use File::Path 'mkpath';
 use File::Temp 'tempdir';
 use File::Basename 'dirname';
 use DBI;
+use Digest::MD5 'md5_hex';
 
 our $MT_HOME;
 
@@ -233,13 +234,17 @@ sub dbh {
 sub prepare_fixture {
     my $self = shift;
     my $code;
-    my $fixture_dir = "$MT_HOME/t/fixture";
+    my @addons = _find_addons_and_plugins();
+    my $uid    = substr( md5_hex( join '+', sort @addons ), 0, 7 );
+
+    my $fixture_dir = "$MT_HOME/t/fixture/$uid";
+
     my $id = (caller)[1];    # file
     $id =~ s!^($MT_HOME/)?!!;
     $id =~ s!\.t$!!;
     $id =~ s!^(?:(.*?)/)?t/!!;
     if ($1) {
-        $fixture_dir = "$MT_HOME/$1/t/fixture";
+        $fixture_dir = "$MT_HOME/$1/t/fixture/$uid";
         mkpath $fixture_dir unless -d $fixture_dir;
     }
     if ( ref $_[0] eq 'CODE' ) {
@@ -272,6 +277,10 @@ sub prepare_fixture {
         $code->();
     }
     if ( $ENV{MT_TEST_UPDATE_FIXTURE} ) {
+        mkpath $fixture_dir unless -d $fixture_dir;
+        open my $fh, '>', "$fixture_dir/README" or die $!;
+        print $fh join "\n", @addons, "";
+        close $fh;
         $self->save_schema($schema);
         $self->save_fixture($fixture);
     }
@@ -293,6 +302,14 @@ sub _slurp {
     open my $fh, '<', $file or die "$file: $!";
     local $/;
     <$fh>;
+}
+
+sub _find_addons_and_plugins {
+    my @files;
+    push @files, glob "$MT_HOME/addons/*/config.yaml";
+    push @files, glob "$MT_HOME/plugins/*/*.pl";
+    my %seen;
+    grep {!$seen{$_}++} map {$_ =~ m!/((?:addons|plugins)/[^/]+)/!; $1} @files;
 }
 
 sub load_schema_and_fixture {
