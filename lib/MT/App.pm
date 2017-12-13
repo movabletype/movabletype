356ff7609fcae2c75d461f2eaf32c8d50a40c0b5
Yuji Takayama <ytakayama@sixapart.com>
2017-12-13 10:05:40 +0900

    MTC-10795: Should escape redirect url.

--- lib/MT/App.pm
+++ lib/MT/App.pm
@@ -3305,7 +3305,7 @@ sub run {
         if ( $app->{redirect_use_meta} ) {
             $app->send_http_header();
             $app->print( '<meta http-equiv="refresh" content="0;url='
-                    . $app->{redirect}
+                    . encode_html($app->{redirect})
                     . '">' );
         }
         else {
