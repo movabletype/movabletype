43915e0cc0df0ef8ab0829ed118a6750a610316d
Masahiro Iuchi <miuchi@sixapart.com>
2018-01-16 13:53:54 +0900

    Revert "Remove list_actions for blog. MTC-24991"
    This reverts commit 901f3700b96f72d9b17b768875619abc71b1d439.

--- lib/MT/App/CMS.pm
+++ lib/MT/App/CMS.pm
@@ -1129,7 +1129,12 @@ sub core_list_actions {
                 code          => "${pkg}Website::dialog_move_blogs",
                 permit_action => 'move_blogs',
                 dialog        => 1,
-                condition     => sub {0},
+                condition     => sub {
+                    return 0 if $app->mode eq 'view';
+
+                    my $count = MT->model('website')->count();
+                    $count > 1 ? 1 : 0;
+                }
             },
             clone_blog => {
                 label         => "Clone Child Site",
@@ -1137,7 +1142,6 @@ sub core_list_actions {
                 permit_action => 'clone_blog',
                 max           => 1,
                 dialog        => 1,
-                condition     => sub {0},
             },
             'delete' => {
                 label      => 'Delete',
