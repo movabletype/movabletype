316916394c89041938bfca895c4123dbf56f9292
Masahiro Iuchi <miuchi@sixapart.com>
2018-01-22 15:20:39 +0900

    Permit edit_commenter action. MTC-24994

--- lib/MT/App/CMS.pm
+++ lib/MT/App/CMS.pm
@@ -2260,6 +2260,7 @@ sub core_enable_object_methods {
             edit   => sub { $app->param('id') ? 1 : 0 },
             save   => sub { $app->param('id') ? 1 : 0 },
         },
+        commenter    => { edit   => 1, },
         content_type => { delete => 1, },
         entry        => { edit   => 1 },
         page         => {
