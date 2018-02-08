f320dd72769abff86eea6bcf225f2fbc1e616c5d
Yuji Takayama <ytakayama@sixapart.com>
2018-02-08 10:17:53 +0900

    MTC-24980: Add file header comment.

--- lib/MT/DataAPI/Endpoint/v4/Publish.pm
+++ lib/MT/DataAPI/Endpoint/v4/Publish.pm
@@ -1,3 +1,9 @@
+# Movable Type (r) (C) 2001-2018 Six Apart, Ltd. All Rights Reserved.
+# This code cannot be redistributed without permission from www.sixapart.com.
+# For more information, consult your Movable Type license.
+#
+# $Id$
+
 package MT::DataAPI::Endpoint::v4::Publish;
 use strict;
 use warnings;
