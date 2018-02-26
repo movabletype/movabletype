85d8e9281224f22112bf9961d544c956ae74bd9f
Yuji Takayama <ytakayama@sixapart.com>
2018-02-26 23:59:33 +0900

    MTC-24977: Bump version.

--- lib/MT.pm
+++ lib/MT.pm
@@ -63,7 +63,7 @@ BEGIN {
     }
 
     if ( $RELEASE_VERSION_ID eq '__RELEASE' . '_VERSION_ID__' ) {
-        $RELEASE_VERSION_ID = 'r.2401';
+        $RELEASE_VERSION_ID = 'r.2602';
     }
 
     $DebugMode = 0;
