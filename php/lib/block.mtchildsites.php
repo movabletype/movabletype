d937cd072f847a0f01bc83edb2e1c9745c832dbd
Yuji Takayama <ytakayama@sixapart.com>
2018-03-06 20:48:51 +0900

    Fix wrong merge.

--- php/lib/block.mtchildsites.php
+++ php/lib/block.mtchildsites.php
@@ -1,3 +1,4 @@
+<?php
 # Movable Type (r) (C) 2001-2018 Six Apart, Ltd. All Rights Reserved.
 # This code cannot be redistributed without permission from www.sixapart.com.
 # For more information, consult your Movable Type license.
