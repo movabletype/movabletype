f36ffc2c151979458907607b3267149892686308
Yuji Takayama <ytakayama@sixapart.com>
2018-04-02 08:44:18 +0900

    Bump version.

--- lib/MT.pm
+++ lib/MT.pm
@@ -63,7 +63,7 @@ BEGIN {
     }
 
     if ( $RELEASE_VERSION_ID eq '__RELEASE' . '_VERSION_ID__' ) {
-        $RELEASE_VERSION_ID = 'r.3603';
+        $RELEASE_VERSION_ID = 'r.3604';
     }
 
     $DebugMode = 0;
