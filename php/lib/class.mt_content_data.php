1d28a4713bd9bbd432ec72aa87a75c653e067663
Ryota Masuko <r.g.masuko@gmail.com>
2018-07-13 14:03:32 +0900

    Add mtcontentlabel for dynamic. MTC-25185

--- php/lib/class.mt_content_data.php
+++ php/lib/class.mt_content_data.php
@@ -14,6 +14,57 @@ class ContentData extends BaseObject
 {
     public $_table = 'mt_cd';
     protected $_prefix = "cd_";
+    protected $_has_meta = true;
+
+    public function label( $label ) {
+        if ( $this->id ){
+            $ct = $this->content_type();
+            if ( $ct->data_label ) {
+                $field = $this->get_content_field( $ct->data_label );
+                if( empty( $field ) ) {
+                    return $ctx->error("Cannot load content field (UniqueID:'$ct->data_label').");
+                }
+                $data_array = $this->data();
+                return $data_array[ $field->id ];
+            } else {
+                return $this->label;
+            }
+        }
+        return '';
+    }
+    public function data() {
+        $raw_data = $this->data;
+        if ( empty($raw_data) ) return array();
+        if ( preg_match( '/^SERG/', $raw_data, $matches ) ) {
+          $mt = MT::get_instance();
+          $this->_data = $mt->db()->unserialize( $raw_data );
+          return $this->_data;
+        } else {
+          if (function_exists('json_encode')) {
+              return json_encode( $raw_data );
+          }
+        }
+        return array();
+    }
+    public function content_type() {
+      $where = "content_type_id = " . $this->content_type_id;
+
+      require_once('class.mt_content_type.php');
+      $ct = new ContentType();
+      $ct->Load($where);
+      return $ct;
+    }
+
+    private function get_content_field( $unique_id ) {
+      $where = "cf_content_type_id = " . $this->content_type_id;
+      $where .= " and cf_unique_id = '$unique_id'";
+
+      require_once('class.mt_content_field.php');
+      $cf = new ContentField();
+      $cf->Load($where);
+      return $cf;
+    }
+
 }
 
 
--- t/mt7/tag/content-label.t
+++ t/mt7/tag/content-label.t
@@ -15,8 +15,8 @@ BEGIN {
 
 use MT::Test::Tag;
 
-# plan tests => 2 * blocks;
-plan tests => 1 * blocks;
+plan tests => 2 * blocks;
+# plan tests => 1 * blocks;
 
 use MT;
 use MT::Test;
@@ -95,7 +95,7 @@ print "\n";
 
 MT::Test::Tag->run_perl_tests($blog_id);
 
-# MT::Test::Tag->run_php_tests($blog_id);
+MT::Test::Tag->run_php_tests($blog_id);
 
 __END__
 
