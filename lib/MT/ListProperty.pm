5ab2fc18e38fc2793996c46a6a2e3e1199274b8f
Masahiro Iuchi <miuchi@sixapart.com>
2018-07-25 14:34:24 +0900

    Enable to configure common list_properties for content_data by plugin. MTC-25710

--- lib/MT/ListProperty.pm
+++ lib/MT/ListProperty.pm
@@ -283,6 +283,7 @@ sub list_properties {
             $props{$key} = $prop;
         }
     }
+
     my $common_props = MT->registry( 'list_properties', '__common' );
     if ($common_props) {
         for my $key ( keys %$common_props ) {
@@ -294,6 +295,21 @@ sub list_properties {
             $props{$key} = $prop if $prop;
         }
     }
+
+    if ( $cls =~ /^content_data\.content_data_[0-9]+$/ ) {
+        my $content_data_common_props
+            = MT->registry( 'list_properties', 'content_data' );
+        if ($content_data_common_props) {
+            for my $key ( keys %$content_data_common_props ) {
+                my $prop = MT::ListProperty->instance( 'content_data', $key );
+                if ( $prop->has('condition') ) {
+                    next unless $prop->condition;
+                }
+                $props{$key} = $prop if $prop;
+            }
+        }
+    }
+
     return \%props;
 }
 
