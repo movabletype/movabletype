0868fb9871185811765707879adbde98b264bc77
Kenichi Ishigaki <ishigaki@cpan.org>
2018-07-25 12:05:23 +0900

    Implement ContentAuthor* tags (except for ContentAuthorLink)

---
+++ php/lib/function.mtcontentauthorid.php
@@ -0,0 +1,18 @@
+<?php
+# Movable Type (r) (C) 2001-2018 Six Apart, Ltd. All Rights Reserved.
+# This code cannot be redistributed without permission from www.sixapart.com.
+# For more information, consult your Movable Type license.
+#
+# $Id$
+
+function smarty_function_mtcontentauthorid($args, &$ctx) {
+    $content = $ctx->stash('content');
+    if (!isset($content))
+        return $ctx->error($ctx->mt->translate(
+            "You used an '[_1]' tag outside of the context of a content; Perhaps you mistakenly placed it outside of an 'MTContents' container tag?", "mtContentAuthorID" ));
+
+    $id = $content->cd_author_id;
+    if (!$id) return '';
+    return $id;
+}
+?>
