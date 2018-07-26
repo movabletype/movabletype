ed3a41d7e636a12568c2fac1fde1e08385f13f87
Ryota Masuko <r.g.masuko@gmail.com>
2018-07-26 14:00:43 +0900

    Add MT:ContentsHeader for dynamic. MTC-25690

---
+++ php/lib/block.mtcontentsheader.php
@@ -0,0 +1,15 @@
+<?php
+# Movable Type (r) (C) 2001-2018 Six Apart, Ltd. All Rights Reserved.
+# This code cannot be redistributed without permission from www.sixapart.com.
+# For more information, consult your Movable Type license.
+#
+# $Id$
+
+function smarty_block_mtcontentsheader($args, $content, &$ctx, &$repeat) {
+    if (!isset($content)) {
+        return $ctx->_hdlr_if($args, $content, $ctx, $repeat, $ctx->stash('_contents_counter') == 1);
+    } else {
+        return $ctx->_hdlr_if($args, $content, $ctx, $repeat);
+    }
+}
+?>
