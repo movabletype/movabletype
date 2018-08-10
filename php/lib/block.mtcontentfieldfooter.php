1a646456f3e18dfbc8313fe4cf24b7bbaba49538
Kenichi Ishigaki <ishigaki@cpan.org>
2018-08-10 12:56:49 +0900

    Implement ContentField tags

---
+++ php/lib/block.mtcontentfieldfooter.php
@@ -0,0 +1,21 @@
+<?php
+# Movable Type (r) (C) 2001-2018 Six Apart, Ltd. All Rights Reserved.
+# This code cannot be redistributed without permission from www.sixapart.com.
+# For more information, consult your Movable Type license.
+#
+# $Id$
+
+function smarty_block_mtcontentfieldfooter($args, $content, &$ctx, &$repeat) {
+    if (!isset($content)) {
+        $ctx->localize(array('conditional', 'else_content'));
+        $ctx->stash('conditional', $ctx->stash('ContentFieldFooter') == 1);
+        $ctx->stash('else_content', null);
+    } else {
+        if (!$ctx->stash('conditional')) {
+            $content = $ctx->stash('else_content');
+        }
+        $ctx->restore(array('conditional', 'else_content'));
+    }
+    return $content;
+}
+?>
