09a6d4bdf43f628ba6754ec6d7ffc92afab3e730
Masahiro Iuchi <miuchi@sixapart.com>
2018-09-10 13:05:13 +0900

    [ci skip] Fix highlight problem of single-select asset field. MTC-25883

--- mt-static/js/list_table.js
+++ mt-static/js/list_table.js
@@ -1,6 +1,11 @@
 function toggleSelectedRow(checkbox) {
   var $tr = jQuery(checkbox).parents('tr');
   var highlightClass = 'mt-table__highlight';
+
+  if (checkbox.type == 'radio') {
+    $tr.parents('tbody').find('tr').removeClass(highlightClass);
+  }
+
   if (checkbox.checked) {
     $tr.addClass(highlightClass);
   } else {
