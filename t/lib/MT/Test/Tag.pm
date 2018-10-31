f39a915cf7d479e6d8cad629af3543dcf9e2eda4
Kenichi Ishigaki <ishigaki@cpan.org>
2018-10-31 15:27:45 +0900

    Check if $expected_method is defined

--- t/lib/MT/Test/Tag.pm
+++ t/lib/MT/Test/Tag.pm
@@ -101,7 +101,7 @@ SKIP: {
                 my $expected
                     = $block->expected_error ? $block->expected_error
                     : $block->error          ? $block->error
-                    : $block->can($expected_method)
+                    : ( $expected_method && $block->can($expected_method) )
                     ? $block->$expected_method
                     : $block->expected;
                 $expected =~ s/\\r/\\n/g;
