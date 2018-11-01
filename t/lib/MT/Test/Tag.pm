0edc0fd902f958a05fae3315f363b9ecc64f0ddb
Masahiro Iuchi <miuchi@sixapart.com>
2018-11-01 19:28:21 +0900

    Do not use overridden $expected_method

--- t/lib/MT/Test/Tag.pm
+++ t/lib/MT/Test/Tag.pm
@@ -25,11 +25,11 @@ sub vars {
 }
 
 sub run_perl_tests {
-    my ( $blog_id, $callback, $expected_method ) = @_;
+    my ( $blog_id, $callback, $original_expected_method ) = @_;
 
     if ( $callback && !ref $callback ) {
-        $expected_method = $callback;
-        $callback        = undef;
+        $original_expected_method = $callback;
+        $callback                 = undef;
     }
 
     MT->instance;
@@ -53,6 +53,7 @@ sub run_perl_tests {
 
             $callback->( $ctx, $block ) if $callback;
 
+            my $expected_method = $original_expected_method;
             if ( !$expected_method or !exists $block->{$expected_method} ) {
                 $expected_method = 'expected';
             }
