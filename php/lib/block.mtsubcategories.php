eb208c37ec62e4bbd968ec318f5ac98d8b383504
Ryota Masuko <r.g.masuko@gmail.com>
2018-12-26 18:54:27 +0900

    Fix category_set_id. MTC-26239

--- php/lib/block.mtsubcategories.php
+++ php/lib/block.mtsubcategories.php
@@ -28,7 +28,7 @@ function smarty_block_mtsubcategories($args, $content, &$ctx, &$repeat) {
         if ($ctx->stash('category_set')) {
             $category_set_id = $ctx->stash('category_set')->id;
         } else {
-            $category_set_id = $args['category_set_id'] || 0;
+            $category_set_id = $args['category_set_id'] ? $args['category_set_id'] : 0;
         }
 
         # Store the tokens for recursion
