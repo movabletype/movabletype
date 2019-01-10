a2cfeab3f326278caa89e5b03ebc1feaf452e899
Masahiro Iuchi <miuchi@sixapart.com>
2019-01-10 21:23:58 +0900

    Fix wrong TemplateMap parameter is set. MTC-26179

--- lib/MT/CMS/Blog.pm
+++ lib/MT/CMS/Blog.pm
@@ -912,6 +912,7 @@ sub rebuild_pages {
         if ( !$special ) {
             return $app->permission_denied()
                 unless $perms->can_do('rebuild');
+            my $map;
             if ($template_id) {
                 my $tmpl = MT->model('template')->load($template_id)
                     or return $app->errtrans( 'Cannot load template #[_1].',
@@ -921,7 +922,7 @@ sub rebuild_pages {
                     ->can_do('rebuild');
             }
             elsif ($map_id) {
-                my $map = MT->model('templatemap')->load($map_id)
+                $map = MT->model('templatemap')->load($map_id)
                     or return $app->errtrans( 'Cannot load template #[_1].',
                     $map_id );
                 return $app->permission_denied()
@@ -951,7 +952,7 @@ sub rebuild_pages {
                     $no_static ? ( NoStatic => 1 ) : (),
                     $template_id ? ( TemplateID => $template_id, Force => 1 )
                     : (),
-                    $map_id ? ( TemplateMap => $template_id, Force => 1 )
+                    $map ? ( TemplateMap => $map, Force => 1 )
                     : (),
                 ) or return $app->publish_error();
                 $offset += $count;
