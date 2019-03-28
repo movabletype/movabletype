ffe7b6d1fae7c5672e299cea637955993360f47d
Kenichi Ishigaki <ishigaki@cpan.org>
2019-03-29 03:55:43 +0900

    Store a field basename in $term->{field_name} instead of prepending $term->{term} for clarity

--- lib/MT/App/Search.pm
+++ lib/MT/App/Search.pm
@@ -1072,7 +1072,7 @@ sub _query_parse_core {
 
             # MTC-25640: Restore field__name
             if ( $term->{field} =~ s/^field__(.+)/field/ ) {
-                $term->{term} = $1 . ':' . $term->{term};
+                $term->{field_name} = $1;
             }
             unless ( exists $columns->{ $term->{field} } ) {
                 if (   $filter_types
@@ -1293,8 +1293,7 @@ sub _join_field {
         $query =~ s/'/"/g;
     }
 
-    my ( $basename, $val ) = split ':', $query, 2;
-    return unless $basename && $val;
+    my $basename = $term->{field_name};
 
     require MT::Meta;
     my $field_basename = 'field.' . $basename;
