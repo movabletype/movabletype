2804e0a22b969e9930056b8479342183cc201192
Yuji Takayama <taka.swordbreaker@gmail.com>
2019-04-03 09:42:48 +0900

    [MTC-26479] Bump version.

--- lib/MT.pm
+++ lib/MT.pm
@@ -42,7 +42,7 @@ BEGIN {
         )
         = (
         '__PRODUCT_NAME__',   'MT',
-        '7.1.1',                '__PRODUCT_VERSION_ID__',
+        '7.1.2',                '__PRODUCT_VERSION_ID__',
         '__RELEASE_NUMBER__', '__PORTAL_URL__',
         '__RELEASE_VERSION_ID__',
         );
@@ -60,11 +60,11 @@ BEGIN {
     }
 
     if ( $RELEASE_NUMBER eq '__RELEASE' . '_NUMBER__' ) {
-        $RELEASE_NUMBER = 1;
+        $RELEASE_NUMBER = 2;
     }
 
     if ( $RELEASE_VERSION_ID eq '__RELEASE' . '_VERSION_ID__' ) {
-        $RELEASE_VERSION_ID = 'r.4503';
+        $RELEASE_VERSION_ID = 'r.4601';
     }
 
     $DebugMode = 0;
