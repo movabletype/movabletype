c5a95e43579c000c3d87394a7962ddf61581dc09
Masahiro Iuchi <miuchi@sixapart.com>
2019-05-09 13:32:24 +0900

    Write build/perltidy.pl

---
+++ build/perltidy.pl
@@ -0,0 +1,42 @@
+use strict;
+use warnings;
+
+use File::Find;
+use Getopt::Long;
+
+GetOptions( "dryrun" => \my $dryrun );
+
+chdir '../' if -e './perltidy.pl';
+
+perltidy($_) for get_cgi_files();
+find( { wanted => \&wanted, follow => 1, no_chdir => 1 }, get_perl_dirs() );
+
+sub wanted {
+    if ( is_perl_file($File::Find::name) ) {
+        perltidy($File::Find::name);
+    }
+}
+
+sub get_cgi_files {
+    grep { $_ !~ /mt-config\.cgi\z/ } glob '*.cgi';
+}
+
+sub get_perl_dirs {
+    my @perl_dirs = ( 'lib', 'tools', 'plugins', 't' );
+    push @perl_dirs, 'addons' if -e 'addons';
+    @perl_dirs;
+}
+
+sub is_perl_file {
+    my ($file) = @_;
+    return 0 if $file =~ m|/L10N/|;
+    return 1 if $file =~ /\.(?:cgi|pl|pm)\z/ || $file =~ m|tools/|;
+    0;
+}
+
+sub perltidy {
+    my ($file) = @_;
+    print "perltidy $file\n";
+    `perltidy -pbp -nst -b -bext='/' $file` unless $dryrun;
+}
+
