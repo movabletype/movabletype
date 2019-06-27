4f53b8c62b8d8d948ee1cc3b6e547778ca11c6a0
Ryota Masuko <r.g.masuko@gmail.com>
2019-06-27 09:24:46 +0900

    add last status. MTC-25471

--- lib/MT/FileMgr/FTPS.pm
+++ lib/MT/FileMgr/FTPS.pm
@@ -41,10 +41,15 @@ sub init {
     }
 
     my $ftp = $fmgr->{ftp} = Net::FTPSSL->new( $_[0], %options )
-        or return $fmgr->error("FTPS connection failed: $@");
+        or return $fmgr->error("FTPS connection failed: " . $Net::FTPSSL::ERRSTR);
     $ftp->login( @_[ 1, 2 ] )
         or return $fmgr->error( 'FTPS login failed: ' . $ftp->message );
     $fmgr;
+}
+
+sub last_status {
+  my $fmgr = shift;
+  $fmgr->{ftp}->last_status_code();
 }
 
 1;
