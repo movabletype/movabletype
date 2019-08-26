53d05febcd1a9928dc29f1ccb3d0eed177457f5b
Kenichi Ishigaki <ishigaki@cpan.org>
2019-08-26 23:35:55 +0900

    Validate content type not only when archive type is content type based but also when archive type is content type group based

--- lib/MT/ContentPublisher.pm
+++ lib/MT/ContentPublisher.pm
@@ -1676,7 +1676,7 @@ sub _rebuild_content_archive_type {
     for my $map (@map) {
 
         # SKIP no relation content data
-        if ( $at eq 'ContentType' ) {
+        if ( $at =~ /^ContentType/ ) {
             my $template = MT->model('template')->load( $map->template_id );
             next
                 if $template
