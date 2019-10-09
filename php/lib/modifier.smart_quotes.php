b5d8261ada9f7aea745e4b0f56ecfce6c37aaa61
mchino <mchino@sixapart.com>
2019-10-09 13:13:45 +0900

    Match perl specification. MTC-26873

--- php/lib/modifier.smart_quotes.php
+++ php/lib/modifier.smart_quotes.php
@@ -3,24 +3,7 @@
 function smarty_modifier_smart_quotes($text, $attr = NULL, $cxt = NULL)
 {
     require_once "smartypants.php";
-    $smart_quotes = SmartQuotes($text, $attr, $cxt);
-
-    $result = str_replace(array(
-        '&#92;',
-        '&#34;',
-        '&#39;',
-        '&#46;',
-        '&#45;',
-        '&#96;'
-    ), array(
-        '\\',
-        '\"',
-        "\'",
-        '\.',
-        '\-',
-        '\`'
-    ), $smart_quotes);
-    return $result;
+    return SmartQuotes($text, $attr, $cxt);
 }
 
 ?>
