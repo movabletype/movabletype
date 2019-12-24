7924b0413821a9697e719c2008576862ae22dc68
Kenichi Ishigaki <ishigaki@cpan.org>
2019-12-24 10:42:06 +0900

    Add MT::Util::Digest:: modules

---
+++ lib/MT/Util/Digest/MD5.pm
@@ -0,0 +1,27 @@
+# Movable Type (r) (C) 2001-2019 Six Apart Ltd. All Rights Reserved.
+# This code cannot be redistributed without permission from www.sixapart.com.
+# For more information, consult your Movable Type license.
+#
+# $Id$
+
+package MT::Util::Digest::MD5;
+
+use strict;
+use warnings;
+
+our ( @ISA, @EXPORT_OK );
+BEGIN {
+    if ( eval { require Digest::MD5 } ) {
+        push @ISA, 'Digest::MD5';
+        @EXPORT_OK = @Digest::MD5::EXPORT_OK;
+        Digest::MD5->import(@EXPORT_OK);
+    }
+    else {
+        require Digest::Perl::MD5;
+        push @ISA, 'Digest::Perl::MD5';
+        @EXPORT_OK = @Digest::Perl::MD5::EXPORT_OK;
+        Digest::Perl::MD5->import(@EXPORT_OK);
+    }
+}
+
+1;
