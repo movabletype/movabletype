34f7cf2eefaed7961e81628860f189349cdba1d0
Seiichiro Hazama <shazama@sixapart.com>
2020-01-01 03:32:46 +0900

    Happy New Year 2020 (#1139)

--- lib/MT.pm
+++ lib/MT.pm
@@ -1,4 +1,4 @@
-# Movable Type (r) (C) 2001-2019 Six Apart Ltd. All Rights Reserved.
+# Movable Type (r) (C) 2001-2020 Six Apart Ltd. All Rights Reserved.
 # This code cannot be redistributed without permission from www.sixapart.com.
 # For more information, consult your Movable Type license.
 #
@@ -3932,7 +3932,7 @@ Movable Type.
 
 =head1 AUTHOR & COPYRIGHT
 
-Except where otherwise noted, MT is Copyright 2001-2019 Six Apart.
+Except where otherwise noted, MT is Copyright 2001-2020 Six Apart.
 All rights reserved.
 
 =cut
