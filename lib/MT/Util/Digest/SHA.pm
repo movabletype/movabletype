3aa83bfe1b8666dd78a1c9b648fd742a6f57ac60
Kenichi Ishigaki <ishigaki@cpan.org>
2020-01-21 09:59:15 +0900

    Use Digest::SHA instead of Digest::SHA1

--- lib/MT/Util/Digest/SHA.pm
+++ lib/MT/Util/Digest/SHA.pm
@@ -2,22 +2,22 @@
 # This code cannot be redistributed without permission from www.sixapart.com.
 # For more information, consult your Movable Type license.
 
-package MT::Util::Digest::SHA1;
+package MT::Util::Digest::SHA;
 
 use strict;
 use warnings;
 
 our ( @ISA, @EXPORT_OK );
 BEGIN {
-    if ( eval { require Digest::SHA1 } ) {
-        push @ISA, 'Digest::SHA1';
-        @EXPORT_OK = @Digest::SHA1::EXPORT_OK;
-        Digest::SHA1->import(@EXPORT_OK);
+    if ( eval { require Digest::SHA } ) {
+        push @ISA, 'Digest::SHA';
+        @EXPORT_OK = @Digest::SHA::EXPORT_OK;
+        Digest::SHA->import(@EXPORT_OK);
     }
     else {
         require Digest::SHA::PurePerl;
         push @ISA, 'Digest::SHA::PurePerl';
-        @EXPORT_OK = qw/sha1 sha1_hex sha1_base64/;
+        @EXPORT_OK = @Digest::SHA::PurePerl::EXPORT_OK;
         Digest::SHA::PurePerl->import(@EXPORT_OK);
     }
 }
