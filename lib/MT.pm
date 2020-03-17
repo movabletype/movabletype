1f98b7477774da0156dff34931b554b18ccb73bf
Kenichi Ishigaki <ishigaki@cpan.org>
2020-03-17 11:40:22 +0900

    Remove decode_html while translating

--- lib/MT.pm
+++ lib/MT.pm
@@ -1754,8 +1754,7 @@ sub update_ping_list {return}
             }
             else {
                 $args{params} = '' unless defined $args{params};
-                my @p = map MT::Util::decode_html($_),
-                        split /\s*%%\s*/, $args{params}, -1;
+                my @p = split /\s*%%\s*/, $args{params}, -1;
                 @p = ('') unless @p;
                 my $phrase = $args{phrase};
                 $phrase = Encode::decode('utf8', $phrase)
