edd1df667df5dd2131c46aa7484cf02563770f6d
Kenichi Ishigaki <ishigaki@cpan.org>
2020-06-23 22:57:57 +0900

    Backport test tool changes from MT7

---
+++ t/lib/MT/Test/Role/Wight.pm
@@ -0,0 +1,41 @@
+package MT::Test::Role::Wight;
+
+use Role::Tiny;
+
+# The following methods are just to keep compatibility with Test::Wight
+
+sub visit {
+    my ( $self, $path_query ) = @_;
+    my $url = $self->{base_url}->clone;
+    $url->path_query($path_query);
+    $self->driver->get( $url->as_string );
+    $self;
+}
+
+sub find {
+    my ( $self, $selector ) = @_;
+    my $element = eval { $self->driver->find_element($selector); };
+    $self->{_element} = $element;
+    $self;
+}
+
+sub value {
+    my $self = shift;
+    my $element = $self->{_element} or return;
+    $element->get_value;
+}
+
+sub attribute {
+    my ( $self, $attr ) = @_;
+    my $element = $self->{_element} or return;
+    $element->get_attribute($attr);
+}
+
+sub set {
+    my ( $self, $value ) = @_;
+    my $element = $self->{_element} or return;
+    $element->clear;
+    $element->send_keys("$value");
+}
+
+1;
