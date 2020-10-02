b7fc13c62fd30e9095b5b51d432793c828f3a39a
Kenichi Ishigaki <ishigaki@cpan.org>
2020-10-02 15:47:02 +0900

    Use $magick->magick to determine image format

--- lib/MT/Image/ImageMagick.pm
+++ lib/MT/Image/ImageMagick.pm
@@ -57,10 +57,10 @@ sub init {
 
     # Set quality.
     my $quality;
-    if ( $arg{magick} eq 'jpg' || $arg{magick} eq 'jpeg' ) {
+    if ( lc( $magick->Get('magick') ) eq 'jpeg' ) {
         $quality = $image->jpeg_quality;
     }
-    elsif ( $arg{magick} eq 'png' ) {
+    elsif ( lc( $magick->Get('magick') ) eq 'png' ) {
         $quality = $image->png_quality;
     }
     if ( defined $quality ) {
