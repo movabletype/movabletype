4e116bb822c54c3d122ea5082401d7871807111c
Ryota Masuko <rmasuko@sixapart.com>
2020-10-26 16:41:20 +0900

    Revert "MTC-25205: The permalink will be return blank string when no archive mapping cloud be found."
    This reverts commit a4e654e955d0189108c6c2e9912eedd01682ea0d.

--- lib/MT/ContentData.pm
+++ lib/MT/ContentData.pm
@@ -1541,15 +1541,15 @@ sub archive_file {
 sub archive_url {
     my $self = shift;
     my $blog = $self->blog or return;
-    my $file = $self->archive_file(@_) or return;
     my $url  = $blog->archive_url || '';
-    MT::Util::caturl( $url, $file );
+    $url .= '/' unless $url =~ m!/$!;
+    $url . $self->archive_file(@_);
 }
 
 sub permalink {
     my $self                   = shift;
     my $blog                   = $self->blog or return;
-    my $url                    = $self->archive_url( $_[0] ) or return;
+    my $url                    = $self->archive_url( $_[0] );
     my $effective_archive_type = ( $_[0] || 'ContentType' );
     $url
         .= '#'
