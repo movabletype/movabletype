9270e06ffd58b031c8f2461e628de69de5ad973a
Kenichi Ishigaki <ishigaki@cpan.org>
2021-03-08 23:08:30 +0900

    Clear cache

--- lib/MT/CMS/Entry.pm
+++ lib/MT/CMS/Entry.pm
@@ -1647,6 +1647,7 @@ sub save {
 
     # Delete old archive files.
     if ( $app->config('DeleteFilesAtRebuild') && $id ) {
+        $app->request->cache( 'file', {} );    # clear cache
         my $file = archive_file_for( $obj, $blog, $archive_type );
         if ( $file ne $orig_file || $obj->status != MT::Entry::RELEASE() ) {
             $app->publisher->remove_entry_archive_file(
