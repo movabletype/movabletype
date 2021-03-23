777eda11bbc731221232a306cc9c4e58becc994d
Takatsugu Shigeta <tshigeta@sixapart.com>
2021-03-23 10:01:44 +0900

    Run pre_save_blog/pre_save_website callbacks on adjust_sitepath

--- lib/MT/CMS/Tools.pm
+++ lib/MT/CMS/Tools.pm
@@ -1941,6 +1941,7 @@ sub adjust_sitepath {
         my $blog = $app->model('blog')->load($id)
             or return $app->error(
             $app->translate( 'Cannot load blog #[_1].', $id ) );
+        my $original           = $blog->clone();
         my $old_site_path      = scalar $q->param("old_site_path_$id");
         my $old_site_url       = scalar $q->param("old_site_url_$id");
         my $site_path          = scalar $q->param("site_path_$id") || q();
@@ -2052,6 +2053,8 @@ sub adjust_sitepath {
                     )
             );
         }
+        _call_pre_save_blog( $app, $blog, $original )
+            or $app->print_encode( $app->translate("failed") . "\n" ), next;
         $blog->save
             or $app->print_encode( $app->translate("failed") . "\n" ), next;
         $app->print_encode( $app->translate("ok") . "\n" );
@@ -3196,6 +3199,19 @@ sub _log_dirty_restore {
 sub login_json {
     my $app = shift;
     return $app->json_result( { magic_token => $app->current_magic, } );
+}
+
+sub _call_pre_save_blog {
+    my ( $app, $blog, $original ) = @_;
+    my @types = ('blog');
+    if ( !$blog->is_blog() ) {
+        push @types, 'website';
+    }
+    my $filter_result = 1;
+    for my $t (@types) {
+        $filter_result &&= $app->run_callbacks( 'cms_pre_save.' . $t, $app, $blog, $original );
+    }
+    return $filter_result;
 }
 
 1;
