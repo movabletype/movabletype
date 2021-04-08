4b23d6ad07142202f725fc3c9b8a7f31edbd3e6d
Seiichiro Hazama <shazama@sixapart.com>
2021-04-08 15:23:36 +0900

    Bump version. MTC-27798 * 6.7.8 -> 6.7.9

--- lib/MT.pm
+++ lib/MT.pm
@@ -39,7 +39,7 @@ BEGIN {
         )
         = (
         '__PRODUCT_NAME__',   'MT',
-        '6.7.8',              '__PRODUCT_VERSION_ID__',
+        '6.7.9',              '__PRODUCT_VERSION_ID__',
         '__RELEASE_NUMBER__', '__PORTAL_URL__'
         );
 
@@ -56,7 +56,7 @@ BEGIN {
     }
 
     if ( $RELEASE_NUMBER eq '__RELEASE' . '_NUMBER__' ) {
-        $RELEASE_NUMBER = 8;
+        $RELEASE_NUMBER = 9;
     }
 
     $DebugMode = 0;
