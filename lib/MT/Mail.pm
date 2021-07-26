497e8ee5a6fccb085a16d5b4f5248de781d43b3a
Kenichi Ishigaki <ishigaki@cpan.org>
2021-07-26 14:10:24 +0900

    Move the common DebugEmailAddress handling to send() to dedupe

--- lib/MT/Mail.pm
+++ lib/MT/Mail.pm
@@ -138,6 +138,9 @@ sub send {
         ( $id ? ( id => $id ) : () )
         );
 
+    $hdrs{To} = $mgr->DebugEmailAddress
+        if ( is_valid_email( $mgr->DebugEmailAddress || '' ) );
+
     if ( $xfer eq 'sendmail' ) {
         return $class->_send_mt_sendmail( \%hdrs, $body, $mgr );
     }
@@ -156,8 +159,6 @@ sub send {
 sub _send_mt_debug {
     my $class = shift;
     my ( $hdrs, $body, $mgr ) = @_;
-    $hdrs->{To} = $mgr->DebugEmailAddress
-        if ( is_valid_email( $mgr->DebugEmailAddress || '' ) );
     for my $key ( keys %$hdrs ) {
         my @arr
             = ref( $hdrs->{$key} ) eq 'ARRAY'
@@ -173,8 +174,6 @@ sub _send_mt_debug {
 sub _send_mt_smtp {
     my $class = shift;
     my ( $hdrs, $body, $mgr ) = @_;
-    $hdrs->{To} = $mgr->DebugEmailAddress
-        if ( is_valid_email( $mgr->DebugEmailAddress || '' ) );
 
     # SMTP Configuration
     my $host      = $mgr->SMTPServer;
@@ -342,8 +341,7 @@ my @Sendmail
 sub _send_mt_sendmail {
     my $class = shift;
     my ( $hdrs, $body, $mgr ) = @_;
-    $hdrs->{To} = $mgr->DebugEmailAddress
-        if ( is_valid_email( $mgr->DebugEmailAddress || '' ) );
+
     my $sm_loc;
     for my $loc ( $mgr->SendMailPath, @Sendmail ) {
         next unless $loc;
