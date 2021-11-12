f83707e68de7ecd5b0526d6b8c474752b44c04e1
Kenichi Ishigaki <ishigaki@cpan.org>
2021-11-12 15:32:22 +0900

    Show driver if set by env

--- t/lib/MT/Test/Env.pm
+++ t/lib/MT/Test/Env.pm
@@ -286,7 +286,7 @@ sub connect_info {
                 $connect_info{$key} = $ENV{$env_key};
             }
         }
-
+        note "DRIVER: $connect_info{ObjectDriver}";
         # TODO: $self->{dsn} = "dbi:$driver:...";
     }
     %connect_info;
