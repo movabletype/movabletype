1ee02f5fc30fa155d510bbf14cc8f7e8cdc7a38f
Kenichi Ishigaki <ishigaki@cpan.org>
2021-12-01 21:33:25 +0900

    Set requet uri for better diagnosis

--- t/lib/MT/Test/Tag.pm
+++ t/lib/MT/Test/Tag.pm
@@ -161,7 +161,8 @@ SKIP: {
                 require MT::Util::UniqueID;
                 my $log = $ENV{MT_TEST_PHP_ERROR_LOG_FILE_PATH} ||
                           File::Spec->catfile($ENV{MT_TEST_ROOT}, 'php-' . MT::Util::UniqueID::create_session_id() . '.log');
-                my $php_script = php_test_script( $block->blog_id || $blog_id, $template, $text, $log, $extra );
+                my $block_name = $block->name || $block->seq_num;
+                my $php_script = php_test_script( $block_name, $block->blog_id || $blog_id, $template, $text, $log, $extra );
                 my $got = Encode::decode_utf8(MT::Test::PHP->run($php_script));
 
                 my $php_error = '';
@@ -230,8 +231,10 @@ sub MT::Test::Tag::_filter_vars {
 }
 
 sub MT::Test::Tag::php_test_script {    # full qualified to avoid Spiffy magic
-    my ( $blog_id, $template, $text, $log, $extra ) = @_;
+    my ( $block_name, $blog_id, $template, $text, $log, $extra ) = @_;
     $text ||= '';
+
+    $ENV{REQUEST_URI} = "$0 [$block_name]";
 
     $template =~ s/<\$(mt.+?)\$>/<$1>/gi;
     $template =~ s/\$/\\\$/g;
