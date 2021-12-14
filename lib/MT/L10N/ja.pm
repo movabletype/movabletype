e1d15ea7caa0e1bd8a27ee62a3ae081429a632fb
Kenichi Ishigaki <ishigaki@cpan.org>
2021-12-14 17:49:48 +0900

    Update L10N entries for preview size

--- lib/MT/L10N/ja.pm
+++ lib/MT/L10N/ja.pm
@@ -4135,8 +4135,8 @@ use vars qw( @ISA %Lexicon );
 ## tmpl/cms/content_field_type_options/asset_image.tmpl
 	'Allow users to select multiple image assets?' => '複数の画像の選択を許可する',
 	'Allow users to upload a new image asset?' => '新しい画像ファイルのアップロードを許可する',
-	'Preview height' => 'プレビューの高さ',
-	'Preview width' => 'プレビューの幅',
+	'Preview height' => '編集画面のプレビュー画像の最大高',
+	'Preview width' => '編集画面のプレビュー画像の最大幅',
 
 ## tmpl/cms/content_field_type_options/asset_video.tmpl
 	'Allow users to select multiple video assets?' => '複数のビデオの選択を許可する',
