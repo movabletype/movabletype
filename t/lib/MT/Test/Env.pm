fb59ac638a1b7533cec8549106e4405327872fb1
Keita Jamadam Sugama <sugama@jamadam.com>
2022-01-31 11:22:56 +0900

    use 8bit for test

--- t/lib/MT/Test/Env.pm
+++ t/lib/MT/Test/Env.pm
@@ -138,6 +138,7 @@ sub write_config {
         LoggerPath             => 'TEST_ROOT/log',
         LoggerLevel            => 'DEBUG',
         MailTransfer           => 'debug',
+        MailTransferEncoding   => '8bit',
         DBIRaiseError          => 1,
         ShowIpInformation      => 1,
         EnableAddressBook      => 1,
