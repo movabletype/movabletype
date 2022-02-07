1161510ba228d26ac80d567272fab1995c3fccee
Kenichi Ishigaki <ishigaki@cpan.org>
2022-02-07 11:32:36 +0900

    Add methods and modify templates

--- lib/MT/CMS/Export.pm
+++ lib/MT/CMS/Export.pm
@@ -8,6 +8,7 @@ package MT::CMS::Export;
 use strict;
 use warnings;
 use MT::Util qw( dirify );
+use Encode;
 
 sub start_export {
     my $app = shift;
@@ -20,6 +21,22 @@ sub start_export {
     my $blog = $app->model('blog')->load($blog_id);
     return $app->return_to_dashboard( redirect => 1 )
         if !$blog;
+
+    my $status = $app->model('import_export_status')->load({
+        blog_id  => $blog_id,
+        ended_at => \'IS NULL',
+        type     => 1,
+    });
+    if ($status) {
+        $param{status} = $status->as_hashref_for_site($blog);
+    }
+
+    if (MT->config->ForceBackgroundExport) {
+        $param{background_export_is_available} = 1;
+        $param{force_background_export}        = 1;
+    } elsif (MT->config->EnableBackgroundExport) {
+        $param{background_export_is_available} = 1;
+    }
 
     $param{blog_id} = $blog_id;
     $app->add_breadcrumb( $app->translate('Export Site Entries') );
@@ -46,6 +63,29 @@ sub export {
         403 )
         unless $perms && $perms->can_do('export_blog');
     $app->validate_magic() or return;
+    my $status = $app->model('import_export_status')->load({
+        blog_id  => $blog_id,
+        ended_at => \'IS NULL',
+        type     => 1,
+    });
+    if ($status) {
+        return $app->redirect($app->uri(
+            mode => 'start_export',
+            args => { blog_id => $blog_id },
+        ));
+    }
+    $status = $app->model('import_export_status')->new(
+        blog_id => $blog_id,
+        type    => 1,
+    );
+
+    if ($app->param('background_export')) {
+        return _register_background_export($app, $status);
+    }
+
+    $status->started_at(time);
+    $status->save;
+
     my $file = dirify( $blog->name ) . ".txt";
 
     if ( $file eq ".txt" ) {
@@ -67,6 +107,139 @@ sub export {
     require MT::ImportExport;
     MT::ImportExport->export( $blog, sub { $app->print_encode(@_) } )
         or return $app->error( MT::ImportExport->errstr, 500 );
+
+    $status->ended_at(time);
+    $status->save;
+
+    1;
+}
+
+sub _register_background_export {
+    my ($app, $status) = @_;
+
+    require MT::TheSchwartz;
+    require TheSchwartz::Job;
+    require JSON;
+    require Digest::MD5;
+
+    my %param;
+    for my $key ($app->multi_param) {
+        next if $key =~ /\A(?:magic_token|__mode)\z/;
+        $param{$key} = $app->param($key);
+    }
+
+    my $blog_id = $app->param('blog_id');
+
+    $param{author_id} = $app->user->id;
+
+    my $job = TheSchwartz::Job->new;
+    $job->funcname('MT::Worker::Export');
+    $job->uniqkey(Digest::MD5::md5_hex(join "-", "export", $blog_id, time));
+    $job->arg(JSON::encode_json(\%param));
+    $job->run_after(time);
+    MT::TheSchwartz->insert($job);
+
+    $status->job_id($job->jobid);
+    $status->save;
+
+    return $app->redirect($app->uri(
+        mode => 'background_export',
+        args => {
+            blog_id => $blog_id,
+            id      => $status->id,
+            saved   => 1,
+        },
+    ));
+}
+
+sub background_export {
+    my $app       = shift;
+    my $blog_id   = $app->param('blog_id');
+    my $status_id = $app->param('id');
+
+    my $perms = $app->permissions;
+    return $app->permission_denied()
+        unless $perms && $app->can_do('open_start_export_screen');
+
+    my $blog = $app->model('blog')->load($blog_id)
+        or return $app->return_to_dashboard(redirect => 1);
+
+    my $status = $app->model('import_export_status')->load($status_id);
+    if (!$status) {
+        return $app->redirect($app->uri(
+            mode => 'start_export',
+            args => { blog_id => $blog_id },
+        ));
+    }
+
+    my %param;
+    if ($status->job_id) {
+        my $job = $app->model('ts_job')->load($status->job_id);
+        if ($job) {
+            if ($job->grabbed_until > time) {
+                $param{job_is_running} = 1;
+            }
+            # $param{job_arg} = JSON->new->pretty->canonical->encode(JSON::decode_json($job->arg));
+        } else {
+            $param{job_is_gone} = 1;
+        }
+    }
+
+    $param{status} = $status->as_hashref_for_site($blog);
+
+    my $file = $param{status}{file};
+    $param{can_download} = 1 if $file && -f $file;
+
+    $param{saved}  = $app->param('saved');
+
+    $app->add_breadcrumb($app->translate('Background Export Site Entries'));
+    $app->load_tmpl('background_export.tmpl', \%param);
+}
+
+sub download_export {
+    my $app       = shift;
+    my $blog_id   = $app->param('blog_id');
+    my $status_id = $app->param('id');
+
+    my $perms = $app->permissions;
+    return $app->permission_denied()
+        unless $perms && $app->can_do('open_start_export_screen');
+
+    my $blog = $app->model('blog')->load($blog_id)
+        or return $app->return_to_dashboard(redirect => 1);
+
+    my $status = $app->model('import_export_status')->load($status_id);
+    if (!$status) {
+        return $app->redirect($app->uri(
+            mode => 'start_export',
+            args => { blog_id => $blog_id },
+        ));
+    }
+
+    my $file = $status->get_data->{file};
+    unless ($file && -f $file) {
+        return $app->redirect($app->uri(
+            mode => 'start_export',
+            args => { blog_id => $blog_id },
+        ));
+    }
+
+    $app->{no_print_body} = 1;
+    local $| = 1;
+
+    my $utf8 = Encode::find_encoding('utf8');
+    my $charset = $app->charset;
+    $app->set_header("Content-Disposition" => "attachment; filename=$file");
+    $app->send_http_header(
+        $charset
+        ? "text/plain; charset=$charset"
+        : 'text/plain'
+    );
+    open my $fh, '<', $file or return $app->error($app->translate("Cannot open [_1].", $file));
+    while(<$fh>) {
+        $app->print_encode($utf8->decode($_));
+    }
+    close $fh;
     1;
 }
 
--- lib/MT/CMS/Import.pm
+++ lib/MT/CMS/Import.pm
@@ -23,6 +23,14 @@ sub start_import {
         if !$blog;
 
     my %param;
+    my $status = $app->model('import_export_status')->load({
+        blog_id  => $blog_id,
+        ended_at => \'IS NULL',
+        type     => 0,
+    });
+    if ($status) {
+        $param{status} = $status->as_hashref_for_site($blog);
+    }
 
     # FIXME: This should build a category hierarchy!
     my $cat_class = $app->model('category');
@@ -92,6 +100,13 @@ sub start_import {
             = $app->load_text_filters( $blog->convert_paras, 'entry' );
     }
 
+    if (MT->config->ForceBackgroundImport) {
+        $param{background_import_is_available} = 1;
+        $param{force_background_import}        = 1;
+    } elsif (MT->config->EnableBackgroundImport) {
+        $param{background_import_is_available} = 1;
+    }
+
     $app->add_breadcrumb( $app->translate('Import Site Entries') );
     $app->load_tmpl( 'import.tmpl', \%param );
 }
@@ -152,6 +167,29 @@ sub do_import {
 
     $app->validate_magic() or return;
 
+    my $status = $app->model('import_export_status')->load({
+        blog_id  => $blog_id,
+        ended_at => \'IS NULL',
+        type     => 0,
+    });
+    if ($status) {
+        return $app->redirect($app->uri(
+            mode => 'start_import',
+            args => { blog_id => $blog_id },
+        ));
+    }
+    $status = $app->model('import_export_status')->new(
+        blog_id => $blog_id,
+        type    => 0,
+    );
+
+    if ($app->param('background_import')) {
+        return _register_background_import($app, $status);
+    }
+
+    $status->started_at(time);
+    $status->save;
+
     $app->add_breadcrumb(
         $app->translate('Import Site Entries'),
         $app->uri(
@@ -211,9 +249,6 @@ sub do_import {
         (%options) ? (%options) : (),
     );
 
-    $param->{import_success} = $import_result;
-    $param->{error} = $importer->{type}->errstr unless $import_result;
-
     if ($import_result) {
         my $rebuild_url = $app->uri(
             mode => 'rebuild_confirm',
@@ -221,13 +256,115 @@ sub do_import {
         );
         $param->{rebuild_open}
             = qq!window.open('$rebuild_url', 'rebuild_blog_$blog_id', 'width=400,height=400,resizable=yes'); return false;!;
+
+        $status->set_data({ success => 1 });
+    } else {
+        $status->set_data({ error => $param->{error} });
     }
 
     $app->print_encode(
         $app->build_page( "include/import_end.tmpl", $param ) );
 
     close $fh if $fh;
+
+    $status->ended_at(time);
+    $status->save;
     1;
+}
+
+sub _register_background_import {
+    my ($app, $status) = @_;
+
+    require MT::TheSchwartz;
+    require TheSchwartz::Job;
+    require JSON;
+    require Digest::MD5;
+
+    my %param;
+    for my $key ($app->multi_param) {
+        next if $key =~ /\A(?:file|magic_token|__mode)\z/;
+        $param{$key} = $app->param($key);
+    }
+
+    my $tmpdir = MT->config->ExportTempDir || MT->config->TempDir;
+    if (my ($fh) = $app->upload_info('file')) {
+        my $name    = join "-", "import", $param{blog_id}, time;    # or $app->param('file')
+        my $tmpfile = File::Spec->catfile($tmpdir, $name);
+        unless (-d $tmpdir) {
+            require File::Path;
+            File::Path::mktree($tmpdir);
+        }
+        open my $out, '>', $tmpfile
+            or return $app->error($app->translate("Error writing upload to '[_1]': [_2]", $tmpfile, $!));
+        my $buf;
+        while ($fh->read($buf, 8192)) {
+            print $out $buf;
+        }
+        $param{file} = $tmpfile;
+    }
+    $param{author_id} = $app->user->id;
+
+    my $job = TheSchwartz::Job->new;
+    $job->funcname('MT::Worker::Import');
+    $job->uniqkey(Digest::MD5::md5_hex($param{file}));
+    $job->arg(JSON::encode_json(\%param));
+    $job->run_after(time);
+    MT::TheSchwartz->insert($job);
+
+    $status->job_id($job->jobid);
+    $status->save;
+
+    my $blog_id = $app->param('blog_id');
+    return $app->redirect($app->uri(
+        mode => 'background_import',
+        args => {
+            blog_id => $blog_id,
+            id      => $job->jobid,
+            saved   => 1,
+        },
+    ));
+}
+
+sub background_import {
+    my $app       = shift;
+    my $blog_id   = $app->param('blog_id');
+    my $status_id = $app->param('id');
+
+    my $perms = $app->permissions;
+    return $app->permission_denied()
+        unless $perms && $app->can_do('open_start_import_screen');
+
+    my $blog = $app->model('blog')->load($blog_id)
+        or return $app->return_to_dashboard(redirect => 1);
+
+    my %param;
+    my $status = $app->model('import_export_status')->load($status_id);
+    if (!$status) {
+        return $app->redirect($app->uri(
+            mode => 'start_import',
+            args => {
+                blog_id => $blog_id,
+            },
+        ));
+    }
+    $param{status} = $status->as_hashref_for_site($blog);
+
+    if ($status->job_id) {
+        my $job = $app->model('ts_job')->load($status->job_id);
+        if ($job) {
+            if ($job->grabbed_until > time) {
+                $param{job_is_running} = 1;
+            }
+            # $param{job_arg} = JSON->new->pretty->canonical->encode(JSON::decode_json($job->arg));
+        } else {
+            $param{job_is_gone} = 1;
+        }
+    }
+
+    $param{saved} = $app->param('saved');
+
+    $app->add_breadcrumb($app->translate('Background Import Site Entries'));
+    $app->load_tmpl('background_import.tmpl', \%param);
 }
 
 1;
