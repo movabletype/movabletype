885654ac12815054f83df129e4503cba3b088349
Kenichi Ishigaki <ishigaki@cpan.org>
2022-02-25 15:39:46 +0900

    Remove tiff? from MT::Asset::Image::extensions

--- lib/MT/Asset/Image.pm
+++ lib/MT/Asset/Image.pm
@@ -27,8 +27,7 @@ __PACKAGE__->install_properties(
 # List of supported file extensions (to aid the stock 'can_handle' method.)
 sub extensions {
     my $pkg = shift;
-    return $pkg->SUPER::extensions(
-        [ qr/gif/i, qr/jpe?g/i, qr/png/i, qr/bmp/i, qr/tiff?/i, qr/ico/i ] );
+    return $pkg->SUPER::extensions( [ qr/gif/i, qr/jpe?g/i, qr/png/i, qr/bmp/i, qr/ico/i ] );
 }
 
 sub image_metadata {
