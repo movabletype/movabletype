b01b50b12c93e38982e50d7776a206a8fba8c133
Kenichi Ishigaki <ishigaki@cpan.org>
2022-06-02 15:33:10 +0900

    Cache (modified) author

--- lib/MT/ContentData.pm
+++ lib/MT/ContentData.pm
@@ -655,7 +655,17 @@ sub author {
     $self->cache_property(
         'author',
         sub {
-            scalar MT::Author->load( $self->author_id || 0 );
+            my $author_id = $self->author_id or return undef;
+            my $req       = MT::Request->instance;
+            my $cache     = $req->stash('author_cache');
+            my $author    = $cache->{$author_id};
+            unless ($author) {
+                require MT::Author;
+                $author = MT::Author->load($author_id) or return undef;
+                $cache->{$author_id} = $author;
+                $req->stash('author_cache', $cache);
+            }
+            $author;
         },
     );
 }
@@ -665,7 +675,17 @@ sub modified_author {
     $self->cache_property(
         'modified_author',
         sub {
-            scalar MT::Author->load( $self->modified_by || 0 );
+            my $modified_by = $self->modified_by or return undef;
+            my $req         = MT::Request->instance;
+            my $cache       = $req->stash('author_cache');
+            my $author      = $cache->{$modified_by};
+            unless ($author) {
+                require MT::Author;
+                $author = MT::Author->load($modified_by) or return undef;
+                $cache->{$modified_by} = $author;
+                $req->stash('author_cache', $cache);
+            }
+            $author;
         },
     );
 }
