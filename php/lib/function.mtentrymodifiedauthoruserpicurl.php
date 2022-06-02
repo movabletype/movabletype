cea3450db09ee4d2d5dcffab1819a7a1ebc4efe3
Kenichi Ishigaki <ishigaki@cpan.org>
2022-06-02 15:43:06 +0900

    Add php handler implementation

---
+++ php/lib/function.mtentrymodifiedauthoruserpicurl.php
@@ -0,0 +1,25 @@
+<?php
+# Movable Type (r) (C) Six Apart Ltd. All Rights Reserved.
+# This code cannot be redistributed without permission from www.sixapart.com.
+# For more information, consult your Movable Type license.
+#
+# $Id$
+
+function smarty_function_mtentrymodifiedauthoruserpicurl($args, &$ctx) {
+    $entry = $ctx->stash('entry');
+    if (!$entry) return '';
+
+    $author = $entry->modified_author();
+    if (!$author) return '';
+
+    $asset_id = isset($author->author_userpic_asset_id) ? $author->author_userpic_asset_id : 0;
+    $asset = $ctx->mt->db()->fetch_assets(array('id' => $asset_id));
+    if (!$asset) return '';
+
+    $blog =& $ctx->stash('blog');
+
+    require_once("MTUtil.php");
+    $userpic_url = userpic_url($asset[0], $blog, $author);
+    return $userpic_url;
+}
+?>
