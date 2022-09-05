89045bab567d0bf2af24f60fe04ba8153a0a4428
Keita Jamadam Sugama <sugama@jamadam.com>
2022-09-05 12:15:33 +0900

    remove SKIP_PHP. do not eval

--- t/lib/MT/Test/Tag.pm
+++ t/lib/MT/Test/Tag.pm
@@ -319,10 +319,10 @@ sub _update_config {
 
 sub _check_skip_php {
     my $block    = shift;
-    my $skip_php = $block->skip_php // $block->SKIP_PHP;
+    my $skip_php = $block->skip_php;
     if (defined($skip_php)) {
         if (length($skip_php)) {
-            return eval(_filter_vars($skip_php));
+            return _filter_vars($skip_php);
         } else {
             return 1;
         }
