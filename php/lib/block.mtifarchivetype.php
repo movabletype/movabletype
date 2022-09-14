5df2ef8aaebfa947ae8c0c199c49f1b8e73d055e
Keita Jamadam Sugama <sugama@jamadam.com>
2022-09-14 15:47:41 +0900

    silence php warnings

--- php/lib/block.mtifarchivetype.php
+++ php/lib/block.mtifarchivetype.php
@@ -8,8 +8,7 @@
 function smarty_block_mtifarchivetype($args, $content, &$ctx, &$repeat) {
     # status: complete
     if (!isset($content)) {
-        $at = $args['type'];
-        $at or $at = $args['archive_type'];
+        $at = $args['type'] ?? $args['archive_type'] ?? null;
         $cat = $ctx->stash('current_archive_type');
         $cat or $at = $ctx->stash('archive_type');
         $same = ($at && $cat) && ($at == $cat);
