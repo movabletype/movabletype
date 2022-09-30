b4534e584886c48fa98f9caf96d3f69c977d3835
Kenichi Ishigaki <ishigaki@cpan.org>
2022-09-30 13:43:18 +0900

    Update TinyMCE(5|6) L10N items

--- plugins/TinyMCE6/lib/TinyMCE6/L10N/ja.pm
+++ plugins/TinyMCE6/lib/TinyMCE6/L10N/ja.pm
@@ -16,53 +16,30 @@ use vars qw( %Lexicon );
 
 %Lexicon = (
 
-## mt-static/plugins/TinyMCE/tiny_mce/plugins/autosave/plugin.min.js
-
-## mt-static/plugins/TinyMCE/tiny_mce/plugins/compat3x/utils/editable_selects.js
-	'value' => 'value',
-
-## mt-static/plugins/TinyMCE/tiny_mce/plugins/insertdatetime/plugin.js
-	'%Y-%m-%d' => '%Y-%m-%d',
-	'%H:%M:%S' => '%H:%M:%S',
-
-## mt-static/plugins/TinyMCE/tiny_mce/plugins/insertdatetime/plugin.min.js
-
-## mt-static/plugins/TinyMCE/tiny_mce/plugins/mt/langs/plugin.js
-	'Class Name' => 'クラス名',
-	'Bold (Ctrl+B)' => '太字  (Ctrl+B)',
-	'Italic (Ctrl+I)' => '斜体 (Ctrl+I)',
-	'Underline (Ctrl+U)' => '下線 (Ctrl+U)',
-	'Strikethrough' => '取り消し線',
+## mt-static/plugins/TinyMCE6/lib/js/tinymce/plugins/mt/langs/plugin.js
 	'Block Quotation' => '引用ブロック',
-	'Unordered List' => '番号なしリスト',
-	'Ordered List' => '番号付きリスト',
-	'Horizontal Line' => '水平線を挿入',
-	'Insert/Edit Link' => 'リンクの挿入/編集',
-	'Unlink' => 'リンクを解除',
-	'Undo (Ctrl+Z)' => '元に戻す (Ctrl+Z)',
-	'Redo (Ctrl+Y)' => 'やり直す (Ctrl+Y)',
-	'Select Text Color' => 'テキスト色',
-	'Select Background Color' => '背景色',
-	'Remove Formatting' => '書式の削除',
-	'Align Left' => '左揃え',
-	'Align Center' => '中央揃え',
-	'Align Right' => '右揃え',
-	'Indent' => '字下げを増やす',
-	'Outdent' => '字下げを減らす',
-	'Insert Link' => 'リンクの挿入',
+	'Copy column' => '列のコピー',
+	'Cut column' => '列の切り取り',
+	'Emphasis' => '斜体',
+	'Horizontal align' => '横配置',
+	'Insert Asset Link' => 'アセットの挿入',
 	'Insert HTML' => 'HTMLの挿入',
 	'Insert Image Asset' => '画像の挿入',
-	'Insert Asset Link' => 'アセットの挿入',
+	'Insert Link' => 'リンクの挿入',
+	'List Item' => 'リスト要素',
+	'Ordered List' => '番号付きリスト',
+	'Paste column after' => '列の後に貼り付け',
+	'Paste column before' => '列の前に貼り付け',
+	'Strong Emphasis' => '太字',
 	'Toggle Fullscreen Mode' => '全画面表示の切り替え',
 	'Toggle HTML Edit Mode' => 'HTML編集モードの切り替え',
-	'Strong Emphasis' => '太字',
-	'Emphasis' => '斜体',
-	'List Item' => 'リスト要素',
+	'Unordered List' => '番号なしリスト',
+	'Vertical align' => '縦配置',
 
-## mt-static/plugins/TinyMCE/tiny_mce/plugins/mt/plugin.js
+## mt-static/plugins/TinyMCE6/lib/js/tinymce/plugins/mt/plugin.js
 	'HTML' => 'HTML',
 
-## mt-static/plugins/TinyMCE/tiny_mce/plugins/mt_fullscreen/langs/plugin.js
+## mt-static/plugins/TinyMCE6/lib/js/tinymce/plugins/mt_fullscreen/langs/plugin.js
 	'Fullscreen' => '全画面表示',
 
 ## mt-static/plugins/TinyMCE6/tiny_mce/plugins/autosave/plugin.js
@@ -71,7 +48,6 @@ use vars qw( %Lexicon );
 ## plugins/TinyMCE6/config.yaml
 	'Default WYSIWYG editor.' => '既定のWYSIWYGエディタ',
 	'TinyMCE' => 'TinyMCE',
-
 );
 
 1;
