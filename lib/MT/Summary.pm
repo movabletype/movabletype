8d89dcdceb45219199f63844c8f6e42800688ef8
Shigeki <sikeda@sixapart.com>
2022-12-01 02:06:38 +0000

    Remove unused constant

--- lib/MT/Summary.pm
+++ lib/MT/Summary.pm
@@ -11,7 +11,6 @@ use warnings;
 
 use base qw( MT::Meta );
 
-use constant FRESH            => 0;
 use constant NEEDS_JOB        => 1;
 use constant IN_QUEUE         => 2;
 use constant NO_AUTO_GENERATE => 3;
