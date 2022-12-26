10d05ea117a1cd0f757794212c7c53ab5d697e5f
Keita Jamadam Sugama <sugama@jamadam.com>
2022-12-26 22:20:02 +0900

    reduce search uncertainty MTC-28689 (#2505)

--- lib/MT/App/Search/TagSearch.pm
+++ lib/MT/App/Search/TagSearch.pm
@@ -230,11 +230,7 @@ sub search_terms {
             }
         }
     }
-    push @sort,
-        {
-        desc   => $desc,
-        column => $sort
-        };
+    push @sort, { desc => $desc, column => $sort }, { desc => $desc, column => 'id' };
 
     my %args = (
         'join' => $join_on,
