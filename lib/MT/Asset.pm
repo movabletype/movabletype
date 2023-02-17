49e82a2bb508e0a12adf467cb21b99275d8797f4
Kenichi Ishigaki <ishigaki@cpan.org>
2023-02-17 16:18:07 +0900

    Fix deduplication

--- lib/MT/Asset.pm
+++ lib/MT/Asset.pm
@@ -735,9 +735,9 @@ sub extensions {
     my @custom_ext = map {qr/$_/i}
         split( /\s*,\s*/, MT->config('AssetFileTypes')->{$this_pkg} || '' );
     my %seen;
-    my ($new_ext) = grep { ++$seen{$_} < 2 }[ @$ext, @custom_ext ];
+    my @new_ext = grep { !$seen{$_}++ } (@$ext, @custom_ext);
 
-    return \@$new_ext;
+    return \@new_ext;
 }
 
 # This property is a meta-property.
