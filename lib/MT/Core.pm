a5f3f36e27a20798976b8b25e59230fb4f03c9c1
Takatsugu Shigeta <tshigeta@sixapart.com>
2023-04-26 21:11:40 +0900

    Add AdminThemeId directive

--- lib/MT/Core.pm
+++ lib/MT/Core.pm
@@ -2341,6 +2341,7 @@ BEGIN {
             'WaitAfterReboot' => { default => '1.0' },
             'DisableMetaRefresh' => { default => 1 },
             'DynamicTemplateAllowPHP' => { default => 1 },
+            'AdminThemeId' => undef,
             'TrimFilePath' => { default => 0 },
         },
         upgrade_functions => \&load_upgrade_fns,
