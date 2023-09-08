de941d501345c3abc2a05213fc6a28465ff51295
Seiichiro Hazama <shazama@sixapart.com>
2023-09-08 16:43:18 +0900

    Bump version to 8.000001. MTC-29171 * 8.000000 -> 8.000001

--- php/mt.php
+++ php/mt.php
@@ -10,8 +10,8 @@
  */
 require_once('lib/class.exception.php');
 
-define('VERSION', '8.000000');
-define('PRODUCT_VERSION', '8.0.0');
+define('VERSION', '8.000001');
+define('PRODUCT_VERSION', '8.0.1');
 define('DATA_API_DEFAULT_VERSION', '6');
 
 $PRODUCT_NAME = '__PRODUCT_NAME__';
@@ -21,7 +21,7 @@ define('PRODUCT_NAME', $PRODUCT_NAME);
 
 $RELEASE_NUMBER = '__RELEASE_NUMBER__';
 if ( $RELEASE_NUMBER == '__RELEASE_' . 'NUMBER__' )
-    $RELEASE_NUMBER = 1;
+    $RELEASE_NUMBER = 0;
 define('RELEASE_NUMBER', $RELEASE_NUMBER);
 
 $PRODUCT_VERSION_ID = '__PRODUCT_VERSION_ID__';
