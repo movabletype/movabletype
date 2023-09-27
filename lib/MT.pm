b7401b43e082b4709f9d934cd90a4f3da7b51996
Seiichiro Hazama <shazama@sixapart.com>
2023-09-27 11:28:39 +0900

    Bump version. MTC-29226 * r.5405 -> r.5501 * 7.901.1 -> 7.902.0

--- lib/MT.pm
+++ lib/MT.pm
@@ -39,14 +39,14 @@ our $plugins_installed;
 BEGIN {
     $plugins_installed = 0;
 
-    ( $VERSION, $SCHEMA_VERSION ) = ( '7.901', '7.0055' );
+    ( $VERSION, $SCHEMA_VERSION ) = ( '7.902', '7.0055' );
     (   $PRODUCT_NAME, $PRODUCT_CODE,   $PRODUCT_VERSION,
         $VERSION_ID,   $RELEASE_NUMBER, $PORTAL_URL,
         $RELEASE_VERSION_ID
         )
         = (
         '__PRODUCT_NAME__',   'MT',
-        '7.901.1',            '__PRODUCT_VERSION_ID__',
+        '7.902.0',            '__PRODUCT_VERSION_ID__',
         '__RELEASE_NUMBER__', '__PORTAL_URL__',
         '__RELEASE_VERSION_ID__',
         );
@@ -64,11 +64,11 @@ BEGIN {
     }
 
     if ( $RELEASE_NUMBER eq '__RELEASE' . '_NUMBER__' ) {
-        $RELEASE_NUMBER = 1;
+        $RELEASE_NUMBER = 0;
     }
 
     if ( $RELEASE_VERSION_ID eq '__RELEASE' . '_VERSION_ID__' ) {
-        $RELEASE_VERSION_ID = 'r.5405';
+        $RELEASE_VERSION_ID = 'r.5501';
     }
 
     $DebugMode = 0;
