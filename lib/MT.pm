9b59471a01a5a2532866a395695a1213b06f8320
Seiichiro Hazama <shazama@sixapart.com>
2024-03-13 10:44:51 +0900

    Bump version. MTC-29576 * 8.1.0 -> 8.2.0

--- lib/MT.pm
+++ lib/MT.pm
@@ -39,14 +39,14 @@ our $plugins_installed;
 BEGIN {
     $plugins_installed = 0;
 
-    ( $VERSION, $SCHEMA_VERSION ) = ( '8.001000', '8.0000' );
+    ( $VERSION, $SCHEMA_VERSION ) = ( '8.002000', '8.0000' );
     (   $PRODUCT_NAME, $PRODUCT_CODE,   $PRODUCT_VERSION,
         $VERSION_ID,   $RELEASE_NUMBER, $PORTAL_URL,
         $RELEASE_VERSION_ID
         )
         = (
         '__PRODUCT_NAME__',   'MT',
-        '8.1.0',              '__PRODUCT_VERSION_ID__',
+        '8.2.0',              '__PRODUCT_VERSION_ID__',
         '__RELEASE_NUMBER__', '__PORTAL_URL__',
         '__RELEASE_VERSION_ID__',
         );
