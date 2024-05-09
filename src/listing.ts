60c2dbbfb4971119d0975fd76dac2ee51d9c7d62
Masahiro Iuchi <miuchi@sixapart.com>
2024-05-10 02:03:29 +0900

    Fix "npm run svelte-check-types" errors

--- src/listing.ts
+++ src/listing.ts
@@ -1,9 +1,20 @@
 import ListTop from "./listing/elements/ListTop.svelte";
 
+const listTopTarget = document.querySelector('[data-is="list-top"]');
+if (!listTopTarget) {
+  throw new Error("Target element is not found");
+}
+
 const listTop = new ListTop({
-  target: document.querySelector('[data-is="list-top"]').parentNode,
+  target: listTopTarget,
   props: {
-    opts: JSON.parse(document.querySelector("#listing-opts").textContent),
-    store: JSON.parse(document.querySelector("#listing-store").textContent),
+    opts: JSON.parse(
+      (document.querySelector("#listing-opts") || {}).textContent || "{}"
+    ),
+    store: JSON.parse(
+      (document.querySelector("#listing-store") || {}).textContent || "{}"
+    ),
   },
 });
+
+export { listTop };
