bcc7ce6891c2e3ccf8345bbbfe43880564ddd4b9
Kenichi Ishigaki <ishigaki@cpan.org>
2024-06-19 18:40:16 +0900

    Return 403 if MakeSuperuserRespectDataAPIDisableSite is true and data_api_is_enabled is false

--- lib/MT/App/DataAPI.pm
+++ lib/MT/App/DataAPI.pm
@@ -965,7 +965,7 @@ sub api {
         $app->param( 'blog_id', $id );
 
         require MT::CMS::Blog;
-        if (   !$user->is_superuser
+        if (   (!$user->is_superuser or MT->config->MakeSuperuserRespectDataAPIDisableSite)
             && !MT::CMS::Blog::data_api_is_enabled( $app, $id, $app->blog ) )
         {
             return $app->print_error(403);
