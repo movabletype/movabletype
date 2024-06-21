636da15ef3fb88626d8e8f6cfb7da36df2a21cbd
Keita Jamadam Sugama <sugama@jamadam.com>
2024-06-21 23:30:06 +0900

    Revert "inherit env vars to php daemon for debugging"
    This reverts commit 1acd035cb9aff3c8b955e2fe2df94ca7f9c2a150.

--- t/lib/MT/Test/Tag/daemon.php
+++ t/lib/MT/Test/Tag/daemon.php
@@ -2,44 +2,38 @@
 
 ini_set("memory_limit", "-1");
 
-$opts = getopt('', ['port:']);
-
-include_once($_ENV['MT_HOME'] . '/php/mt.php');
-include_once($_ENV['MT_HOME'] . '/php/lib/MTUtil.php');
-include_once($_ENV['MT_HOME'] . '/t/lib/MT/Test/Tag/error_handler.php');
+$opts = getopt('', ['port:', 'mt_home:', 'mt_config:', 'log:', 'init_blog_id:', 'ignore_php_dynamic_properties_warnings:']);
 
 $socket = stream_socket_server("tcp://127.0.0.1:". $opts['port']);
+
+include_once($opts['mt_home'] . '/php/mt.php');
+include_once($opts['mt_home'] . '/php/lib/MTUtil.php');
+include_once($opts['mt_home'] . '/t/lib/MT/Test/Tag/error_handler.php');
+
+$error_handler = new MT_Test_Error_Handler();
+set_error_handler([$error_handler, 'handler']);
+$error_handler->log = $opts['log'];
+$error_handler->ignore_php_dynamic_properties_warnings = $opts['ignore_php_dynamic_properties_warnings'];
+
+# fix following tests by using first blog_id for init.
+# - t/tag/35-tags-assets.t
+# - t/mt7/tag/archive/archive-type-label.t
+$mt = MT::get_instance($opts['init_blog_id'], $opts['mt_config']);
+
+$mt->init_plugins();
+$db = $mt->db();
+if (is_a($db, 'MTDatabaseoracle')) {
+    $db->execute("ALTER SESSION SET TIME_ZONE = '+00:00'");
+} else {
+    $db->execute("SET time_zone = '+00:00'");
+}
+$ctx = $mt->context();
 
 while ($remote = stream_socket_accept($socket)) {
 
     $stream = stream_get_contents($remote);
     if (!$stream) {
         continue;
-    }
-
-    list($blog_id, $tmpl, $extra, $env) = json_decode($stream, true);
-    $_ENV = array_merge($_ENV, $env);
-
-    if (!isset($mt)) {
-        $error_handler = new MT_Test_Error_Handler();
-        set_error_handler([$error_handler, 'handler']);
-        $error_handler->log = $_ENV['MT_TEST_PHP_ERROR_LOG_FILE_PATH'];
-        $error_handler->ignore_php_dynamic_properties_warnings = 
-                        $_ENV['MT_TEST_IGNORE_PHP_DYNAMIC_PROPERTIES_WARNINGS'] ?? false;
-    
-        # fix following tests by using first blog_id for init.
-        # - t/tag/35-tags-assets.t
-        # - t/mt7/tag/archive/archive-type-label.t
-        $mt = MT::get_instance($blog_id, $_ENV['MT_CONFIG']);
-    
-        $mt->init_plugins();
-        $db = $mt->db();
-        if (is_a($db, 'MTDatabaseoracle')) {
-            $db->execute("ALTER SESSION SET TIME_ZONE = '+00:00'");
-        } else {
-            $db->execute("SET time_zone = '+00:00'");
-        }
-        $ctx = $mt->context();
     }
 
     // $mt->cache_driver()->flush_all();
@@ -54,7 +48,8 @@ while ($remote = stream_socket_accept($socket)) {
     # fix tests with local config
     $mt->configure_from_db();
 
-    $error_handler->log = $_ENV['MT_TEST_PHP_ERROR_LOG_FILE_PATH'];
+    list($blog_id, $tmpl, $extra, $log) = json_decode($stream);
+    $error_handler->log = $log;
     $blog = $db->fetch_blog($blog_id);
     $ctx->stash('blog', $blog);
     $ctx->stash('blog_id', $blog_id);
