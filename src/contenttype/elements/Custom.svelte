d7c1d7666cc6ca19a9dcb2acf56c8d9067963b76
Masahiro Iuchi <miuchi@sixapart.com>
2024-06-24 04:58:23 +0900

    Remove id prop

--- src/contenttype/elements/Custom.svelte
+++ src/contenttype/elements/Custom.svelte
@@ -28,7 +28,6 @@
     if (component) {
       component.fieldIndex = fieldIndex;
       component.fieldsStore = fieldsStore;
-      component.id = `field-options-${field.id}`;
       /* @ts-expect-error : window.svelteAdditionalTypes is not defined */
     } else if (window.svelteAdditionalTypes[field.type]) {
       /* @ts-expect-error : window.svelteAdditionalTypes is not defined */
@@ -37,7 +36,6 @@
           config: config,
           fieldIndex: fieldIndex,
           fieldsStore: fieldsStore,
-          id: `field-options-${field.id}`,
           optionsHtmlParams: optionsHtmlParams,
         },
         target,
