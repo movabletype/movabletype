2c557d642e9eab9e096d165f9c5f4a3f763b5136
Masahiro Iuchi <miuchi@sixapart.com>
2024-06-25 07:44:34 +0900

    Keep opts in ContentTypeEditor

--- src/contenttype/ContentTypeEditor.ts
+++ src/contenttype/ContentTypeEditor.ts
@@ -5,6 +5,7 @@ import ContentFields from "./elements/ContentFields.svelte";
 export default class ContentTypeEditor {
   static accessor config: MT.ContentType.ConfigSettings = {};
   static accessor optionsHtmlParams: MT.ContentType.OptionsHtmlParams = {};
+  static accessor opts: MT.ContentType.ContentFieldsOpts;
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   static registerCustomType(type: string, mountFunction: any): void {
@@ -16,11 +17,12 @@ export default class ContentTypeEditor {
     opts: MT.ContentType.ContentFieldsOpts,
   ): void {
     const target = this.getContentFieldsTarget(targetSelector);
+    this.opts = opts;
     new ContentFields({
       props: {
         config: this.config,
         optionsHtmlParams: this.optionsHtmlParams,
-        opts: opts,
+        opts: this.opts,
         root: target,
       },
       target: target,
