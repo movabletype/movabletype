ee129e581b896fd8d1ca7a3443f1bd7d0afae3de
Masahiro Iuchi <miuchi@sixapart.com>
2024-06-24 15:58:25 +0900

    Create ContentTypeEditor class

--- src/contenttype.ts
+++ src/contenttype.ts
@@ -2,28 +2,29 @@ import observable from "@riotjs/observable";
 
 import ContentFields from "./contenttype/elements/ContentFields.svelte";
 
-function getContentFieldsTarget(): Element {
-  const target = document.querySelector('[data-is="content-fields"]');
-  if (!target) {
-    throw new Error("Target element is not found");
+class ContentTypeEditor {
+  static mount(props: {
+    config: MT.ContentType.ConfigSettings;
+    optionsHtmlParams: MT.ContentType.OptionsHtmlParams;
+    opts: MT.ContentType.ContentFieldsOpts;
+  }): void {
+    const target = this.getContentFieldsTarget();
+    new ContentFields({
+      props: {
+        ...props,
+        root: target,
+      },
+      target: target,
+    });
   }
-  return target;
+
+  private static getContentFieldsTarget(): Element {
+    const target = document.querySelector('[data-is="content-fields"]');
+    if (!target) {
+      throw new Error("Target element is not found");
+    }
+    return target;
+  }
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function svelteMountContentFields(props: {
-  config: MT.ContentType.ConfigSettings;
-  optionsHtmlParams: MT.ContentType.OptionsHtmlParams;
-  opts: MT.ContentType.ContentFieldsOpts;
-}): void {
-  const target = getContentFieldsTarget();
-  new ContentFields({
-    props: {
-      ...props,
-      root: target,
-    },
-    target: target,
-  });
-}
-
-export { observable, svelteMountContentFields };
+export { ContentTypeEditor, observable };
