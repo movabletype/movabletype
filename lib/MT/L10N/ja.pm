cfe926348eb9160ca21868c5fa03c98a639c08be
Kenichi Ishigaki <ishigaki@cpan.org>
2024-06-25 14:16:12 +0900

    Use Kanji

--- lib/MT/L10N/ja.pm
+++ lib/MT/L10N/ja.pm
@@ -4160,7 +4160,7 @@ use vars qw( @ISA %Lexicon );
 	'Data API' => 'Data API',
 	'Disable Data API' => 'Data API を利用しない',
 	'Enable Data API in system scope.' => 'システム領域の設定やデータを Data API の出力結果に含める',
-	'Enable Data API in this site for all users including system administrators.' => 'システム管理者をふくむすべてのユーザーにData APIの利用を許可する。',
+	'Enable Data API in this site for all users including system administrators.' => 'システム管理者を含むすべてのユーザーにData APIの利用を許可する。',
 	'Enable Data API in this site.' => 'Data API の利用を許可する。',
 	'External Notifications' => '更新通知',
 	'Note: This option is currently ignored because outbound notification pings are disabled system-wide.' => '備考: システム外部ping通知がシステムレベルで無効のため、このオプションは現在無効となっています。',
