715d501e9fb1d3fb108be19345220bc7c18024f9
Masahiro Iuchi <miuchi@sixapart.com>
2024-07-21 07:10:30 +0900

    Fix bugs that some content field types are not copied. MTC-29717

--- lib/MT/ContentFieldType/Tags.pm
+++ lib/MT/ContentFieldType/Tags.pm
@@ -22,7 +22,8 @@ sub field_html_params {
 
     my $tag_names;
     if (@value) {
-        if ( $app->param('reedit') && !$app->param('had_error') ) {
+        # origin parameter check is for copy_this_content_data
+        if ( $app->param('reedit') && !$app->param('had_error') && !$app->param('origin') ) {
             $tag_names = join $tag_delim, @value;
         }
         else {
