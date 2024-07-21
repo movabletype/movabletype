e25ff28c2d2948706e29978f2fcae0fded027e42
Masahiro Iuchi <miuchi@sixapart.com>
2024-07-21 21:01:16 +0900

    Validate origin parameter

--- plugins/CopyThisContentData/lib/CopyThisContentData/CMS.pm
+++ plugins/CopyThisContentData/lib/CopyThisContentData/CMS.pm
@@ -7,6 +7,10 @@ use MT::CMS::ContentData;
 sub hdlr_copy_this_content_data {
     my $app = shift;
 
+    $app->validate_param({
+        origin => [qw/ID/],
+    }) or return;
+
     my $blog_id      = $app->blog ? $app->blog->id : undef;
     my $id           = $app->param('origin');
     my $content_data = $id ? $app->model('content_data')->load($id) : undef;
