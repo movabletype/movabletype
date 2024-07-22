cc9a4750c4ebf45988dc1588e63353a24221942b
Kenichi Ishigaki <ishigaki@cpan.org>
2024-07-22 23:22:43 +0900

    Return early if type and mode are not expected

--- lib/MT/CMS/Entry.pm
+++ lib/MT/CMS/Entry.pm
@@ -2619,6 +2619,11 @@ sub pre_save {
     my $eh = shift;
     my ( $app, $obj ) = @_;
 
+    # The following is only for specific mode and type (ie. not for searching/replacing)
+    my $mode = $app->mode;
+    my $type = $app->param('_type') || '';
+    return 1 unless ($mode eq 'save' and $type eq 'entry');
+
     # save tags
     my $tags = $app->param('tags');
     if ( defined $tags ) {
