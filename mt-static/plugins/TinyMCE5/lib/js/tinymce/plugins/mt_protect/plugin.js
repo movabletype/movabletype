ab7095be50269ab9a23deace6d7ed72556002c7d
Taku Amano <taku@taaas.jp>
2024-08-02 10:19:36 +0900

    Backport TinyMCE fixes to support-7.x

--- mt-static/plugins/TinyMCE5/lib/js/tinymce/plugins/mt_protect/plugin.js
+++ mt-static/plugins/TinyMCE5/lib/js/tinymce/plugins/mt_protect/plugin.js
@@ -1,9 +1,1 @@
-tinymce.PluginManager.add('mt_protect', function (ed) {
-  ed.on('PreInit', function () {
-    ed.parser.addNodeFilter('noscript', function (nodes) {
-      nodes.forEach(function (node) {
-        node.remove();
-      });
-    });
-  })
-});
+function d(n){if(n.attr("srcdoc"))return!0;var r=n.attr("src");if(!r)return!0;var t=document.createElement("a");return t.href=r,t.hostname===window.location.hostname&&t.protocol===window.location.protocol&&t.port===window.location.port}tinymce.PluginManager.add("mt_protect",function(n){n.on("PreInit",function(){n.parser.addNodeFilter("noscript",function(t){t.forEach(function(o){o.remove()})});var r=n.parser.getNodeFilters().find(function(t){return t.name==="iframe"});r&&r.callbacks.unshift(function(t){t.forEach(function(o){var e=o.attr("sandbox"),l=o.replace;o.replace=function(c){var i=c.children();if(i[0]&&i[0].name==="iframe"){var a=i[0];if(!d(a))a.attr("sandbox",e);else{let s="allow-scripts";e===void 0?a.attr("data-mt-protect-no-sandbox","1"):(a.attr("data-mt-protect-sandbox",e),s=e.split(/\s+/).filter(function(f){return!/allow-same-origin/i.test(f)}).join(" ")),a.attr("sandbox",s)}}return l.apply(this,[c]),this}})})})});
