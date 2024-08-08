0cf4827d389783bd02de1b49563c2d559f9b7a75
Shigeki Ikeda <sikeda@sixapart.com>
2024-08-08 11:35:25 +0900

    Use translate for Yearly in MTArchiveTitle

--- lib/MT/ArchiveType/AuthorYearly.pm
+++ lib/MT/ArchiveType/AuthorYearly.pm
@@ -57,11 +57,9 @@ sub archive_title {
     my $start = start_end_year($stamp);
     my $year  = MT::Template::Context::_hdlr_date( $ctx,
         { ts => $start, 'format' => "%Y" } );
-    my $lang = lc MT->current_language || 'en_us';
-    $lang = 'ja' if lc($lang) eq 'jp';
     my $author = $obj->display_name($ctx);
 
-    sprintf( "%s%s%s", $author, $year, ( $lang eq 'ja' ? '&#24180;' : '' ) );
+    $author . MT->translate('YEARLY_ARCHIVE_TITLE', $year);
 }
 
 sub archive_file {
