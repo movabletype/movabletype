85582a1976ea589aca271eb3509d1569e7fe3f06
Keita Jamadam Sugama <sugama@jamadam.com>
2024-09-17 09:42:56 +0900

    introduce DynamicTemplateAllowSmartyTags and fix smarty delimiter confliction MTC-29585

--- lib/MT/Core.pm
+++ lib/MT/Core.pm
@@ -2240,6 +2240,7 @@ BEGIN {
             'WaitAfterReboot' => { default => '1.0' },
             'DisableMetaRefresh' => { default => 1 },
             'DynamicTemplateAllowPHP' => { default => 1 },
+            'DynamicTemplateAllowSmartyTags' => { default => 1 },
             'AdminThemeId' => { default => 'admin2023' },
             'PHPErrorLogFilePath' => undef,
             'LogEachFilePublishedInTheBackground' => undef,
