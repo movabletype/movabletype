194328420c1a6e738add5d5af3a74e614b9f2e8d
Takatsugu Shigeta <tshigeta@sixapart.com>
2024-10-11 10:40:25 +0900

    UseRiot is enabled by default

--- lib/MT/Core.pm
+++ lib/MT/Core.pm
@@ -2244,7 +2244,7 @@ BEGIN {
             'PHPErrorLogFilePath' => undef,
             'LogEachFilePublishedInTheBackground' => undef,
             'TrimFilePath' => { default => 0 },
-            'UseRiot' => undef,
+            'UseRiot' => { default => 1 },
         },
         upgrade_functions => \&load_upgrade_fns,
         applications      => {
