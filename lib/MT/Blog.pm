70a544e6fcf8f104f48a93413f470a075b1dd0f7
Keita Jamadam Sugama <sugama@jamadam.com>
2025-01-27 17:25:36 +0900

    fix uuv on site list sorted by parent_website

--- lib/MT/Blog.pm
+++ lib/MT/Blog.pm
@@ -312,8 +312,7 @@ sub list_props {
                     $parent_names{$_} = '' unless defined $parent_names{$_};
                 }
                 my @sorted = sort {
-                    $parent_names{ $a->parent_id }
-                        cmp $parent_names{ $b->parent_id }
+                    ($parent_names{ $a->parent_id || 0 } || '') cmp ($parent_names{ $b->parent_id || 0 } || '')
                 } @$objs;
                 return @sorted;
             },
