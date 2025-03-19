7afeabdd6a94640ea2dd49fb155c119ec545083a
Takatsugu Shigeta <tshigeta@sixapart.com>
2025-03-19 14:25:26 +0900

    Bump version

--- lib/MT.pm
+++ lib/MT.pm
@@ -46,7 +46,7 @@ BEGIN {
         )
         = (
         '__PRODUCT_NAME__',   'MT',
-        '7.906.2',            '__PRODUCT_VERSION_ID__',
+        '7.906.3',            '__PRODUCT_VERSION_ID__',
         '__RELEASE_NUMBER__', '__PORTAL_URL__',
         '__RELEASE_VERSION_ID__',
         );
@@ -64,11 +64,11 @@ BEGIN {
     }
 
     if ( $RELEASE_NUMBER eq '__RELEASE' . '_NUMBER__' ) {
-        $RELEASE_NUMBER = 2;
+        $RELEASE_NUMBER = 3;
     }
 
     if ( $RELEASE_VERSION_ID eq '__RELEASE' . '_VERSION_ID__' ) {
-        $RELEASE_VERSION_ID = 'r.5507';
+        $RELEASE_VERSION_ID = 'r.5508';
     }
 
     $DebugMode = 0;
