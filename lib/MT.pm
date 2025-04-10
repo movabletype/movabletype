d060ebc15d138330cbe85977d5e33d36cef43e4c
Kenichi Ishigaki <ishigaki@cpan.org>
2025-04-10 17:23:29 +0900

    Revert "Merge pull request #3531 from movabletype/fix/load-required-meta-from-yaml"
    This reverts commit b95c2f8ffdf0e3591e42ee9b78da05d635bee9fd, reversing
    changes made to b7eb0addc34dc49ac942376fb011ee3f5567bc94.

--- lib/MT.pm
+++ lib/MT.pm
@@ -1479,7 +1479,6 @@ sub init_plugins {
                 envelope => $plugin_envelope
             }
         );
-        $p->load_required_meta() if $pclass eq 'MT::Plugin';
 
         # rebless? based on config?
         local $plugin_sig = $plugin_dir;
