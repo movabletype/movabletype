a9ddd3b77e3202aa4a469e265ed0da2c720536ba
Keita Jamadam Sugama <sugama@jamadam.com>
2025-05-09 16:30:32 +0900

    perltidy

--- lib/MT/App.pm
+++ lib/MT/App.pm
@@ -1717,10 +1717,11 @@ sub bake_commenter_cookie {
     $app->bake_cookie(%user_session_kookee);
 
     my %user_session_http_only_kookee = (
-        -name  => $app->commenter_session_cookie_name . '_http_only',
-        -value => $sess_obj->get('http_only_token'),
-        -path  => $cookie_path,
-        -expires => '+3650d');
+        -name    => $app->commenter_session_cookie_name . '_http_only',
+        -value   => $sess_obj->get('http_only_token'),
+        -path    => $cookie_path,
+        -expires => '+3650d'
+    );
     $app->bake_cookie(%user_session_http_only_kookee);
 }
 
