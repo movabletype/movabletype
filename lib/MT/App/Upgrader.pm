4fe9bb85ec7917409738f71474b6fc58b4cd9515
Kenichi Ishigaki <ishigaki@cpan.org>
2025-05-20 14:36:32 +0900

    Pass param to main

--- lib/MT/App/Upgrader.pm
+++ lib/MT/App/Upgrader.pm
@@ -218,7 +218,7 @@ sub upgrade {
         $param{lacks_core_modules} = 1;
     }
 
-    return $app->main(@_) unless $app->needs_upgrade;
+    return $app->main(\%param) unless $app->needs_upgrade;
 
     my $install_mode;
 
@@ -234,7 +234,7 @@ sub upgrade {
     }
 
     if ( $method ne 'POST' ) {
-        return $app->main();
+        return $app->main(\%param);
     }
 
     $app->validate_magic or return;
