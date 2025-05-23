9b36611d4cc0bc9d49b1ef95ad47ca8e1e5248b6
Keita Jamadam Sugama <sugama@jamadam.com>
2025-05-23 09:40:25 +0900

    load edit-author.js as module

--- src/edit-author.ts
+++ src/edit-author.ts
@@ -1,30 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const apiPasswordOpener = document.querySelector<HTMLAnchorElement>(
-    "#menu-list-item-web_api_password a",
-  );
+const apiPasswordOpener = document.querySelector<HTMLAnchorElement>(
+  "#menu-list-item-web_api_password a",
+);
 
-  if (apiPasswordOpener !== null) {
-    apiPasswordOpener.addEventListener("click", (event: MouseEvent) => {
+if (apiPasswordOpener !== null) {
+  apiPasswordOpener.addEventListener("click", (event: MouseEvent) => {
+    event.preventDefault();
+    jQuery.fn.mtModal.open(apiPasswordOpener.href, { large: true });
+  });
+}
+
+const apiPasswordDeleter = document.querySelector<HTMLElement>(
+  "#delete_api_password",
+);
+
+if (apiPasswordDeleter !== null) {
+  apiPasswordDeleter.addEventListener("submit", (event: SubmitEvent) => {
+    if (
+      !confirm(
+        window.trans("Are you sure you want to delete the existing password?"),
+      )
+    ) {
       event.preventDefault();
-      jQuery.fn.mtModal.open(apiPasswordOpener.href, { large: true });
-    });
-  }
-
-  const apiPasswordDeleter = document.querySelector<HTMLElement>(
-    "#delete_api_password",
-  );
-
-  if (apiPasswordDeleter !== null) {
-    apiPasswordDeleter.addEventListener("submit", (event: SubmitEvent) => {
-      if (
-        !confirm(
-          window.trans(
-            "Are you sure you want to delete the existing password?",
-          ),
-        )
-      ) {
-        event.preventDefault();
-      }
-    });
-  }
-});
+    }
+  });
+}
