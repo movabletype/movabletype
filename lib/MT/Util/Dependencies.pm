1e9c143a5e9fd61aecd4af76f084450c7b174380
Kenichi Ishigaki <ishigaki@cpan.org>
2025-05-28 22:26:05 +0900

    Remove Authen::SASL::XS from ::Util::Dependencies

--- lib/MT/Util/Dependencies.pm
+++ lib/MT/Util/Dependencies.pm
@@ -32,11 +32,6 @@ our %Requirements = (
         tags  => ["Mail"],
         url   => "https://metacpan.org/pod/Authen::SASL",
     },
-    "Authen::SASL::XS" => {
-        label => "This module is optional. It enhances performance of Authen::SASL.",
-        tags  => ["Mail", "Performance"],
-        url   => "https://metacpan.org/pod/Authen::SASL::XS",
-    },
     "Cache::Memcached" => {
         label => "Cache::Memcached and a memcached server are optional. They are used to cache in-memory objects.",
         tags  => ["Cache"],
