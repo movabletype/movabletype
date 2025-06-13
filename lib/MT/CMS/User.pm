5c0879358d23c9870b0f99e530e6fed8b10148e1
Keita Jamadam Sugama <sugama@jamadam.com>
2025-06-13 15:14:45 +0900

    use list props for blog name column

--- lib/MT/CMS/User.pm
+++ lib/MT/CMS/User.pm
@@ -1196,6 +1196,9 @@ PERMCHECK: {
         $role = MT::Role->load($role_id);
     }
 
+    require MT::ListProperty;
+    my $blog_list_props = MT::ListProperty->list_properties('blog');
+
     my $hasher = sub {
         my ( $obj, $row ) = @_;
         $row->{label} = $row->{name};
@@ -1236,6 +1239,10 @@ PERMCHECK: {
         }
         if ( UNIVERSAL::isa( $obj, 'MT::Group' ) ) {
             $row->{icon} = MT->static_path . 'images/icons/ic_group.svg';
+        }
+
+        if (UNIVERSAL::isa($obj, 'MT::Blog')) {
+            $row->{label_html} = $blog_list_props->{name}->html($obj, $app);
         }
 
         if (UNIVERSAL::isa($obj, 'MT::Blog') && $obj->is_blog()) {
