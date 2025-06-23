308e47ba18557fd34dd683d4c90ef45d9792cb0e
Taku Amano <taku@taaas.jp>
2025-06-23 15:05:03 +0900

    DashboardWidgetTemplate: Check if the target element exists before adding the choice

--- src/mt-static/plugins/DashboardWidgetTemplate/js/list_template.ts
+++ src/mt-static/plugins/DashboardWidgetTemplate/js/list_template.ts
@@ -2,34 +2,34 @@ const label = document.querySelector<HTMLScriptElement>(
   "script[data-dashboard-widget-template-widget-l10n-dashboard-widget]",
 )?.dataset.dashboardWidgetTemplateWidgetL10nDashboardWidget as string;
 
-const newTemplateType = document.querySelector(
-  "#new-template-type",
-) as HTMLSelectElement;
-const option = document.createElement("option");
-option.value = "dashboard_widget";
-option.textContent = label;
-const widgetOption = newTemplateType.querySelector(
-  `option[value*="edit_widget"]`,
-);
-newTemplateType.insertBefore(option, widgetOption);
+const newTemplateType = document.querySelector("#new-template-type");
+if (newTemplateType) {
+  const option = document.createElement("option");
+  option.value = "dashboard_widget";
+  option.textContent = label;
+  const widgetOption = newTemplateType.querySelector(
+    `option[value*="edit_widget"]`,
+  );
+  newTemplateType.insertBefore(option, widgetOption);
+}
 
 const quickFilterWidgetSet = document.querySelector(
   `#quickfilters #widget-set-tab`,
-) as HTMLElement;
-const quickFilterDashboardWidgetSource = quickFilterWidgetSet.outerHTML.replace(
-  /widget-set/g,
-  "dashboard_widget",
 );
-const quickFilterTemplate = document.createElement("template");
-quickFilterTemplate.innerHTML = quickFilterDashboardWidgetSource;
-const quickFilterDashboardWidget = quickFilterTemplate.content;
-(
-  quickFilterDashboardWidget.querySelector("a") as HTMLAnchorElement
-).textContent = label;
-quickFilterWidgetSet.parentNode?.insertBefore(
-  quickFilterDashboardWidget,
-  quickFilterWidgetSet,
-);
+if (quickFilterWidgetSet) {
+  const quickFilterDashboardWidgetSource =
+    quickFilterWidgetSet.outerHTML.replace(/widget-set/g, "dashboard_widget");
+  const quickFilterTemplate = document.createElement("template");
+  quickFilterTemplate.innerHTML = quickFilterDashboardWidgetSource;
+  const quickFilterDashboardWidget = quickFilterTemplate.content;
+  (
+    quickFilterDashboardWidget.querySelector("a") as HTMLAnchorElement
+  ).textContent = label;
+  quickFilterWidgetSet.parentNode?.insertBefore(
+    quickFilterDashboardWidget,
+    quickFilterWidgetSet,
+  );
+}
 
 document
   .querySelector(
