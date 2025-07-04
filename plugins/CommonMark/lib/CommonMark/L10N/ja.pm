674f3c1df3c2371f680dabef5afbc683f3cbd6e9
Kenichi Ishigaki <ishigaki@cpan.org>
2025-07-04 18:22:37 +0900

    Add L10N items

---
+++ plugins/CommonMark/lib/CommonMark/L10N/ja.pm
@@ -0,0 +1,19 @@
+# Movable Type (r) (C) Six Apart Ltd. All Rights Reserved.
+# This code cannot be redistributed without permission from www.sixapart.com.
+# For more information, consult your Movable Type license.
+
+package CommonMark::L10N::ja;
+
+use strict;
+use warnings;
+use base 'CommonMark::L10N';
+
+our %Lexicon = (
+
+## plugins/CommonMark/config.yaml
+	'CommonMark is a standard, unambiguous syntax specification for Markdown.' => 'CommonMarkはあいまいさを廃した標準的なマークダウンの仕様です。',
+	'CommonMark' => 'CommonMark',
+	'GitHub Flavored Markdown' => 'GitHub Flavored Markdown',
+);
+
+1;
