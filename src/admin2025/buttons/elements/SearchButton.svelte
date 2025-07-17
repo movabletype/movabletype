3e52661ab81203431aca93029db670f82e2cddaf
Taku Amano <taku@taaas.jp>
2025-07-18 08:00:22 +0900

    Introduce "modal" action to handle modal overflow in Svelte components.

--- src/admin2025/buttons/elements/SearchButton.svelte
+++ src/admin2025/buttons/elements/SearchButton.svelte
@@ -2,6 +2,7 @@
   import { onMount } from "svelte";
   import { portal } from "svelte-portal";
   import { isOuterClick } from "../outerClick";
+  import { modal } from "../../svelte";
   import { ContentType } from "src/@types/contenttype";
   import { SearchTab } from "../search-button";
   import SearchForm from "../../forms/search/SearchForm.svelte";
@@ -65,6 +66,7 @@
     class="search-button-modal-overlay"
     on:click={handleClose}
     use:portal={"body"}
+    use:modal
   ></div>
   <div
     class="modal search-button-modal"
--- src/admin2025/buttons/elements/SiteListButton.svelte
+++ src/admin2025/buttons/elements/SiteListButton.svelte
@@ -3,6 +3,7 @@
   import SVG from "../../../svg/elements/SVG.svelte";
   import { portal } from "svelte-portal";
   import { isOuterClick } from "../outerClick";
+  import { modal } from "../../svelte";
   import { fetchSites } from "src/utils/fetch-sites";
   import { Site } from "src/@types/site";
 
@@ -337,6 +338,7 @@
     class="site-list-button-modal-overlay"
     on:click={handleClose}
     use:portal={"body"}
+    use:modal
   ></div>
   <div
     class="modal site-list-button-modal"
