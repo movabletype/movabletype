a7806925587019137d2fee5257c390ced09fe991
Masahiro Iuchi <miuchi@sixapart.com>
2025-07-18 21:54:57 +0900

    Change separator and data order in PLUGIN_VERSIONS

--- lib/MT/Util/PluginVersions.pm
+++ lib/MT/Util/PluginVersions.pm
@@ -21,7 +21,7 @@ sub load_plugin_versions {
     while (<$fh>) {
         chomp;
         next if /^#/ or /^\s*$/;
-        my ($version, $plugin_sig) = split /\t/, $_;
+        my ($plugin_sig, $version) = split /,/, $_;
         $map{$plugin_sig} = $version;
     }
     \%map;
@@ -29,7 +29,7 @@ sub load_plugin_versions {
 
 sub update_plugin_versions {
     my $temp_dir = File::Temp::tempdir(CLEANUP => 1);
-    my $cmd      = qq(touch ${temp_dir}/mtconf && MT_CONFIG=${temp_dir}/mtconf MT_CONFIG_ObjectDriver=DBI::sqlite MT_CONFIG_Database=${temp_dir}/mtdb perl -Iextlib -Ilib -MMT -E 'MT->new; map { say(\$_->version . "\t" . \$_->{plugin_sig}) } sort { \$a->{plugin_sig} cmp \$b->{plugin_sig} } grep { \$_ && \$_->isa("MT::Plugin") } map { \$MT::Plugins{\$_}->{object} } keys %MT::Plugins');
+    my $cmd      = qq(touch ${temp_dir}/mtconf && MT_CONFIG=${temp_dir}/mtconf MT_CONFIG_ObjectDriver=DBI::sqlite MT_CONFIG_Database=${temp_dir}/mtdb perl -Iextlib -Ilib -MMT -E 'MT->new; map { say(\$_->{plugin_sig} . "," . \$_->version) } sort { \$a->{plugin_sig} cmp \$b->{plugin_sig} } grep { \$_ && \$_->isa("MT::Plugin") } map { \$MT::Plugins{\$_}->{object} } keys %MT::Plugins');
     my $result   = `$cmd`;
 
     my $file = plugin_versions_file();
