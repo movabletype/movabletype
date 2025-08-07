8e722230643b1a7f5dd862dec1705f518fbbcabd
Masahiro Iuchi <miuchi@sixapart.com>
2025-08-07 10:23:39 +0900

    Move functions to the compoent that are used, and recude the scope of processing

--- src/listing/elements/ListFilterItem.svelte
+++ src/listing/elements/ListFilterItem.svelte
@@ -87,92 +87,6 @@
     return Number(element.dataset.mtListItemContentIndex);
   };
 
-  const initializeDateOption = (): void => {
-    const dateOption = ($node: JQuery<HTMLElement>): void => {
-      const val = $node.val();
-      let type: string;
-      switch (val) {
-        case "hours":
-          type = "hours";
-          break;
-        case "days":
-          type = "days";
-          break;
-        case "before":
-        case "after":
-          type = "date";
-          break;
-        case "future":
-        case "past":
-        case "blank":
-        case "not_blank":
-          type = "none";
-          break;
-        default:
-          type = "range";
-      }
-      $node
-        .parents(".item-content")
-        .find(".date-options span.date-option")
-        .hide();
-      $node
-        .parents(".item-content")
-        .find(".date-option." + type)
-        .show();
-    };
-    jQuery(root)
-      .find(".filter-date")
-      .each(function (_index, element) {
-        const $node = jQuery(element);
-        dateOption($node);
-        $node.on("change", function () {
-          dateOption($node);
-        });
-      });
-    jQuery(root)
-      .find("input.date")
-      .datepicker({
-        dateFormat: "yy-mm-dd",
-        dayNamesMin: localeCalendarHeader,
-        monthNames: [
-          "- 01",
-          "- 02",
-          "- 03",
-          "- 04",
-          "- 05",
-          "- 06",
-          "- 07",
-          "- 08",
-          "- 09",
-          "- 10",
-          "- 11",
-          "- 12",
-        ],
-        showMonthAfterYear: true,
-        prevText: "<",
-        nextText: ">",
-      });
-  };
-
-  const initializeOptionWithBlank = (): void => {
-    const changeOption = ($node: JQuery<HTMLElement>): void => {
-      if ($node.val() === "blank" || $node.val() === "not_blank") {
-        $node.parent().find("input[type=text]").hide();
-      } else {
-        $node.parent().find("input[type=text]").show();
-      }
-    };
-    jQuery(root)
-      .find(".filter-blank")
-      .each(function (_index, element) {
-        const $node = jQuery(element);
-        changeOption($node);
-        $node.on("change", function () {
-          changeOption($node);
-        });
-      });
-  };
-
   const removeFilterItem = (e: Event): void => {
     const target = e.target as HTMLElement;
     if (!target) {
@@ -221,8 +135,7 @@
                   field={filterTypeHash[loopItem.type].field}
                   item={loopItem}
                   parentDiv={fieldParentDivs[index]}
-                  {initializeDateOption}
-                  {initializeOptionWithBlank}
+                  {localeCalendarHeader}
                 />
               {/key}
               {#if !filterTypeHash[loopItem.type].singleton}
@@ -269,8 +182,7 @@
             field={filterTypeHash[item.type].field}
             {item}
             parentDiv={fieldParentDivs[0]}
-            {initializeDateOption}
-            {initializeOptionWithBlank}
+            {localeCalendarHeader}
           />
         {/key}
         {#if !filterTypeHash[item.type].singleton}
