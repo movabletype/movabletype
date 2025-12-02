e18a230b11ee99592b17dee6796514ea11e169ec
Ryota Masuko <r.g.masuko@gmail.com>
2025-12-02 13:50:57 +0900

    add Vitest for Svelte testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,23 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig, type ViteUserConfig } from "vitest/config";
 import { createHttpServer } from "./src/tests/helpers/server";
 import type { AddressInfo } from "net";
 
-export default defineConfig(async () => {
+export default defineConfig(async (): Promise<ViteUserConfig> => {
   const server = await createHttpServer();
   const port = (server.address() as AddressInfo).port;
   process.env.JSDOM_SERVER_PORT = port.toString();
 
+  const { svelte } = await import("@sveltejs/vite-plugin-svelte");
+
   return {
+    plugins: [
+      svelte({
+        hot: false,
+      }),
+    ],
+    resolve: {
+      conditions: ["browser"],
+    },
     test: {
       watch: false,
       include: ["src/**/*.{test,spec}.{js,ts}"],
@@ -19,7 +29,7 @@ export default defineConfig(async () => {
           resources: "usable",
         },
       },
-      setupFiles: ["./src/tests/helpers/window.ts"],
+      setupFiles: ["./src/tests/setup.ts"],
       globalSetup: ["./src/tests/helpers/server.ts"],
     },
   };
