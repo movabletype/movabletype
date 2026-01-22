874ed7b997efa35f1c69198d65dd1b2225f303c1
Kenichi Ishigaki <ishigaki@cpan.org>
2026-01-22 16:37:56 +0900

    Update extlib modules

--- extlib/LWP/Protocol/file.pm
+++ extlib/LWP/Protocol/file.pm
@@ -4,7 +4,7 @@ use parent qw(LWP::Protocol);
 
 use strict;
 
-our $VERSION = '6.78';
+our $VERSION = '6.81';
 
 require LWP::MediaTypes;
 require HTTP::Request;
--- extlib/URI/ftps.pm
+++ extlib/URI/ftps.pm
@@ -3,7 +3,7 @@ package URI::ftps;
 use strict;
 use warnings;
 
-our $VERSION = '5.32';
+our $VERSION = '5.34';
 
 use parent 'URI::ftp';
 
