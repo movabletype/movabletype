33b160a8df91a583b6a3b9fd078fd0e7158ddbd4
Kenichi Ishigaki <ishigaki@cpan.org>
2026-02-12 21:16:31 +0900

    Return an error message instead of redirect to dashboard

--- lib/MT/App/CMS.pm
+++ lib/MT/App/CMS.pm
@@ -2877,10 +2877,10 @@ sub is_authorized {
                 return 1;
             }
         }
-        return $app->permission_denied();
+        return $app->errtrans('Permission denied');
     }
     else {
-        return $app->permission_denied();
+        return $app->errtrans('Permission denied');
     }
 
 }
