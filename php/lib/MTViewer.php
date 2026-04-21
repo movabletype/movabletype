b6379749e0d00f5f57e9218efa157427a4a40ffb
Keita Jamadam Sugama <sugama@jamadam.com>
2026-04-21 10:10:54 +0900

    make error handler testable

--- php/lib/MTViewer.php
+++ php/lib/MTViewer.php
@@ -246,7 +246,8 @@ class MTViewer extends Smarty {
     function error($err, $error_type = E_USER_ERROR) {
         $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2);
         $caller = $trace[1] ?? $trace[0];
-        $this->mt->error_handler($error_type, $err, $caller['file'] ?? __FILE__, $caller['line'] ?? __LINE__);
+        $error_handler = get_error_handler();
+        $error_handler($error_type, $err, $caller['file'] ?? __FILE__, $caller['line'] ?? __LINE__);
         return '';
     }
 
