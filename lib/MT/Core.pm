66ab50aa33348d1d974154590df7aec37c990c33
Taku Amano <taku@taaas.jp>
2026-07-04 18:36:38 +0900

    fix: Retrieve the ID of a child blog by specifying the required permissions

--- lib/MT/Core.pm
+++ lib/MT/Core.pm
@@ -2292,6 +2292,7 @@ BEGIN {
             'CSVExportEscapeFormula' => { default => 1 },
             'AllowNonAsciiFilename' => { default => 1 },
             'RequireUpgradePermission' => { default => 1 },
+            'RequireAdministerSiteForChildAssets' => { default => 1 },
         },
         upgrade_functions => \&load_upgrade_fns,
         applications      => {
