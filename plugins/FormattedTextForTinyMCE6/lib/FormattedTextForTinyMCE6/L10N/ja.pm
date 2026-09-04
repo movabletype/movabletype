113e7e7f47175194fe40195879c5bf1dfddac8ca
Kenichi Ishigaki <ishigaki@cpan.org>
2026-09-04 17:02:55 +0900

    Add L10N items to FormattedTextForTinyMCE6

--- plugins/FormattedTextForTinyMCE6/lib/FormattedTextForTinyMCE6/L10N/ja.pm
+++ plugins/FormattedTextForTinyMCE6/lib/FormattedTextForTinyMCE6/L10N/ja.pm
@@ -14,6 +14,10 @@ use vars qw( %Lexicon );
 
 %Lexicon = (
 
+## mt-static/plugins/FormattedTextForTinyMCE6/extension.js
+	'Insert Boilerplate' => '定型文の挿入',
+	'Name' => '名前',
+
 ## plugins/FormattedTextForTinyMCE6/config.yaml
 	'Add the "Insert Boilerplate" button to the TinyMCE.' => 'TinyMCE に「定型文の挿入」ボタンを追加します。',
 
@@ -22,6 +26,8 @@ use vars qw( %Lexicon );
 
 ## plugins/FormattedTextForTinyMCE6/tmpl/extension.tmpl
 	'Boilerplate' => '定型文',
+	'Description' => '説明',
+	'Preview' => 'プレビュー',
 	'Select a Boilerplate' => '定型文を選択...',
 );
 
